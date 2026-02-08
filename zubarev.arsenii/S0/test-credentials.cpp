09706d653de7ca30fce0299631caada342a76856
zub <ars1for1work@gmail.com>
2026-02-08 11:49:32 +0300

    delete suite

--- zubarev.arsenii/S0/test-credentials.cpp
+++ zubarev.arsenii/S0/test-credentials.cpp
@@ -2,7 +2,6 @@
 #include <sstream>
 #include "credentials.hpp"
 
-BOOST_AUTO_TEST_SUITE(credentials_suite)
 
 BOOST_AUTO_TEST_CASE(credentials_test)
 {
@@ -10,12 +9,3 @@ BOOST_AUTO_TEST_CASE(credentials_test)
   zubarev::out_credentials(out);
   BOOST_TEST(out.str() == "zubarev.arsenii");
 }
-
-BOOST_AUTO_TEST_CASE(yacredentials_test)
-{
-  std::ostringstream out;
-  zubarev::out_credentials(out);
-  BOOST_TEST(out.str() == "zubarev.arsenii");
-}
-
-BOOST_AUTO_TEST_SUITE_END()
