f976c24bbd98ca80f9577adf3b89adc0ab09f80a
aaaa24 <chernov.arseniy@yandex.ru>
2026-02-09 08:12:08 +0300

    delete test suite

--- chernov.arseniy/S0/test-credentials.cpp
+++ chernov.arseniy/S0/test-credentials.cpp
@@ -2,20 +2,9 @@
 #include <sstream>
 #include "credentials.hpp"
 
-BOOST_AUTO_TEST_SUITE(credentials_suite)
-
 BOOST_AUTO_TEST_CASE(credentials_test)
 {
   std::ostringstream out;
   chernov::out_credentials(out);
   BOOST_TEST(out.str() == "chernov.arseniy");
 }
-
-BOOST_AUTO_TEST_CASE(yacredentials_test)
-{
-  std::ostringstream out;
-  chernov::out_credentials(out);
-  BOOST_TEST(out.str() == "chernov.arseniy");
-}
-
-BOOST_AUTO_TEST_SUITE_END()
