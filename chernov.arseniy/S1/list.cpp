93e1022c1c6c39cf0a83c48a37a7f56a436b00b7
aaaa24 <chernov.arseniy@yandex.ru>
2026-03-07 16:59:40 +0300

    add List::createFake, ::removeFake, ::List (without params), ::~List, ::size

--- chernov.arseniy/S1/list.cpp
+++ chernov.arseniy/S1/list.cpp
@@ -1,1 +1,34 @@
 #include "list.hpp"
+
+template< class T >
+chernov::Node< T > * chernov::List< T >::createFake()
+{
+  fake_ = static_cast< Node< T > * >(::operator new (sizeof(Node< T > *)));
+  return fake_;
+}
+
+template< class T >
+void chernov::List< T >::removeFake() noexcept
+{
+  ::operator delete(fake_);
+}
+
+template< class T >
+chernov::List< T >::List():
+  fake_(nullptr),
+  size_(0)
+{
+  createFake();
+}
+
+template< class T >
+chernov::List< T >::~List() noexcept
+{
+  removeFake();
+}
+
+template< class T >
+size_t chernov::List< T >::size() noexcept
+{
+  return size_;
+}
--- chernov.arseniy/S1/list.hpp
+++ chernov.arseniy/S1/list.hpp
@@ -1,9 +1,26 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
+#include <cstddef>
+
 namespace chernov {
   template< class T >
-  class List;
+  class Node {
+    T data;
+    Node< T > * next;
+  };
+
+  template< class T >
+  class List {
+    Node< T > * fake_;
+    size_t size_;
+    Node< T > * createFake();
+    void removeFake() noexcept;
+  public:
+    List();
+    ~List() noexcept;
+    size_t size() noexcept;
+  };
 }
 
 #endif
