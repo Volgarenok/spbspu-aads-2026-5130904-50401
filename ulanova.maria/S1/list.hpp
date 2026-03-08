af186c5edae40f77d6083098070e37db5e416ac9
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-03-09 00:39:07 +0300

    struct LCIter

--- ulanova.maria/S1/list.hpp
+++ ulanova.maria/S1/list.hpp
@@ -11,42 +11,64 @@ template<class T>
 class LIter
 {
   friend class List<T>;
-  private:
-    Node<T>* node;
-  public:
-    LIter(Node<T>* n = nullptr) : node(n) {}
-    T& operator*()
-    {
-      return node -> data;
-    }
-    LIter& operator++()
-    {
-      node = node -> next;
-      return *this;
-    }
-    bool operator!=(const LIter& other) const
-    {
-      return node != other.node;
-    }
+private:
+  Node<T>* node;
+public:
+  LIter(Node<T>* n = nullptr) : node(n) {}
+  T& operator*() noexcept
+  {
+    return node -> data;
+  }
+  LIter& operator++() noexcept
+  {
+    node = node -> next;
+    return *this;
+  }
+  bool operator!=(const LIter& other) const noexcept
+  {
+    return node != other.node;
+  }
 };
 
+template<class T>
+class LCIter
+{
+  friend class List<T>;
+private:
+  Node<T>* node;
+public:
+  LCIter(Node<T>* n = nullptr) : node(n) {}
+  const T& operator*() const noexcept
+  {
+    return node -> data;
+  }
+  LIter& operator++() noexcept
+  {
+    node = node -> next;
+    return *this;
+  }
+  bool operator!=(const LIter& other) const noexcept
+  {
+    return node != other.node;
+  }
+};
 
 template <class T>
 class List
 {
-  private:
-    Node<T>* head;
-  public:
-    List();
-    ~List();
+private:
+  Node<T>* head;
+public:
+  List();
+  ~List();
     
-    void push_front(const T& value) noexcept;
-    void pop_front() noexcept;
-    void insert_after() noexcept;
-    void erase_after() noexcept;
+  void push_front(const T& value) noexcept;
+  void pop_front() noexcept;
+  void insert_after() noexcept;
+  void erase_after() noexcept;
 
-    iterator begin();
-    iterator end();
+  iterator begin();
+  iterator end();
 
-    T& front();
+  T& front();
 };
