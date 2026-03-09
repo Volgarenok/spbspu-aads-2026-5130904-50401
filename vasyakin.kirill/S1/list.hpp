cb857225a12905c9d217b67ee59290e5a8e01aa4
potomnazovy <skeleton39876@mail.ru>
2026-03-09 15:44:58 +0300

    add libraries and namespace

---
+++ vasyakin.kirill/S1/list.hpp
@@ -0,0 +1,16 @@
+#ifndef LIST_HPP
+#define LIST_HPP
+
+#include <cstddef>
+#include <utility>
+#include <stdexcept>
+#include <limits>
+
+namespace vasyakin
+{
+  template< class T > class Node;
+  template< class T > class List;
+  const int MAX = std::numeric_limits< int >::max();
+}
+
+#endif
