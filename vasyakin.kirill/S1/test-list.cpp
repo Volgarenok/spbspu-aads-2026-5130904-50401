36fc4ee95a99f8885a8c553bfb375bc335fddd52
potomnazovy <skeleton39876@mail.ru>
2026-03-09 16:34:43 +0300

    add copy and move construct List boost tests

--- vasyakin.kirill/S1/test-list.cpp
+++ vasyakin.kirill/S1/test-list.cpp
@@ -53,4 +53,39 @@ BOOST_AUTO_TEST_CASE(DestructAfterMove)
   BOOST_CHECK_EQUAL(moved.get_size(), 1);
 }
 
+BOOST_AUTO_TEST_CASE(CopyConstruct)
+{
+  vasyakin::List< int > list;
+  list.push_back(1);
+  list.push_back(2);
+  vasyakin::List< int > copy(list);
+  list.push_back(3);
+  BOOST_CHECK_EQUAL(copy.get_size(), 2);
+  copy.push_back(52);
+  BOOST_CHECK_EQUAL(list.get_size(), 3);
+  auto it = copy.begin();
+  BOOST_CHECK_EQUAL(*it, 1);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 2);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 52);
+}
+BOOST_AUTO_TEST_CASE(MoveConstruct)
+{
+  vasyakin::List< int > list;
+  list.push_back(1);
+  list.push_back(2);
+  vasyakin::List< int > moved(std::move(list));
+  BOOST_CHECK_EQUAL(list.get_size(), 0);
+  BOOST_CHECK(list.begin() == list.end());
+  BOOST_CHECK_EQUAL(moved.get_size(), 2);
+  auto it = moved.begin();
+  BOOST_CHECK_EQUAL(*it, 1);
+  ++it;
+  BOOST_CHECK_EQUAL(*it, 2);
+  moved.push_back(99);
+  BOOST_CHECK_EQUAL(list.get_size(), 0);
+  BOOST_CHECK_EQUAL(moved.get_size(), 3);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
