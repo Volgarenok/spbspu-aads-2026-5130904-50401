52ac424f1f5032c4b61102126dfbba2b2bce9454
potomnazovy <skeleton39876@mail.ru>
2026-03-09 15:47:35 +0300

    add LIter class

--- vasyakin.kirill/S1/list.hpp
+++ vasyakin.kirill/S1/list.hpp
@@ -12,7 +12,23 @@ namespace vasyakin
   template< class T > class List;
   const int MAX = std::numeric_limits< int >::max();
 
-  
+  template< class T >
+  class LIter
+  {
+  private:
+    friend class List< T >;
+    Node< T >* ptr;
+    explicit LIter(Node< T >* p);
+
+  public:
+    T& operator*() const;
+    T* operator->() const;
+    LIter& operator++();
+    LIter operator++(int);
+    bool operator==(const LIter& other) const;
+    bool operator!=(const LIter& other) const;
+    Node< T >* get_ptr() const;
+  };
 }
 
 #endif
