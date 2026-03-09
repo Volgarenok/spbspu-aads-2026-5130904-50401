ee5a6f8793696fb436430fc766b78a135536a314
kirill <kirill@kirill.kirill>
2026-03-09 15:00:49 -0400

    считывание списка пар строк списков 0_0

--- petrov.kirill/S1/main.cpp
+++ petrov.kirill/S1/main.cpp
@@ -21,6 +21,18 @@ namespace petrov
       curr = curr->next;
       return *this;
     }
+    bool operator==(const LIter& LIt) const
+    {
+      return curr == LIt.curr;
+    }
+    bool operator!=(const LIter& LIt) const
+    {
+      return curr != LIt.curr;
+    }
+    bool operator!=(LIter& LIt)
+    {
+      return curr != LIt.curr;
+    }
     bool operator==(LIter& LIt)
     {
       return curr == LIt.curr;
@@ -35,24 +47,61 @@ namespace petrov
       List<T>* next;
       LIter<T> begin()
       {
-        return LIter(this);
+        return LIter<T>(this);
       }
       LIter<T> end()
       {
-        return LIter(nullptr);
+        return LIter<T>(static_cast<List<T>*>(nullptr));
       }
   };
 }
 
+int string_to_int(std::string string_to_record)
+{
+  int c = 0;
+  size_t i = 0;
+  while (string_to_record[i] != '\0')
+  {
+    c = c*10 + (string_to_record[i] - 48);
+    i++;
+  }
+  return c;
+}
+
 int main()
 {
-  petrov::List<int> d;
-  d.next = new petrov::List<int>;
-  d.next->val = 1;
-  d.val = 2;
-  petrov::LIter<int> c = d.begin();
-  std::cout << *c << "\n";
-  ++c;
-  std::cout << *c << "\n";
+  petrov::List<std::pair<std::string, petrov::List<int>*>> data;
+  data.next = nullptr;
+  petrov::List<std::pair<std::string, petrov::List<int>*>>* data_ptr = &data;
+  std::string string_to_record;
+  petrov::List<int>* numbers_ptr = nullptr;
+  while (std::cin >> string_to_record)
+  {
+     if (!(string_to_record[0] > 47 && string_to_record[0] < 59))
+    {
+      petrov::List<std::pair<std::string, petrov::List<int>*>>* new_data_ptr = new petrov::List<std::pair<std::string, petrov::List<int>*>>;
+      new_data_ptr->next = nullptr;
+      new_data_ptr->val.first = string_to_record;
+      new_data_ptr->val.second = nullptr;
+      data_ptr->next = new_data_ptr;
+      data_ptr = new_data_ptr;
+      numbers_ptr = nullptr; 
+    }
+    else
+    {
+      petrov::List<int>* new_numbers_ptr = new petrov::List<int>;
+      new_numbers_ptr->val = string_to_int(string_to_record);
+      new_numbers_ptr->next = nullptr;
+      if (data_ptr->val.second == nullptr)
+      {
+        data_ptr->val.second->next = new_numbers_ptr;
+      }
+      else
+      {
+        numbers_ptr->next = new_numbers_ptr;
+      }
+      numbers_ptr = new_numbers_ptr;
+    }
+  }
 }
 
