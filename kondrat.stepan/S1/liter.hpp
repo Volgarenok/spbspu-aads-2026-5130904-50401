4970f493f8826283226fd2b10e0652979d47348c
StKondrat <kndrt13@icloud.com>
2026-03-09 14:07:24 +0300

    implementation added to liter.hpp

--- kondrat.stepan/S1/liter.hpp
+++ kondrat.stepan/S1/liter.hpp
@@ -1,8 +1,17 @@
 #ifndef LITER_HPP
 #define LITER_HPP
 
+#include <cassert>
+#include <memory>
+
 namespace kondrat
 {
+  template< class T >
+  class Node;
+
+  template< class  >
+  class List;
+
   template< class T >
   class LIter
   {
@@ -21,6 +30,66 @@ namespace kondrat
       LIter(Node< T > * node) : node_(node) {}
       Node< T > * node_;
   };
+
+
+  template< class T >
+  LIter< T >::LIter() : node_(nullptr) {}
+
+  template< class T >
+  LIter< T > & LIter< T >::operator++()
+  {
+    node_ = node_->next;
+    return *this;
+  }
+
+  template< class T >
+  LIter< T > LIter< T >::operator++(int)
+  {
+    LIter< T > temp = *this;
+    ++(*this);
+    return temp;
+  }
+
+  template< class T >
+  LIter< T > & LIter< T >::operator--()
+  {
+    node_ = node_->prev;
+    return *this;
+  }
+
+  template< class T >
+  LIter< T > LIter< T >::operator--(int)
+  {
+    LIter< T > temp = *this;
+    --(*this);
+    return temp;
+  }
+
+  template< class T >
+  bool LIter< T >::operator==(LIter< T > rhs) const
+  {
+    return node_ == rhs.node_;
+  }
+
+  template< class T >
+  bool LIter< T >::operator!=(LIter< T > rhs) const
+  {
+    return !(node_ == rhs.node_);
+  }
+
+  template< class T >
+  T & LIter< T >::operator*()
+  {
+    assert(node_ != nullptr);
+    return node_->val;
+  }
+
+  template< class T >
+  T * LIter< T >::operator->()
+  {
+    assert(node_ != nullptr);
+    return std::adressof(node_->val);
+  }
 }
 
 #endif
