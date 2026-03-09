95ec8b10447dac69e0ded9240edc263c218cdc58
kirill <kirill@kirill.kirill>
2026-03-09 06:29:14 -0400

    fix literals

--- petrov.kirill/S1/main.cpp
+++ petrov.kirill/S1/main.cpp
@@ -1,5 +1,58 @@
 #include <iostream>
+#include <string>
 
-int main() {
-  std::cout << "this commit for check tests\n";
+namespace petrov
+{
+  template<class T>
+  class List;
+  template<class T>
+  class LIter
+  {
+    List<T>* curr;
+    LIter(List<T>* p) : curr(p)
+    {}  
+  public:
+    T& operator*()
+    {
+      return curr->val;
+    }
+    LIter& operator++()
+    {
+      curr = curr->next;
+      return *this;
+    }
+    bool operator==(LIter& LIt)
+    {
+      return curr == LIt.curr;
+    }
+    friend class List<T>;
+  };
+    template<class T>
+  class List
+  {
+    public:
+      T val;
+      List<T>* next;
+      LIter<T> begin()
+      {
+        return LIter(this);
+      }
+      LIter<T> end()
+      {
+        return LIter(nullptr);
+      }
+  };
 }
+
+int main()
+{
+  petrov::List<int> d;
+  d.next = new petrov::List<int>;
+  d.next->val = 1;
+  d.val = 2;
+  petrov::LIter<int> c = d.begin();
+  std::cout << *c << "\n";
+  ++c;
+  std::cout << *c << "\n";
+}
+
