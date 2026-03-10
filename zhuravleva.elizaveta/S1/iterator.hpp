e084d37eb8f6ddfebc71a09ad0c24dc282bbc880
elizazha <lizokbella@mail.ru>
2026-03-10 14:16:51 +0300

    +class LIter

--- zhuravleva.elizaveta/S1/iterator.hpp
+++ zhuravleva.elizaveta/S1/iterator.hpp
@@ -5,5 +5,61 @@
 #include "node.hpp"
 
 
+template<class T>
+class List;
+
+template<class T>
+class LIter
+{
+  friend class List<T>;
+
+private:
+  Node<T>* current;
+
+public:
+
+  LIter() noexcept:
+    current(nullptr)
+  {}
+
+  LIter(Node<T>* ptr) noexcept:
+    current(ptr)
+  {}
+
+  bool hasNext() const noexcept
+  {
+    return current != nullptr;
+  }
+
+  LIter next() const
+  {
+    if(!current)
+    {
+      throw std::runtime_error("null iterator");
+    }
+    return LIter(current->next);
+  }
+
+  T& operator*() const
+  {
+    if(!current)
+    {
+      throw std::runtime_error("null iterator");
+    }
+    return current->data;
+  }
+
+  bool operator==(const LIter& other) const noexcept
+  {
+    return current == other.current;
+  }
+
+  bool operator!=(const LIter& other) const noexcept
+  {
+    return current != other.current;
+  }
+};
+
+
 
 #endif
