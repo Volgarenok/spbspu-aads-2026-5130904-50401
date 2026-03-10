26549874af1a6334db1917db815720df0c94db28
legleb <sedovgleb2570@gmail.com>
2026-03-10 22:15:28 +0300

    Complete class List

--- sedov.gleb/S1/list.hpp
+++ sedov.gleb/S1/list.hpp
@@ -155,7 +155,7 @@ namespace sedov
     {
       for (Node< T > * cur = h.head_; cur != nullptr; cur = cur->next_)
       {
-        push_back(cur->val_);
+        pushBack(cur->val_);
       }
     }
 
@@ -220,6 +220,154 @@ namespace sedov
     const T & back() const
     {
       return tail_->val_;
+    }
+
+    void pushFront(const T & v)
+    {
+      Node< T > * newNode = new Node< T >(v);
+      newNode->next_ = head_;
+      if (head_)
+      {
+        head_->prev_ = newNode;
+      }
+      else
+      {
+        tail_ = newNode;
+      }
+      head_ = newNode;
+      ++size_;
+    }
+
+    void pushBack(const T & v)
+    {
+      Node< T > * newNode = new Node< T >(v);
+      newNode->prev_ = tail_;
+      if (tail_)
+      {
+        tail_->next_ = newNode;
+      }
+      else
+      {
+        head_ = newNode;
+      }
+      tail_ = newNode;
+      ++size_;
+    }
+
+    LIter< T > insert(LIter< T > p, const T & v)
+    {
+      if (!p.ptr_)
+      {
+        push_back(v);
+        return LIter< T >(tail_);
+      }
+      if (p.ptr_ == head_)
+      {
+        push_front(v);
+        return LIter< T >(head_);
+      }
+      Node< T > * newNode = new Node< T >(v);
+      Node< T > * next = p.ptr_;
+      Node< T > * prev = next->prev_;
+      newNode->prev_ = prev;
+      newNode->next_ = next;
+      prev->next_ = newNode;
+      next->prev_ = newNode;
+      ++size_;
+      return LIter< T >(newNode);
+    }
+
+    void popFront()
+    {
+      if (!head_)
+      {
+        return;
+      }
+      Node< T > * temp = head_;
+      head_ = head_->next_;
+      if (head_)
+      {
+        head_->prev_ = nullptr;
+      }
+      else
+      {
+        tail_ = nullptr;
+      }
+      delete temp;
+      --size_;
+    }
+
+    void popBack()
+    {
+      if (!tail_)
+      {
+        return;
+      }
+      Node< T > * temp = tail_;
+      tail_ = tail_->prev_;
+      if (tail_)
+      {
+        tail_->next_ = nullptr;
+      }
+      else
+      {
+        head_ = nullptr;
+      }
+      delete temp;
+      --size_;
+    }
+
+    LIter< T > erase(LIter< T > p)
+    {
+      if (!p.ptr_)
+      {
+        return end();
+      }
+      Node< T > * next = p.ptr_->next_;
+      Node< T > * prev = p.ptr_->prev_;
+      if (prev)
+      {
+        prev->next_ = next;
+      }
+      else
+      {
+        head_ = next;
+      }
+      if (next)
+      {
+        next->prev_ = prev;
+      }
+      else
+      {
+        tail_ = prev;
+      }
+      delete p.ptr_;
+      --size_;
+      return LIter< T >(next);
+    }
+
+    void clear()
+    {
+      while (head_)
+      {
+        Node< T > * temp = head_;
+        head_ = head_->next_;
+        delete temp;
+      }
+      tail_ = nullptr;
+      size_ = 0;
+    }
+
+    size_t size() const
+    {
+      return size_;
+    }
+
+    void swap(List & h) noexcept
+    {
+      std::swap(head_, h.head_);
+      std::swap(tail_, h.tail_);
+      std::swap(size_, h.size_);
     }
   };
 }
