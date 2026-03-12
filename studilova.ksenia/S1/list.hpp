73c9f6a22a6e016c889d1c62a3d52b473f4e4e01
kstudilova <kstudilova@gmail.com>
2026-03-13 02:03:05 +0300

    add declarations for list and iterators

---
+++ studilova.ksenia/S1/list.hpp
@@ -0,0 +1,16 @@
+#ifndef LIST_HPP
+#define LIST_HPP
+
+namespace studilova
+{
+  template< class T >
+  class List;
+
+  template< class T >
+  class LIter;
+
+  template< class T >
+  class CLIter; 
+}
+
+#endif
