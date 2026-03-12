39ce1f5f38da4a1353989d1301630e339f9115e6
Spider-has <romanh998@gmail.com>
2026-03-12 21:05:44 +0000

    add first full impl of iters

--- khasnulin.roman/S1/LCIter.hpp
+++ khasnulin.roman/S1/LCIter.hpp
@@ -1,6 +1,7 @@
 #ifndef LCITER_HPP
 #define LCITER_HPP
 
+#include <stdexcept>
 namespace khasnulin
 {
   template < class T > class BiList;
@@ -29,57 +30,106 @@ namespace khasnulin
   private:
     using LNode = typename BiList< T >::template LNode< T >;
 
-    LCIter(LNode *node, bool is_end):
+    LCIter(const BiList< T > *list, LNode *node, bool is_end):
+        list_(list),
         curr_(node),
         is_end_(is_end) {};
 
+    const BiList< T > *list_;
     LNode *curr_;
     bool is_end_;
   };
 
-  template < class T > LCIter< T >::LCIter() noexcept
+  template < class T >
+  LCIter< T >::LCIter() noexcept:
+      list_(nullptr),
+      curr_(nullptr),
+      is_end_(true)
   {
   }
 
-  template < class T > LCIter< T >::LCIter(const LIter< T > &it) noexcept
+  template < class T >
+  LCIter< T >::LCIter(const LIter< T > &it) noexcept:
+      list_(it.list_),
+      curr_(it.curr_),
+      is_end_(it.is_end_)
   {
   }
 
   template < class T > LCIter< T > &LCIter< T >::operator++()
   {
+    if (is_end_ || !curr_)
+    {
+      throw std::out_of_range("iterator can't move to next from end");
+    }
+    if (curr_->next == list_->h_)
+    {
+      is_end_ = true;
+    }
+    else
+    {
+      curr_ = curr_->next;
+    }
     return *this;
   }
+
   template < class T > LCIter< T > LCIter< T >::operator++(int)
   {
-    return *this;
+    LCIter< T > it(*this);
+    this->operator++();
+    return it;
   }
 
   template < class T > LCIter< T > &LCIter< T >::operator--()
   {
+    if (curr_ == list_->h_ || !curr_)
+    {
+      throw std::out_of_range("iterator can't move to previous from begin");
+    }
+    if (is_end_)
+    {
+      is_end_ = false;
+    }
+    else
+    {
+      curr_ = curr_->prev;
+    }
     return *this;
   }
 
   template < class T > LCIter< T > LCIter< T >::operator--(int)
   {
-    return *this;
+    LCIter< T > it(*this);
+    this->operator--();
+    return it;
   }
 
   template < class T > bool LCIter< T >::operator==(const LCIter< T > &it) const noexcept
   {
-    return false;
+    return (list_ == it.list_ && is_end_ == it.is_end_ && curr_ == it.curr_);
   }
+
   template < class T > bool LCIter< T >::operator!=(const LCIter< T > &it) const noexcept
   {
-    return false;
+    return !(*this == it);
   }
 
   template < class T > const T &LCIter< T >::operator*()
   {
-    return *(new T());
+    if (is_end_ || !curr_)
+    {
+      throw std::runtime_error("can't get value of the iterator end element");
+    }
+    return curr_->val;
   }
+
   template < class T > const T *LCIter< T >::operator->()
   {
-    return new T();
+    if (is_end_ || !curr_)
+    {
+      throw std::runtime_error("can't use arrow operator of the iterator end element");
+    }
+    return &(curr_->val);
   }
 }
 
--- khasnulin.roman/S1/LIter.hpp
+++ khasnulin.roman/S1/LIter.hpp
@@ -1,6 +1,9 @@
 #ifndef LITER_HPP
 #define LITER_HPP
 
+#include "LCIter.hpp"
+#include <stdexcept>
+
 namespace khasnulin
 {
   template < class T > class BiList;
@@ -23,56 +26,99 @@ namespace khasnulin
     T *operator->();
 
     friend class BiList< T >;
+    friend class LCIter< T >;
 
   private:
     using LNode = typename BiList< T >::template LNode< T >;
 
-    LIter(LNode *node, bool is_end):
+    LIter(const BiList< T > *list, LNode *node, bool is_end):
+        list_(list),
         curr_(node),
         is_end_(is_end) {};
 
+    const BiList< T > *list_;
     LNode *curr_;
     bool is_end_;
   };
 
-  template < class T > LIter< T >::LIter() noexcept
+  template < class T >
+  LIter< T >::LIter() noexcept:
+      list_(nullptr),
+      curr_(nullptr),
+      is_end_(true)
   {
   }
 
   template < class T > LIter< T > &LIter< T >::operator++()
   {
+    if (is_end_ || !curr_)
+    {
+      throw std::out_of_range("iterator can't move to next from end");
+    }
+    if (curr_->next == list_->h_)
+    {
+      is_end_ = true;
+    }
+    else
+    {
+      curr_ = curr_->next;
+    }
     return *this;
   }
   template < class T > LIter< T > LIter< T >::operator++(int)
   {
-    return *this;
+    LIter< T > it(*this);
+    this->operator++();
+    return it;
   }
 
   template < class T > LIter< T > &LIter< T >::operator--()
   {
+    if (curr_ == list_->h_ || !curr_)
+    {
+      throw std::out_of_range("iterator can't move to previous from begin");
+    }
+    if (is_end_)
+    {
+      is_end_ = false;
+    }
+    else
+    {
+      curr_ = curr_->prev;
+    }
     return *this;
   }
   template < class T > LIter< T > LIter< T >::operator--(int)
   {
-    return *this;
+    LIter< T > it(*this);
+    this->operator--();
+    return it;
   }
 
   template < class T > bool LIter< T >::operator==(const LIter< T > &it) const noexcept
   {
-    return false;
+    return (list_ == it.list_ && is_end_ == it.is_end_ && curr_ == it.curr_);
   }
   template < class T > bool LIter< T >::operator!=(const LIter< T > &it) const noexcept
   {
-    return false;
+    return !(*this == it);
   }
 
   template < class T > T &LIter< T >::operator*()
   {
-    return *(new T());
+    if (is_end_ || !curr_)
+    {
+      throw std::runtime_error("can't get value of the iterator end element");
+    }
+    return curr_->val;
   }
   template < class T > T *LIter< T >::operator->()
   {
-    return new T();
+    if (is_end_ || !curr_)
+    {
+      throw std::runtime_error("can't use arrow operator of the iterator end element");
+    }
+    return &(curr_->val);
   }
 }
 
