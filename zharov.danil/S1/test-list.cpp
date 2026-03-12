ca347105a72a5573bc3d608723a4c9745af05ea9
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-03-12 19:00:02 +0300

    test-list.cpp ready

--- zharov.danil/S1/test-list.cpp
+++ zharov.danil/S1/test-list.cpp
@@ -118,3 +118,111 @@ BOOST_AUTO_TEST_CASE(ConstEndTest)
   const zharov::List< int > list;
   BOOST_CHECK(list.constBegin() == list.constBegin());
 }
+
+BOOST_AUTO_TEST_CASE(FrontTest)
+{
+  zharov::List< int > list;
+  list.pushBack(1);
+  list.pushBack(3);
+  BOOST_CHECK_EQUAL(list.front(), 1);
+  const zharov::List< int > list2(list);
+  BOOST_CHECK_EQUAL(list2.front(), 1);
+}
+
+BOOST_AUTO_TEST_CASE(BackTest)
+{
+  zharov::List< int > list;
+  list.pushBack(1);
+  list.pushBack(3);
+  BOOST_CHECK_EQUAL(list.back(), 3);
+  const zharov::List< int > list2(list);
+  BOOST_CHECK_EQUAL(list2.back(), 3);
+}
+
+BOOST_AUTO_TEST_CASE(PushFrontTest)
+{
+  zharov::List< int > list;
+  list.pushFront(1);
+  list.pushFront(3);
+  BOOST_CHECK_EQUAL(list.size(), 2);
+  BOOST_CHECK_EQUAL(list.front(), 3);
+}
+
+BOOST_AUTO_TEST_CASE(PushBackTest)
+{
+  zharov::List< int > list;
+  list.pushBack(1);
+  list.pushBack(3);
+  BOOST_CHECK_EQUAL(list.size(), 2);
+  BOOST_CHECK_EQUAL(list.front(), 1);
+}
+
+BOOST_AUTO_TEST_CASE(InsertTest)
+{
+  zharov::List< int > list;
+  list.pushBack(1);
+  list.pushBack(3);
+  zharov::LIter< int > it = list.begin();
+  list.insert(++it, 2);
+  it = list.begin();
+  BOOST_CHECK_EQUAL(*it, 1);
+  BOOST_CHECK_EQUAL(*(++it), 2);
+  BOOST_CHECK_EQUAL(*(++it), 3);
+  BOOST_CHECK_EQUAL(list.size(), 3);
+}
+
+BOOST_AUTO_TEST_CASE(PopFrontTest)
+{
+  zharov::List< int > list;
+  list.pushBack(1);
+  list.pushBack(2);
+  list.pushBack(3);
+  list.popFront();
+  BOOST_CHECK_EQUAL(list.front(), 2);
+  BOOST_CHECK_EQUAL(list.size(), 2);
+}
+
+BOOST_AUTO_TEST_CASE(PopBackTest)
+{
+  zharov::List< int > list;
+  list.pushBack(1);
+  list.pushBack(2);
+  list.pushBack(3);
+  list.popBack();
+  BOOST_CHECK_EQUAL(list.back(), 2);
+  BOOST_CHECK_EQUAL(list.size(), 2);
+}
+
+BOOST_AUTO_TEST_CASE(EraseTest)
+{
+  zharov::List< int > list;
+  list.pushBack(1);
+  list.pushBack(2);
+  list.pushBack(3);
+  zharov::LIter< int > it = list.begin();
+  list.erase(++it);
+  it = list.begin();
+  BOOST_CHECK_EQUAL(*it, 1);
+  BOOST_CHECK_EQUAL(*(++it), 3);
+  BOOST_CHECK_EQUAL(list.size(), 2);
+}
+
+BOOST_AUTO_TEST_CASE(ClearTest)
+{
+  zharov::List< int > list;
+  list.pushBack(1);
+  list.pushBack(2);
+  list.pushBack(3);
+  BOOST_CHECK_EQUAL(list.size(), 3);
+  list.clear();
+  BOOST_CHECK_EQUAL(list.size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(SizeTest)
+{
+  zharov::List< int > list;
+  list.pushBack(1);
+  list.pushBack(2);
+  list.pushBack(3);
+  BOOST_CHECK_EQUAL(list.size(), 3);
+}
