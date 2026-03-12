7b2bb6833da1f844c88a7cd3b9a90a040578348a
elizazha <lizokbella@mail.ru>
2026-03-12 23:27:04 +0300

    main -> made base input reading

--- zhuravleva.elizaveta/S1/main.cpp
+++ zhuravleva.elizaveta/S1/main.cpp
@@ -5,4 +5,29 @@
 #include "list.hpp"
 
 int main()
-{}
+{
+  zhuravleva::List< std::pair<std::string, zhuravleva::List<size_t > > > sequences;
+
+  std::string name;
+
+  while(std::cin >> name)
+  {
+    zhuravleva::List< size_t > numbers;
+    size_t value = 0;
+    auto pos = numbers.beforeStart();
+    while(std::cin >> value)
+    {
+      pos = numbers.addAfter(pos, value);
+    }
+    std::cin.clear();
+    sequences.AddStart(std::make_pair(name, numbers));
+  }
+
+  if(sequences.empty())
+  {
+    std::cout << "0\n";
+    return 0;
+  }
+
+  return 0;
+}
