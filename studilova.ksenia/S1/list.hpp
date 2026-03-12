d5b3f118e3509ea881fb940302e04b9a3b9bacf1
kstudilova <kstudilova@gmail.com>
2026-03-13 02:19:23 +0300

    add node structure and list interface

--- studilova.ksenia/S1/list.hpp
+++ studilova.ksenia/S1/list.hpp
@@ -1,10 +1,30 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
+#include <cstddef>
+
 namespace studilova
 {
   template< class T >
-  class List;
+  struct Node
+  {
+    T data;
+    Node* next;
+    Node* prev;
+  };
+
+  template< class T >
+  class List
+  {
+    private:
+      Node< T >* head;
+      size_t size_;
+    public:
+      List();
+      ~List();
+
+      bool empty() const;
+  };
 
   template< class T >
   class LIter;
