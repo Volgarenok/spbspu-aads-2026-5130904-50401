ba834badfca32d3fcb0fcaaf16ace889b477ce5b
grisha-git <donkeevgrisa@gmail.com>
2026-03-13 20:37:07 +0300

    Iterator and const iterator classes

--- donkeev.grigoriy/S1/iterator.hpp
+++ donkeev.grigoriy/S1/iterator.hpp
@@ -3,24 +3,144 @@
 
 #include "list.hpp"
 #include "node.hpp"
+#include <memory>
 
-template< class T >
-class LIter
+namespace donkeev
 {
-  friend class List< T >;
-  Node< T >* n;
+  template< class T > class List;
 
-  LIter();
-  ~LIter();
+  template< class T >
+  class LIter
+  {
+    friend class List< T >;
+    Node< T >* n;
 
-  LIter< T >& operator++();
-  LIter< T > operator++(int);
+  public:
+    LIter();
+    LIter(const LIter&);
 
-  T& operator*();
-  T* operator ->();
+    LIter< T >& operator++() noexcept;
+    LIter< T > operator++(int) noexcept;
 
-  bool operator==(LIter< T >) const;
-  bool operator!=(LIter< T >) const;
+    T& operator*() const noexcept;
+    T* operator ->() const noexcept;
 
-};
+    bool operator==(const LIter< T >&) const noexcept;
+    bool operator!=(const LIter< T >&) const noexcept;
+  };
+
+  template< class T >
+  class LCIter
+  {
+    friend class List< T >;
+    const Node< T >* n;
+
+  public:
+    LCIter();
+    LCIter(const LCIter&);
+
+    LCIter< T >& operator++() noexcept;
+    LCIter< T > operator++(int) noexcept;
+
+    const T& operator*() const noexcept;
+    const T* operator ->() const noexcept;
+
+    bool operator==(const LCIter< T >&) const noexcept;
+    bool operator!=(const LCIter< T >&) const noexcept;
+  };
+
+  template< class T >
+  LIter< T >::LIter():
+    n(nullptr)
+  {}
+  template< class T >
+  LIter< T >::LIter(const LIter< T >& yaLIter):
+    n(yaLIter.n)
+  {}
+
+  template< class T >
+  LIter< T >& LIter< T >::operator++() noexcept
+  {
+    n = n->next;
+    return *this;
+  }
+  template< class T >
+  LIter< T > LIter< T >::operator++(int) noexcept
+  {
+    LIter< T > tmp = *this;
+    n = n->next;
+    return tmp;
+  }
+
+  template< class T >
+  T& LIter< T >::operator*() const noexcept
+  {
+    assert(n != nullptr);
+    return n->val;
+  }
+  template< class T >
+  T* LIter< T >::operator->() const noexcept
+  {
+    assert(n != nullptr);
+    return std::addressof(n->val);
+  }
+
+  template< class T >
+  bool LIter< T >::operator==(const LIter< T >& yaLIter) const noexcept
+  {
+    return n == yaLIter.n;
+  }
+  template< class T >
+  bool LIter< T >::operator!=(const LIter< T >& yaLIter) const noexcept
+  {
+    return !(*this == yaLIter);
+  }
+//-----------------------------------
+  template< class T >
+  LCIter< T >::LCIter():
+    n(nullptr)
+  {}
+  template< class T >
+  LCIter< T >::LCIter(const LCIter< T >& yaLCIter):
+    n(yaLCIter.n)
+  {}
+
+  template< class T >
+  LCIter< T >& LCIter< T >::operator++() noexcept
+  {
+    n = n->next;
+    return *this;
+  }
+  template< class T >
+  LCIter< T > LCIter< T >::operator++(int) noexcept
+  {
+    LCIter< T > tmp = *this;
+    n = n->next;
+    return tmp;
+  }
+
+  template< class T >
+  const T& LCIter< T >::operator*() const noexcept
+  {
+    assert(n != nullptr);
+    return n->val;
+  }
+  template< class T >
+  const T* LCIter< T >::operator->() const noexcept
+  {
+    assert(n != nullptr);
+    return std::addressof(n->val);
+  }
+
+  template< class T >
+  bool LCIter< T >::operator==(const LCIter< T >& yaLCIter) const noexcept
+  {
+    return n == yaLCIter.n;
+  }
+  template< class T >
+  bool LCIter< T >::operator!=(const LCIter< T >& yaLCIter) const noexcept
+  {
+    return !(*this == yaLCIter);
+  }
+}
 #endif
