b13ae659cac7ff68310cf7543b592a9c29f35633
Spider-has <romanh998@gmail.com>
2026-03-13 16:09:35 +0000

    add incorrect impl, all test failed

--- khasnulin.roman/S1/Sequences.hpp
+++ khasnulin.roman/S1/Sequences.hpp
@@ -7,7 +7,13 @@
 
 namespace khasnulin
 {
-  std::pair< std::string, BiList< int > > readRow(std::istream &in);
+  std::pair< std::string, BiList< int > > readRow(std::istream &in)
+  {
+    std::string name;
+    BiList< int > list;
+    in >> name;
+    return std::pair< std::string, BiList< int > >{name, list};
+  }
 }
 
 #endif
