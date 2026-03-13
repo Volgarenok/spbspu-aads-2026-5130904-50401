f8c47233f760bc9c43c8f292eaba4b7dc64fc22b
kstudilova <kstudilova@gmail.com>
2026-03-13 03:12:55 +0300

    implement basic List structure

--- studilova.ksenia/S1/list.hpp
+++ studilova.ksenia/S1/list.hpp
@@ -17,20 +17,63 @@ namespace studilova
   class List
   {
     private:
-      Node< T >* head;
+      Node< T >* head_;
       size_t size_;
     public:
       List();
       ~List();
 
       bool empty() const;
+
+      void clear();
   };
+
+  template< class T >
+  List< T >::List()
+    : head_(nullptr), size_(0)
+  {}
+
+  template< class T >
+  List< T >::~List()
+  {
+    clear();
+  }
+
+  template< class T >
+  bool List< T >::empty() const
+  {
+    return size_ == 0;
+  }
+
+  template< class T >
+  void List< T >::clear()
+  {
+    while(!empty())
+    {
+      Node< T >* temp = head_;
+      if (size_ == 1)
+      {
+        delete temp;
+        head_ = nullptr;
+        size_ = 0;
+      } else {
+        Node< T >* tail = head_->prev;
+        head_ = head_->next;
+
+        head_->prev = tail;
+        tail->next = head_;
+
+        delete temp;
+        size_--;
+      }
+    }
+  }
 
   template< class T >
   class LIter;
 
   template< class T >
-  class CLIter; 
+  class CLIter;
 }
 
 #endif
