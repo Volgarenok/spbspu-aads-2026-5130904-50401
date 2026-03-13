21978f53477cb5165950156c75d54fed5be8ca7f
Spider-has <romanh998@gmail.com>
2026-03-13 16:27:50 +0000

    add readRow impl, all tests passed

--- khasnulin.roman/S1/Sequences.hpp
+++ khasnulin.roman/S1/Sequences.hpp
@@ -1,7 +1,7 @@
 #ifndef SEQUENCES_HPP
 #define SEQUENCES_HPP
 
-#include <iosfwd>
+#include <iostream>
 
 #include "List.hpp"
 
@@ -12,7 +12,20 @@ namespace khasnulin
     std::string name;
     BiList< int > list;
     in >> name;
+    int curr_value = 0;
+    while (in >> curr_value)
+    {
+      list.push_back(curr_value);
+    }
+    if (!in.eof() && in.fail())
+    {
+      in.clear();
+    }
     return std::pair< std::string, BiList< int > >{name, list};
+  }
+
+  BiList< std::pair< std::string, BiList< int > > > readAll(std::istream &in)
+  {
   }
 }
 
