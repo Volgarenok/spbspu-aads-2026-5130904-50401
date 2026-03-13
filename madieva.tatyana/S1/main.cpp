1f2c72943b639e592c83a826f9512f1f39b75efc
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2026-03-13 17:19:37 +0300

    improvement class

--- madieva.tatyana/S1/main.cpp
+++ madieva.tatyana/S1/main.cpp
@@ -6,7 +6,7 @@ class Node {
   T val;
   Node< T > * next;
   Node< T > * prev;
-  Node(T a);
+  Node(T a, Node< T > * n, Node< T > * p);
 };
 
 template< class T >
@@ -27,13 +27,16 @@ class List {
   size_t size_;
   public:
   List();
+  void clear();
   void push_front(T a);
   LIter< T > begin();
   LIter< T > end();
+  LCIter< T > begin() const;
+  LCIter< T > end() const;
 };
 
 template< class T >
-Node< T >::Node(T a) : val(a), next(nullptr), prev(nullptr)
+Node< T >::Node(T a, Node< T > * n, Node< T > * p) : val(a), next(n), prev(p)
 {}
 
 template< class T >
@@ -41,27 +44,50 @@ List< T >::List() : head(nullptr), size_(0)
 {}
 
 template< class T >
+void List< T >::clear()
+{
+  if (head) {
+    Node< T > * end = head->prev;
+    while (head != end) {
+      Node< T > * temp = head->next;
+      delete head;
+      head = temp;
+    }
+    delete head;
+    head = nullptr;
+  }
+}
+
+template< class T >
 LIter< T > List< T >::begin() {
-  return head;
+  return LIter< T > (head);
 }
 
 template< class T >
 LIter< T > List< T >::end() {
-  return nullptr;
+  return LIter< T > (nullptr);
+}
+
+template< class T >
+LCIter< T > List< T >::begin() const {
+  return LCIter< T > (head);
+}
+
+template< class T >
+LCIter< T > List< T >::end() const {
+  return  LCIter< T > (nullptr);
 }
 
 template< class T >
 void List< T >::push_front(T a)
 {
   if (!head) {
-    head = new Node< T >(a);
+    head = new Node< T >(a, nullptr, nullptr);
     head->next = head;
     head->prev = head;
     size_ = 1;
   } else {
-    Node< T > * temp = new Node< T >(a);;
-    temp->next = head;
-    temp->prev = head->prev;
+    Node< T > * temp = new Node< T >(a, head, head->prev);
     head->prev->next = temp;
     head->prev = temp;
     head = temp;
