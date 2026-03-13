2d44801cdbbef4c3fe0de44fbcda3b48aab947a2
aaaa24 <chernov.arseniy@yandex.ru>
2026-03-13 13:27:52 +0300

    add output lines

--- chernov.arseniy/S1/main.cpp
+++ chernov.arseniy/S1/main.cpp
@@ -8,6 +8,7 @@ int main()
 {
   using namespace chernov;
   using pStrList = std::pair< std::string, List< size_t > >;
+  using pLCIterSize = std::pair< LCIter< size_t >, size_t >;
   List< pStrList > sequences;
   LIter< pStrList > seqs_iter = sequences.before_begin();
 
@@ -39,12 +40,49 @@ int main()
     return 0;
   }
 
+  List < pLCIterSize > iters;
+  LIter< pLCIterSize > iters_iter = iters.before_begin();
+
+  bool first_name = true;
   seqs_iter = sequences.begin();
-  std::cout << seqs_iter->first;
-  ++seqs_iter;
-  while (seqs_iter != sequences.begin()) {
-    std::cout << " " << seqs_iter->first;
-    ++seqs_iter;
-  }
+  do {
+    if (!first_name) {
+      std::cout << " ";
+    } else {
+      first_name = false;
+    }
+
+    std::cout << seqs_iter->first;
+    if (!seqs_iter->second.empty()) {
+      iters_iter = iters.insert_after(iters_iter, {seqs_iter->second.cbegin(), seqs_iter->second.size()});
+    }
+  } while (++seqs_iter != sequences.begin());
   std::cout << "\n";
+
+  bool exist_nums = true;
+  while (exist_nums) {
+    iters_iter = iters.begin();
+    bool first_num = true;
+    do {
+      pLCIterSize & pair = *iters_iter;
+
+      if (pair.second > 0) {
+        if (!first_num) {
+          std::cout << " ";
+        } else {
+          first_num = false;
+        }
+
+        std::cout << *pair.first;
+        ++pair.first;
+        --pair.second;
+      }
+    } while (++iters_iter != iters.begin());
+
+    if (first_num) {
+      exist_nums = false;
+    } else {
+      std::cout << "\n";
+    }
+  }
 }
