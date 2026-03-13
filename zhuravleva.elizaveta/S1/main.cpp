69a4c29945b9f76124761e14600ebf6debbd8bbf
elizazha <lizokbella@mail.ru>
2026-03-13 10:00:00 +0300

    main -> add display transposwd and add sums

--- zhuravleva.elizaveta/S1/main.cpp
+++ zhuravleva.elizaveta/S1/main.cpp
@@ -31,42 +31,32 @@ int main()
   }
 
   auto sit = sequences.begin();
-
   while(sit.hasNext())
   {
     std::cout << (*sit).first;
-
     sit = sit.next();
-
     if(sit.hasNext())
     {
       std::cout << " ";
     }
   }
-
   std::cout << "\n";
 
   size_t maxLen = 0;
-
   auto it = sequences.cbegin();
-
   while(it.hasNext())
   {
     size_t len = 0;
-
     auto nit = (*it).second.cbegin();
-
     while(nit.hasNext())
     {
       len++;
       nit = nit.next();
     }
-
     if(len > maxLen)
     {
       maxLen = len;
     }
-
     it = it.next();
   }
 
@@ -76,36 +66,83 @@ int main()
   for(size_t column = 0; column < maxLen; column++)
   {
     zhuravleva::List<size_t> newRow;
-
     auto insertPos = newRow.beforeStart();
-
     auto sit = sequences.cbegin();
 
     while(sit.hasNext())
     {
       auto nit = (*sit).second.cbegin();
-
       size_t index = 0;
-
       while(index < column && nit.hasNext())
       {
         nit = nit.next();
         index++;
       }
-
       if(nit.hasNext())
       {
         insertPos = newRow.addAfter(insertPos, *nit);
       }
-
       sit = sit.next();
     }
-
     if(!newRow.empty())
     {
       tpos = transposed.addAfter(tpos, newRow);
     }
   }
 
+ auto tit = transposed.cbegin();
+  while(tit.hasNext())
+  {
+    auto nit = (*tit).cbegin();
+    while(nit.hasNext())
+    {
+      std::cout << *nit;
+      nit = nit.next();
+      if(nit.hasNext())
+      {
+        std::cout << " ";
+      }
+    }
+    std::cout << "\n";
+    tit = tit.next();
+  }
+
+  zhuravleva::List<size_t> sums;
+  auto spos = sums.beforeStart();
+  auto tit2 = transposed.cbegin();
+
+  while(tit2.hasNext())
+  {
+    size_t sum = 0;
+    auto nit = (*tit2).cbegin();
+    while(nit.hasNext())
+    {
+      if(sum > std::numeric_limits<size_t>::max() - *nit)
+      {
+        std::cerr << "overflow\n";
+        return 1;
+      }
+
+      sum += *nit;
+      nit = nit.next();
+    }
+
+    spos = sums.addAfter(spos, sum);
+    tit2 = tit2.next();
+  }
+
+  auto sit2 = sums.begin();
+  while(sit2.hasNext())
+  {
+    std::cout << *sit2;
+    sit2 = sit2.next();
+    if(sit2.hasNext())
+    {
+      std::cout << " ";
+    }
+  }
+  std::cout << "\n";
+
   return 0;
+
 }
