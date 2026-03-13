81a13d75f0a5874bd15af4762edea12b2a9e7db1
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-03-13 11:43:57 +0300

    Enter ready

--- zharov.danil/S1/main.cpp
+++ zharov.danil/S1/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
 #include "list.hpp"
+#include <string>
 
 int main()
-{}
+{
+  zharov::List< std::pair< std::string, zharov::List< int > > > data;
+  std::string name;
+  while (std::cin >> name) {
+    int num;
+    zharov::List< int > nums;
+    while (std::cin >> num) {
+      nums.pushBack(num);
+    }
+    data.pushBack(make_pair(name, nums));
+    std::cin.clear();
+  }
+
+  std::cout << data.size() << '\n';
+  auto it = data.constBegin();
+  for (size_t i = 0; i < data.size(); ++i) {
+    std::cout << it->first << " ";
+    auto num_it = it->second.constBegin();
+    while (num_it != it->second.constEnd()) {
+      std::cout << *num_it << " ";
+      ++num_it;
+    }
+    ++it;
+    std::cout << "\n";
+  }
+}
