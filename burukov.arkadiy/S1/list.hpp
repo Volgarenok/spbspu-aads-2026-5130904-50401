bd744c9412f1ec15d1d3dc2b20bac277a89a1ea9
Arkadiy <99073279+Deflnam@users.noreply.github.com>
2026-03-13 19:46:52 +0300

    Add forward iterator classes

--- burukov.arkadiy/S1/list.hpp
+++ burukov.arkadiy/S1/list.hpp
@@ -18,6 +18,92 @@ namespace burukov
       next_(next)
     {}
   };
+
+  template< class T >
+  class LIter
+  {
+    friend class List< T >;
+  public:
+    LIter():
+      ptr_(nullptr)
+    {}
+    explicit LIter(Node< T > *p):
+      ptr_(p)
+    {}
+    T &operator*()
+    {
+      return ptr_->val_;
+    }
+    T *operator->()
+    {
+      return &(ptr_->val_);
+    }
+    LIter< T > &operator++()
+    {
+      ptr_ = ptr_->next_;
+      return *this;
+    }
+    LIter< T > operator++(int)
+    {
+      LIter< T > temp(*this);
+      ptr_ = ptr_->next_;
+      return temp;
+    }
+    bool operator==(const LIter< T > &rhs) const
+    {
+      return ptr_ == rhs.ptr_;
+    }
+    bool operator!=(const LIter< T > &rhs) const
+    {
+      return ptr_ != rhs.ptr_;
+    }
+
+  private:
+    Node< T > *ptr_;
+  };
+
+  template< class T >
+  class LCIter
+  {
+    friend class List< T >;
+  public:
+    LCIter():
+      ptr_(nullptr)
+    {}
+    explicit LCIter(const Node< T > *p):
+      ptr_(p)
+    {}
+    const T &operator*() const
+    {
+      return ptr_->val_;
+    }
+    const T *operator->() const
+    {
+      return &(ptr_->val_);
+    }
+    LCIter< T > &operator++()
+    {
+      ptr_ = ptr_->next_;
+      return *this;
+    }
+    LCIter< T > operator++(int)
+    {
+      LCIter< T > temp(*this);
+      ptr_ = ptr_->next_;
+      return temp;
+    }
+    bool operator==(const LCIter< T > &rhs) const
+    {
+      return ptr_ == rhs.ptr_;
+    }
+    bool operator!=(const LCIter< T > &rhs) const
+    {
+      return ptr_ != rhs.ptr_;
+    }
+
+  private:
+    const Node< T > *ptr_;
+  };
 }
 
 #endif
