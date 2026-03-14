bd8fe699dafc207f454eee538ed66007de747180
elizazha <lizokbella@mail.ru>
2026-03-14 12:04:44 +0300

    cg 1.4

--- zhuravleva.elizaveta/S1/test-list.cpp
+++ zhuravleva.elizaveta/S1/test-list.cpp
@@ -53,7 +53,7 @@ BOOST_AUTO_TEST_CASE(delete_after_test)
   a.AddStart(2);
   a.AddStart(1);
   auto it = a.begin();
-  a.deleteAfter(it);  
+  a.deleteAfter(it);
   BOOST_CHECK((*it) == 1);
 }
 
@@ -68,4 +68,3 @@ BOOST_AUTO_TEST_CASE(clear_test)
 
   BOOST_CHECK(a.empty());
 }
-
