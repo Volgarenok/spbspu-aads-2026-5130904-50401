c94c5d5565ffcc6518f701271317c0060914efea
Spider-has <romanh998@gmail.com>
2026-03-14 12:38:11 +0000

    add main realisation

--- khasnulin.roman/S1/main.cpp
+++ khasnulin.roman/S1/main.cpp
@@ -1,5 +1,29 @@
-#include "list.hpp"
+#include <iostream>
+
+#include "Sequences.hpp"
 
 int main()
 {
+  using namespace khasnulin;
+  bilist_row_pairs allPairs = readAll(std::cin);
+
+  if (allPairs.empty())
+  {
+    std::cout << "0\n";
+    return 0;
+  }
+
+  printSequenceNames(std::cout, allPairs);
+
+  BiList< BiList< int > > transposed = getTransosedNumsSequences(allPairs);
+
+  if (transposed.empty())
+  {
+    std::cout << "0\n";
+    return 0;
+  }
+
+  printSequencesNumsByPlace(std::cout, transposed);
+
+  printSumsOfSequences(std::cout, transposed);
 }
