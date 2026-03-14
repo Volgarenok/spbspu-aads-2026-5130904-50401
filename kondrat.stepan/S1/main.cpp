a6d804961b10a8995597ab58eec85b41f3d14650
StKondrat <kndrt13@icloud.com>
2026-03-14 21:07:10 +0300

    add get and print funcs

--- kondrat.stepan/S1/main.cpp
+++ kondrat.stepan/S1/main.cpp
@@ -1,2 +1,56 @@
-int main()
-{}
+#include <iostream>
+#include <string>
+#include <utility>
+#include "list.hpp"
+
+namespace kondrat
+{
+  using pairN = std::pair< std::string, kondrat::List< size_t > >;
+
+  void getData(std::istream & in, kondrat::List< pairN > & res)
+  {
+    std::string name;
+
+    while (in >> name)
+    {
+      kondrat::List< size_t > nums;
+      size_t num = 0;
+
+      while (in >> num)
+      {
+        nums.pushBack(num);
+      }
+
+      pairN p(name, nums);
+      res.pushBack(p);
+
+      if (in.fail() && !in.eof())
+      {
+        in.clear();
+      }
+    }
+  }
+
+  void printNames(const kondrat::List< pairN > & data)
+  {
+    bool first = true;
+
+    kondrat::LCIter< pairN > it = data.begin();
+    kondrat::LCIter< pairN > end = data.end();
+
+    while (it != end)
+    {
+      if (!first)
+      {
+        std::cout << ' ';
+      }
+
+      std::cout << (*it).first;
+
+      first = false;
+      ++it;
+    }
+
+    std::cout << '\n';
+  }
+}
