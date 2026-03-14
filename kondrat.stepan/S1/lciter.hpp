561ae228f68bc28ddc259017817aaa0f027fb7f0
StKondrat <kndrt13@icloud.com>
2026-03-14 11:12:31 +0300

    implementation added to lciter.hpp

--- kondrat.stepan/S1/lciter.hpp
+++ kondrat.stepan/S1/lciter.hpp
@@ -1,6 +1,9 @@
 #ifndef LCITER_HPP
 #define LCITER_HPP
 
+#include <cassert>
+#include <memory>
+
 namespace kondrat
 {
   template< class T >
@@ -27,6 +30,68 @@ namespace kondrat
       LCIter(Node< T > * node) : node_(node) {}
       Node< T > * node_;
   };
+
+
+  template< class T >
+  LCIter< T >::LCIter() : node_(nullptr) {}
+
+  template< class T >
+  LCIter< T > & LCIter< T >::operator++()
+  {
+    assert(node_ != nullptr);
+    node_ = node_->next;
+    return *this;
+  }
+
+  template< class T >
+  LCIter< T > LCIter< T >::operator++(int)
+  {
+    LCIter< T > temp = *this;
+    ++(*this);
+    return temp;
+  }
+
+  template< class T >
+  LCIter< T > & LCIter< T >::operator--()
+  {
+    assert(node_ != nullptr);
+    node_ = node_->prev;
+    return *this;
+  }
+
+  template< class T >
+  LCIter< T > LCIter< T >::operator--(int)
+  {
+    LCIter< T > temp = *this;
+    --(*this);
+    return temp;
+  }
+
+  template< class T >
+  bool LCIter< T >::operator==(const LCIter< T > & rhs) const
+  {
+    return node_ == rhs.node_;
+  }
+
+  template< class T >
+  bool LCIter< T >::operator!=(const LCIter< T > & rhs) const
+  {
+    return !(node_ == rhs.node_);
+  }
+
+  template< class T >
+  T & LCIter< T >::operator*() const
+  {
+    assert(node_ != nullptr);
+    return node_->val;
+  }
+
+  template< class T >
+  T * LCIter< T >::operator->() const
+  {
+    assert(node_ != nullptr);
+    return std::addressof(node_->val);
+  }
 }
 
 #endif
