ec79a17b59d89e52d7978782f7d1e15fcb44f1f3
StKondrat <kndrt13@icloud.com>
2026-03-14 15:10:48 +0300

    some edits in List class

--- kondrat.stepan/S1/list.hpp
+++ kondrat.stepan/S1/list.hpp
@@ -9,10 +9,17 @@ namespace kondrat
   class Node
   {
     public:
+      Node(T value, Node< T > * n, Node< T > * p) : val(value), next(n), prev(p) {}
       T val;
       Node< T > * next;
       Node< T > * prev;
   };
+
+  template< class T >
+  class LIter;
+
+  template< class T >
+  class LCIter;
 
   template< class T >
   class List
@@ -21,11 +28,30 @@ namespace kondrat
       List();
       ~List();
       List(const List< T > & list);
-      List< T > & operator=(List< T > & list);
-      List(const List< T > && list);
+      List< T > & operator=(const List< T > & list);
+      List(List< T > && list);
       List< T > & operator=(List< T > && list);
 
+      LIter< T > begin();
+      LIter< T > end();
+      LCIter< T > begin();
+      LCIter< T > end();
+
+      T & front();
+      T & back();
+      const T & front() const;
+      const T & back() const;
+
+      void pushFront(const T & value);
+      void pushBack(const T & value);
+
+      void popFront();
+      void popBack();
+
+      void clear();
+
       size_t size() const;
+      bool empty() const;
     private:
       Node< T > * fake_;
       size_t size_;
