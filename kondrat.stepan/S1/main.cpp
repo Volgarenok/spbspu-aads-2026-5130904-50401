e6e8c43f342518676c72a4e5049f2aa6713aac0e
StKondrat <kndrt13@icloud.com>
2026-03-14 21:24:16 +0300

    some fixes

--- kondrat.stepan/S1/main.cpp
+++ kondrat.stepan/S1/main.cpp
@@ -5,15 +5,15 @@
 
 namespace kondrat
 {
-  using pairN = std::pair< std::string, kondrat::List< size_t > >;
+  using pairN = std::pair< std::string, List< size_t > >;
 
-  void getData(std::istream & in, kondrat::List< pairN > & res)
+  void getData(std::istream & in, List< pairN > & res)
   {
     std::string name;
 
     while (in >> name)
     {
-      kondrat::List< size_t > nums;
+      List< size_t > nums;
       size_t num = 0;
 
       while (in >> num)
@@ -31,12 +31,12 @@ namespace kondrat
     }
   }
 
-  void printNames(const kondrat::List< pairN > & data)
+  void printNames(const List< pairN > & data)
   {
     bool first = true;
 
-    kondrat::LCIter< pairN > it = data.begin();
-    kondrat::LCIter< pairN > end = data.end();
+    LCIter< pairN > it = data.begin();
+    LCIter< pairN > end = data.end();
 
     while (it != end)
     {
@@ -52,5 +52,54 @@ namespace kondrat
     }
 
     std::cout << '\n';
+  }
+
+  bool hasNumbers(List< pairN > & data)
+  {
+    LIter< pairN > it = data.begin();
+    LIter< pairN > end = data.end();
+
+    while (it != end)
+    {
+      if (!((*it).second.empty()))
+      {
+        return true;
+      }
+      ++it;
+    }
+
+    return false;
+  }
+
+  size_t printOneRow(List< pairN > & data)
+  {
+    LIter< pairN > it = data.begin();
+    LIter< pairN > end = data.end();
+
+    bool first = true;
+    size_t sum = 0;
+
+    while (it != end)
+    {
+      if (!((*it).second.empty()))
+      {
+        size_t value = (*it).second.front();
+
+        if (!first)
+        {
+          std::cout << ' ';
+        }
+
+        std::cout << value;
+        sum += value;
+        (*it).second.popFront();
+
+        first = false;
+      }
+      ++it;
+    }
+
+    std::cout << '\n';
+    return sum;
   }
 }
