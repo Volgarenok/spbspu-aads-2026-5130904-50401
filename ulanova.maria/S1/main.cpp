ad8426d26b9010d124afd108fc9badb77436bf8b
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-03-15 14:53:13 +0300

    fix main if emptu clear list

--- ulanova.maria/S1/main.cpp
+++ ulanova.maria/S1/main.cpp
@@ -5,23 +5,29 @@ int main()
 {
   List<Sequence> seqs = read_sequences(std::cin);
 
-  if (!(seqs.cbegin() != seqs.cend()))
+  if (seqs.cbegin() == seqs.cend())
   {
     std::cout << "0\n";
     return 0;
   }
-
+  bool first = true;
   for (LCIter<Sequence> it = seqs.cbegin(); it != seqs.cend(); ++it)
   {
-    std::cout << it->name;
-    if (++LCIter<Sequence>(it) != seqs.cend())
+    if (!first)
     {
-      std::cout << " ";
+      std:: cout << " " ;
     }
+    std::cout << it->name;
+    first = false;
   }
   std::cout << "\n";
 
   List<List<size_t>> transposed = transpose_sequences(seqs);
+  if (transposed.cbegin() == transposed.cend())
+  {
+    std::cout << "0\n";
+    return 0;
+  }
 
   print_sequences(transposed);
 
