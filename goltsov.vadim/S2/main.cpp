75ec09ea2ea2abffad1707ba0b75ea323574b057
hlzvadik <golcovv716@gmail.com>
2026-03-17 02:19:11 +0300

    Smth

--- goltsov.vadim/S2/main.cpp
+++ goltsov.vadim/S2/main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
+#include <string>
 #include "mathoperations.hpp"
 #include "myqueue.hpp"
 #include "mystack.hpp"
 
-long long int priority(char a)
+long long int priority(std::string a)
 {
-  if (a == '-' || a == '+')
+  if (a == "-" || a == "+")
   {
     return 1;
   }
-  else if (a == '%')
+  else if (a == "%")
   {
     return 2;
   }
-  else if (a == '*' || a == '/')
+  else if (a == "*" || a == "/")
   {
     return 3;
   }
-  else if (a == '#')
+  else if (a == "##")
   {
     return 4;
   }
@@ -27,23 +28,23 @@ long long int priority(char a)
   }
 }
 
-goltsov::Queue< char > converToPostfix(goltsov::Queue< char > a)
+goltsov::Queue< std::string > converToPostfix(goltsov::Queue< std::string > a)
 {
-  goltsov::Queue< char > postfix;
-  goltsov::Stack< char > op_and_br;
+  goltsov::Queue< std::string > postfix;
+  goltsov::Stack< std::string > op_and_br;
   while(!a.empty())
   {
-    if (isdigit(a.front()))
+    if (isdigit(a.front()[0]))
     {
       postfix.push(a.front());
     }
-    else if (a.front() == '(')
+    else if (a.front() == "(")
     {
       op_and_br.push(a.front());
     }
-    else if (a.front() == ')')
+    else if (a.front() == ")")
     {
-      while(op_and_br.front() != '(')
+      while(op_and_br.front() != "(")
       {
         postfix.push(op_and_br.front());
         op_and_br.drop();
@@ -75,23 +76,22 @@ goltsov::Queue< char > converToPostfix(goltsov::Queue< char > a)
   return postfix;
 }
 
+goltsov::Queue< std::string > getLine(std::istream& input)
+{
+  goltsov::Queue< std::string > inf;
+  std::string a;
+  while (input >> a)
+  {
+    inf.push(a);\
+    if (input.peek() == '\n' || input.peek() == EOF)
+    {
+      break;
+    }
+  }
+  return inf;
+}
+
 int main()
 {
-  goltsov::Queue< char > a;
-  a.push('(');
-  a.push('5');
-  a.push('+');
-  a.push('3');
-  a.push(')');
-  a.push('-');
-  a.push('2');
-  a.push('*');
-  a.push('3');
-  goltsov::Queue< char > b = converToPostfix(a);
-  while (!b.empty())
-  {
-    std::cout << b.front() << ' ';
-    b.drop();
-  }
-  std::cout << '\n';
+  
 }
