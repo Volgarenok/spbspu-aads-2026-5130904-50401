43c7ad620798a3696fb7d9dab1cfb5fa4467b503
kirill <kirill@kirill.kirill>
2026-03-17 22:46:33 +0300

    first tests

--- petrov.kirill/S1/test_list.cpp
+++ petrov.kirill/S1/test_list.cpp
@@ -1,2 +1,48 @@
+#define BOOST_TEST_MODULE ListTest
 #include <boost/test/included/unit_test.hpp>
 #include "list.hpp"
+
+BOOST_AUTO_TEST_SUITE(ConstructionSuite)
+
+BOOST_AUTO_TEST_CASE(DefaultConstructor)
+{
+  petrov::List< int > list;
+  BOOST_CHECK(list.isEmpty());
+  BOOST_CHECK_EQUAL(list.getSize(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(TestEmptyList)
+{
+  petrov::List< int > list;
+  BOOST_CHECK(list.isEmpty());
+  BOOST_CHECK_EQUAL(list.getSize(), 0);
+  BOOST_CHECK(list.begin() == list.end());
+}
+
+BOOST_AUTO_TEST_CASE(TestPushBack)
+{
+  petrov::List< int > list;
+  list.pushBack(-1);
+  list.pushBack(-33);
+  list.pushBack(27);
+  list.pushBack(77);
+  list.pushBack(9);
+
+  BOOST_CHECK(!list.isEmpty());
+  BOOST_CHECK_EQUAL(list.getSize(), 5);
+  BOOST_CHECK_EQUAL(list.getBack(), 9);
+}
+
+BOOST_AUTO_TEST_CASE(TestClear)
+{
+  petrov::List< int > list;
+  list.pushBack(0);
+  list.pushBack(1);
+  list.pushBack(-1);
+  list.clear();
+
+  BOOST_CHECK(list.isEmpty());
+  BOOST_CHECK_EQUAL(list.getSize(), 0);
+  BOOST_CHECK(list.begin() == list.end());
+}
+BOOST_AUTO_TEST_SUITE_END()
