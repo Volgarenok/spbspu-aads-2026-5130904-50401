bda604b61df7bde392d89a91254e60bbbf142dee
legleb <sedovgleb2570@gmail.com>
2026-03-17 17:50:07 +0300

    Complete all maths funcs

---
+++ sedov.gleb/S2/math_func.cpp
@@ -0,0 +1,164 @@
+#include "math_func.hpp"
+
+namespace sedov
+{
+  bool isOperand(const std::string & sym)
+  {
+    std::string notOp[6] = {"+", "-", "*", "/", "%", ">>"};
+    for (size_t i = 0; i < 6; ++i)
+    {
+      if (sym == notOp[i])
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  size_t getPriority(const std::string & sym)
+  {
+    if (sym == ">>")
+    {
+      return 0;
+    }
+    else if (sym == "+" || sym == "-")
+    {
+      return 1;
+    }
+    else
+    {
+      return 2;
+    }
+  }
+
+  lli_t add(const lli_t & a, const lli_t & b)
+  {
+    lli_t maxllt = std::numeric_limits< lli_t >::max();
+    lli_t minllt = std::numeric_limits< lli_t >::min();
+    if ((b > 0 && a > maxllt - b) || (b < 0 && a < minllt - b))
+    {
+      throw std::overflow_error("Add overflow");
+    }
+    return a + b;
+  }
+
+  lli_t sub(const lli_t & a, const lli_t & b)
+  {
+    lli_t maxllt = std::numeric_limits< lli_t >::max();
+    lli_t minllt = std::numeric_limits< lli_t >::min();
+    if ((b > 0 && a < minllt + b) || (b < 0 && a > maxllt + b))
+    {
+      throw std::overflow_error("Sub overflow");
+    }
+    return a - b;
+  }
+
+  lli_t mult(const lli_t & a, const lli_t & b)
+  {
+    lli_t maxllt = std::numeric_limits< lli_t >::max();
+    lli_t minllt = std::numeric_limits< lli_t >::min();
+    if (a == 0 || b == 0)
+    {
+      return 0;
+    }
+    if (a == -1)
+    {
+      if (b == minllt)
+      {
+        throw std::overflow_error("Mult overflow");
+      }
+      return -b;
+    }
+    if (b == -1)
+    {
+      if (a == minllt)
+      {
+        throw std::overflow_error("Mult overflow");
+      }
+      return -a;
+    }
+    if (a > 0)
+    {
+      if (b > 0)
+      {
+        if (a > maxllt / b)
+        {
+          throw std::overflow_error("Mult overflow");
+        }
+      }
+      else
+      {
+        if (b < minllt / a)
+        {
+          throw std::overflow_error("Mult overflow");
+        }
+      }
+    }
+    else
+    {
+      if (b > 0)
+      {
+        if (a < minllt / b)
+        {
+          throw std::overflow_error("Mult overflow");
+        }
+      }
+      else
+      {
+        if (a != 0 && b < maxllt / a)
+        {
+          throw std::overflow_error("Mult overflow");
+        }
+      }
+    }
+    return a * b;
+  }
+
+  lli_t div(const lli_t & a, const lli_t & b)
+  {
+    lli_t minllt = std::numeric_limits< lli_t >::min();
+    if (b == 0)
+    {
+      throw std::invalid_argument("Div by 0");
+    }
+    if (a == minllt && b == -1)
+    {
+      throw std::overflow_error("Div overflow");
+    }
+    return a / b;
+  }
+
+  lli_t mod(const lli_t & a, const lli_t & b)
+  {
+    lli_t minllt = std::numeric_limits< lli_t >::min();
+    if (b == 0)
+    {
+      throw std::domain_error("Mod by 0");
+    }
+    if (a == minllt && b == -1)
+    {
+      throw std::overflow_error("Mod overflow");
+    }
+    lli_t r = a % b;
+    r += (r < 0) ? std::abs(b) : 0;
+    return r;
+  }
+
+  lli_t bitRightShift(const lli_t & a, const lli_t & b)
+  {
+    if (b < 0)
+    {
+      throw std::invalid_argument("Bit Right Shift by 0");
+    }
+    const int totalBits = sizeof(lli_t) * CHAR_BIT;
+    if (b >= totalBits)
+    {
+      throw std::out_of_range("Out of range bits of number");
+    }
+    if (a == 0)
+    {
+      return 0;
+    }
+    return a >> b;
+  }
+}
