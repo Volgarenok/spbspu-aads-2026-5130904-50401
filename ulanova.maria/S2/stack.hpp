af150f026ec599912a8702ddcd4efe5f74a4ff9c
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-03-21 19:35:36 +0300

    class stack

---
+++ ulanova.maria/S2/stack.hpp
@@ -0,0 +1,51 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+#include "../common/list.hpp"
+#include <stdexcept>
+
+namespace ulanova
+{
+  template <class T>
+  class Stack
+  {
+  private:
+    List<T> list;
+  public:
+    Stack() = default;
+
+    void push(const T& value);
+    T drop();
+    T& front();
+    bool empty();
+  };
+
+  template <class T>
+  void Stack<T>::push(const T& value)
+  {
+    list.push_front(value);
+  }
+  template <class T>
+  T Stack<T>::drop()
+  {
+    if (empty())
+    {
+      throw std::runtime_error("Stack is empty");
+    }
+    return list.pop_front();
+  }
+  template <class T>
+  T& Stack<T>::front()
+  {
+    if(empty())
+    {
+      throw std::runtime_error("Stack is empty");
+    }
+    return list.front();
+  }
+  template <class T>
+  bool Stack<T>::empty()
+  {
+    return list.empty
+  }
+}
+#endif
