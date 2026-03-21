017b9fa7f38a1c97fefc5ae3717b60353eaa0d23
RapidMindd <YaroslavT007@yandex.ru>
2026-03-21 11:33:37 +0300

    queue implementation

--- tarasenko.yaroslav/S2/queue.hpp
+++ tarasenko.yaroslav/S2/queue.hpp
@@ -2,6 +2,7 @@
 #define QUEUE_HPP
 
 #include "bidir_list.hpp"
+
 namespace tarasenko
 {
   template< class T >
@@ -9,6 +10,7 @@ namespace tarasenko
   {
   public:
     void push(const T& rhs);
+    void push(T&& rhs);
     T pop();
     const T& front() const;
     const T& back() const;
@@ -19,5 +21,55 @@ namespace tarasenko
   private:
     BidirList< T > data_;
   };
+
+  template< class T >
+  void Queue< T >::push(const T& rhs)
+  {
+    data_.push_back(rhs);
+  }
+
+  template< class T >
+  void Queue< T >::push(T&& rhs)
+  {
+    data_.push_back(std::move(rhs));
+  }
+
+  template< class T >
+  T Queue< T >::pop()
+  {
+    T value = data_.front();
+    data_.pop_front();
+    return value;
+  }
+
+  template< class T >
+  const T& Queue< T >::front() const
+  {
+    return data_.front();
+  }
+
+  template< class T >
+  const T& Queue< T >::back() const
+  {
+    return data_.back();
+  }
+
+  template< class T >
+  size_t Queue< T >::size() const noexcept
+  {
+    return data_.size();
+  }
+
+  template< class T >
+  bool Queue< T >::empty() const noexcept
+  {
+    return data_.empty();
+  }
+
+  template< class T >
+  void Queue< T >::clear() noexcept
+  {
+    data_.clear();
+  }
 }
 #endif
--- tarasenko.yaroslav/S2/test-queue.cpp
+++ tarasenko.yaroslav/S2/test-queue.cpp
@@ -1,3 +1,60 @@
 #include "boost/test/unit_test.hpp"
 #include "queue.hpp"
 
+BOOST_AUTO_TEST_CASE(push)
+{
+  tarasenko::Queue< int > queue;
+  BOOST_TEST(queue.empty());
+  BOOST_TEST(queue.size() == 0);
+  queue.push(5);
+  BOOST_TEST(queue.size() == 1);
+  BOOST_TEST(queue.front() == 5);
+  BOOST_TEST(queue.back() == 5);
+  const int& num1 = 3;
+  queue.push(num1);
+  BOOST_TEST(queue.front() == 5);
+  BOOST_TEST(queue.back() == 3);
+  const int& num2 = 2;
+  queue.push(std::move(num2));
+  BOOST_TEST(queue.front() == 5);
+  BOOST_TEST(queue.back() == 2);
+  queue.clear();
+  BOOST_TEST(queue.empty());
+}
+
+BOOST_AUTO_TEST_CASE(pop)
+{
+  tarasenko::Queue< int > queue;
+  for (size_t i = 0; i < 10; ++i)
+  {
+    queue.push(i);
+  }
+  for (size_t i = 0; i < 10; ++i)
+  {
+    BOOST_TEST(queue.pop() == i);
+    BOOST_TEST(queue.size() == 10 - (i + 1));
+  }
+}
+
+BOOST_AUTO_TEST_CASE(copy_and_move_semantic)
+{
+  tarasenko::Queue< int > queue;
+  for (size_t i = 0; i < 10; ++i)
+  {
+    queue.push(i);
+  }
+  tarasenko::Queue< int > copy = queue;
+  BOOST_TEST(copy.front() == 0);
+  BOOST_TEST(copy.back() == 9);
+  tarasenko::Queue< int > copy2;
+  copy2 = queue;
+  BOOST_TEST(copy2.front() == 0);
+  BOOST_TEST(copy2.back() == 9);
+  tarasenko::Queue< int > moved = std::move(queue);
+  BOOST_TEST(moved.front() == 0);
+  BOOST_TEST(moved.back() == 9);
+  tarasenko::Queue< int > moved2;
+  moved2 = std::move(moved);
+  BOOST_TEST(moved2.front() == 0);
+  BOOST_TEST(moved2.back() == 9);
+}
