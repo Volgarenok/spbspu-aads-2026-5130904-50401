dfecafe542884f07e5badc03ece49cf2b5b81c07
kirusha-13 <kirillukashevich2007@gmail.com>
2026-03-22 17:44:38 +0300

    Complete functions files + add realization in main

--- lukashevich.kirill/S1/functions.hpp
+++ lukashevich.kirill/S1/functions.hpp
@@ -5,13 +5,17 @@
 #include <iostream>
 #include <string>
 #include <stdexcept>
+#include <limits>
 
 namespace lukashevich
 {
   using pair_t = std::pair< std::string, List< size_t > >;
 
   void getData(std::istream& in, List< pair_t >& res);
-
+  void printSeqName(const List< pair_t >& data);
+  bool hasNum(const List< pair_t >& data);
+  size_t printOneRow(List< pair_t >& data);
+  void printAll(List< pair_t >& data, List< size_t >& sums);
   void printSum(const List< size_t >& sums);
 }
 
--- lukashevich.kirill/S1/main.cpp
+++ lukashevich.kirill/S1/main.cpp
@@ -12,4 +12,23 @@ int main()
     std::cout << 0 << '\n';
     return 0;
   }
+
+  lukashevich::printSeqName(data);
+
+  lukashevich::List< lukashevich::pair_t > copy(data);
+  lukashevich::List< size_t > sums;
+
+  try
+  {
+    lukashevich::printAll(copy, sums);
+  }
+  catch (const std::overflow_error&)
+  {
+    std::cerr << "erorr\n";
+    return 1;
+  }
+
+  lukashevich::printSum(sums);
+
+  return 0;
 }
