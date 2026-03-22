352ac3fdb45f5b9550c96d453ae2e968d8ff412b
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-03-22 13:00:31 +0300

    Approximate program execution plan
    The main file roughly defines the behavior of the program, which
    functions may be needed to implement it.

--- kuznetsov.petr/S2/main.cpp
+++ kuznetsov.petr/S2/main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
+#include <fstream>
 
-int main()
-{}
+#include "math_funcs.hpp"
+#include "../common/includes/stack.hpp"
+#include "../common/includes/queue.hpp"
+
+int main(int argc, char* argv[])
+{
+  namespace kuz = kuznetsov;
+  kuz::Stack< kuz::Queue< std::string > > mathExpressions;
+  try {
+    if (argc > 1) {
+      std::ifstream d(argv[1]);
+      if (!d) {
+        std::cerr << "Cant open file\n";
+        return 1;
+      }
+      kuz::getExpressions(d, mathExpressions);
+    } else {
+      kuz::getExpressions(std::cin, mathExpressions);
+    }
+  } catch (...) {
+
+  }
+
+  if (mathExpressions.empty()) {
+    return 0;
+  }
+
+  kuz::Queue< kuz::lli_t > res;
+  kuz::calculate(mathExpressions, res);
+
+  std::cout << res.front();
+  res.pop();
+  while (!res.empty()) {
+    std:: cout << ' ' << res.front();
+    res.pop();
+  }
+  std::cout << '\n';
+
+}
 
