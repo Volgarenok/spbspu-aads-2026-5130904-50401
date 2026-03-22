c1457cfe29b0b93385c09ead6eb0a1fb4bc2b7a7
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-03-22 11:21:43 +0300

    Defined isOperand function

---
+++ kuznetsov.petr/S2/math_funcs.cpp
@@ -0,0 +1,40 @@
+#include "math_funcs.hpp"
+
+bool kuznetsov::isOperand(const std::string& c)
+{
+  std::string operators[] = {"+", "-", "*", "/", "%", ">>"};
+  for (size_t i = 0; i < 6; ++i) {
+    if (c == operators[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+size_t kuznetsov::getPriority(const std::string& c)
+{
+}
+
+kuznetsov::lli_t kuznetsov::add(const lli_t& a, const lli_t& b)
+{
+}
+
+kuznetsov::lli_t kuznetsov::sub(const lli_t& a, const lli_t& b)
+{
+}
+
+kuznetsov::lli_t kuznetsov::mul(const lli_t& a, const lli_t& b)
+{
+}
+
+kuznetsov::lli_t kuznetsov::div(const lli_t& a, const lli_t& b)
+{
+}
+
+kuznetsov::lli_t kuznetsov::mod(const lli_t& a, const lli_t& b)
+{
+}
+
+kuznetsov::lli_t kuznetsov::bitShiftToRight(const lli_t& a, const lli_t& b)
+{
+}
