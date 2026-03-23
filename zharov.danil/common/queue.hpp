20266f5355aaed3a68792e78f61e624e87f3ba8f
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-03-23 22:45:29 +0300

    ready queue.hpp

--- zharov.danil/common/queue.hpp
+++ zharov.danil/common/queue.hpp
@@ -11,8 +11,8 @@ namespace zharov {
     Queue(const Queue & q);
     Queue(Queue && q) noexcept;
     ~Queue() = default;
-    Queue & operator=(const Queue & h);
-    Queue & operator=(Queue && h) noexcept;
+    Queue & operator=(const Queue & q);
+    Queue & operator=(Queue && q) noexcept;
     T & front();
     const T & front() const;
     T & back();
@@ -22,6 +22,7 @@ namespace zharov {
     void clear();
     void push(T rhs);
     T drop();
+    void swap(Queue & q);
   };
 }
 
@@ -41,11 +42,83 @@ zharov::Queue< T >::Queue(Queue && q) noexcept:
 {}
 
 template< class T >
-zharov::Queue< T > & zharov::Queue< T >::operator=(const Queue & h)
-{}
+zharov::Queue< T > & zharov::Queue< T >::operator=(const Queue & q)
+{
+  if (this != std::addressof(q)) {
+      Queue< T > temp(q);
+      swap(temp);
+    }
+    return *this;
+}
 
 template< class T >
-zharov::Queue< T > & zharov::Queue< T >::operator=(Queue && h) noexcept
-{}
+zharov::Queue< T > & zharov::Queue< T >::operator=(Queue && q) noexcept
+{
+  if (this != std::addressof(q)) {
+    clear();
+    list_ = std::move(q.list_);
+  }
+  return *this;
+}
+
+template< class T >
+T & zharov::Queue< T >::front()
+{
+  return list_.front;
+}
+
+template< class T >
+const T & zharov::Queue< T >::front() const
+{
+  return list_.front;
+}
+
+template< class T >
+T & zharov::Queue< T >::back()
+{
+  return list_.back;
+}
+
+template< class T >
+const T & zharov::Queue< T >::back() const
+{
+  return list_.back;
+}
+
+template< class T >
+bool zharov::Queue< T >::empty() const
+{
+  return !list_.size();
+}
+
+template< class T >
+size_t zharov::Queue< T >::size() const
+{
+  return list_.size();
+}
+
+template< class T >
+void zharov::Queue< T >::clear()
+{
+  list_.clear();
+}
+
+template< class T >
+void zharov::Queue< T >::push(T rhs)
+{
+  list_.pushBack(rhs);
+}
+
+template< class T >
+T zharov::Queue< T >::drop()
+{
+  list_.popFront();
+}
+
+template< class T >
+void zharov::Queue< T >::swap(Queue & q)
+{
+  list_.swap(q.list_);
+}
 
 #endif
