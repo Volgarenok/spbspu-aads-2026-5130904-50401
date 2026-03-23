2ea170b2646d76739c9f80abc8d94c246f11c181
aaaa24 <chernov.arseniy@yandex.ru>
2026-03-24 01:36:42 +0300

    add default constructors, operators, destructor; add empty, size, clear

--- chernov.arseniy/common/queue.cpp
+++ chernov.arseniy/common/queue.cpp
@@ -1,19 +1,44 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 
+#include <cstddef>
 #include <list.hpp>
 
 namespace chernov {
   template< class T >
   class Queue {
   public:
-    Queue();
-    ~Queue();
-    void push(T rhs);
-    T drop();
+    Queue() = default;
+    Queue(const Queue< T > &) = default;
+    Queue(Queue< T > &&) = default;
+    ~Queue() = default;
+    Queue< T > & operator=(const Queue< T > &) = default;
+    Queue< T > & operator=(Queue< T > &&) = default;
+
+    bool empty() const noexcept;
+    size_t size() const noexcept;
+    void clear();
   private:
     List< T > list_;
   };
+}
+
+template< class T >
+bool chernov::Queue< T >::empty() const noexcept
+{
+  return list_.empty();
+}
+
+template< class T >
+size_t chernov::Queue< T >::size() const noexcept
+{
+  return list_.size();
+}
+
+template< class T >
+void chernov::Queue< T >::clear()
+{
+  list_.clear();
 }
 
 #endif
