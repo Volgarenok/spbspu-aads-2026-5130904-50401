b36045cda5ccd14ecdcd2a27b3075557a1648817
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-03-25 00:20:53 +0300

    main

--- ulanova.maria/S2/main.cpp
+++ ulanova.maria/S2/main.cpp
@@ -1,2 +1,33 @@
+#include "infix.hpp"
+#include "postfix.hpp"
+#include <iostream>
+#include <string>
+
 int main()
-{}
+{
+  ulanova::Stack<long long> results;
+  std::string line;
+
+  try
+  {
+    while (std::getline(std::cin, line))
+    {
+      if (line.empty())
+      {
+        continue; //
+      }
+      std::string postfix = ulanova::infix_to_postfix(line);
+      long long result = ulanova::postfix(postfix);
+      results.push(result);
+    }
+    while (!results.empty())
+    {
+      std::cout << results.drop() << "\n";
+    }
+  } catch (const std::exception& e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
+  return 0;
+}
