3454f6f3fa8629d6fe6ac79304e5f16fe03ce133
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-03-24 23:37:34 +0300

    completed math_functions

--- zharov.danil/S2/math_functions.cpp
+++ zharov.danil/S2/math_functions.cpp
@@ -1,37 +1,90 @@
 #include <limits>
+#include <climits>
 #include <stdexcept>
 #include "math_functions.hpp"
 
-using lli_t = zharov::lli_t;
+using ll_t = zharov::ll_t;
 
-lli_t add(const lli_t & a, const lli_t & b)
+
+
+
+ll_t add(const ll_t & a, const ll_t & b)
 {
-  lli_t max = std::numeric_limits< lli_t >::max();
-  lli_t min = std::numeric_limits< lli_t >::min();
+  ll_t max = std::numeric_limits< ll_t >::max();
+  ll_t min = std::numeric_limits< ll_t >::min();
   if ((b > 0 && a > max - b) || (b < 0 && a < min - b)){
     throw std::overflow_error("Overflow in add");
   }
   return a + b;
 }
 
-lli_t sub(const lli_t & a, const lli_t & b)
+ll_t sub(const ll_t & a, const ll_t & b)
 {
-  lli_t max = std::numeric_limits< lli_t >::max();
-  lli_t min = std::numeric_limits< lli_t >::min();
+  ll_t max = std::numeric_limits< ll_t >::max();
+  ll_t min = std::numeric_limits< ll_t >::min();
   if ((b > 0 && a < min + b ) || (b < 0 && a > max + b)) {
     throw std::overflow_error("Overflow in sub");
   }
   return a - b;
 }
 
-lli_t mult(const lli_t & a, const lli_t & b)
-{}
+ll_t mul(const ll_t & a, const ll_t & b)
+{
+  if (a == 0 || b == 0) {
+    return 0;
+  }
+  ll_t max = std::numeric_limits< ll_t >::max();
+  ll_t min = std::numeric_limits< ll_t >::min();
+  if ((a == -1 && b == min) || (b == -1 && a == min)) {
+    throw std::overflow_error("Overflow in mul");
+  }
+  if ((a > 0 && b > 0 && a > max / b) ||
+    (a > 0 && b < 0 && b < min / a) || 
+    (a < 0 && b > 0 && a < min / b) ||
+    (a < 0 && b < 0 && a < max / b)) {
+      throw std::overflow_error("Overflow in mul");
+    }
+  return a * b;
+}
 
-lli_t div(const lli_t & a, const lli_t & b)
-{}
+ll_t div(const ll_t & a, const ll_t & b)
+{
+  ll_t max = std::numeric_limits<ll_t>::max();
+  ll_t min = std::numeric_limits<ll_t>::min();
+  if (b == 0) {
+    throw std::logic_error("Div by zero");
+  }
+  if (a == min && b == -1) {
+    throw std::overflow_error("Overflow in div");
+  }
+  return a / b;
+}
 
-lli_t mod(const lli_t & a, const lli_t & b)
-{}
+ll_t mod(const ll_t & a, const ll_t & b)
+{
+  ll_t min = std::numeric_limits<ll_t>::min();
 
-lli_t bitShiftLeft(const lli_t & a, const lli_t & b)
-{}
+  if (b == 0) {
+    throw std::logic_error("Mod by zero");
+  }
+  if (a == min && b == -1) {
+    throw std::overflow_error("Overflow in mod");
+  }
+  return a % b;
+}
+
+ll_t bitShiftLeft(const ll_t & a, const ll_t & b)
+{
+  ll_t max = std::numeric_limits<ll_t>::max();
+  ll_t width = sizeof(ll_t) * CHAR_BIT;
+  if (a < 0) {
+    throw std::overflow_error("Cannot shift negative number");
+  }
+  if (b < 0 || b >= width) {
+    throw std::overflow_error("Invalid shift amount");
+  }
+  if (a > (max >> b)) {
+    throw std::overflow_error("Overflow in bitShiftLeft");
+  }
+  return a << b;
+}
--- zharov.danil/S2/math_functions.hpp
+++ zharov.danil/S2/math_functions.hpp
@@ -1,17 +1,15 @@
 #ifndef MATH_FUNCTIONS_HPP
 #define MATH_FUNCTIONS_HPP
 
-
-
 namespace zharov {
-  using lli_t = long long int;
+  using ll_t = long long int;
 
-  lli_t add(const lli_t & a, const lli_t & b);
-  lli_t sub(const lli_t & a, const lli_t & b);
-  lli_t mult(const lli_t & a, const lli_t & b);
-  lli_t div(const lli_t & a, const lli_t & b);
-  lli_t mod(const lli_t & a, const lli_t & b);
-  lli_t bitShiftLeft(const lli_t & a, const lli_t & b);
+  ll_t add(const ll_t & a, const ll_t & b);
+  ll_t sub(const ll_t & a, const ll_t & b);
+  ll_t mul(const ll_t & a, const ll_t & b);
+  ll_t div(const ll_t & a, const ll_t & b);
+  ll_t mod(const ll_t & a, const ll_t & b);
+  ll_t bitShiftLeft(const ll_t & a, const ll_t & b);
 }
 
 #endif
