8e16d78bb8fa4480e011752a7f5c7814df4ff0d2
elizazha <lizokbella@mail.ru>
2026-03-24 20:05:03 +0300

    stack.hpp implementation

--- zhuravleva.elizaveta/S2/stack.hpp
+++ zhuravleva.elizaveta/S2/stack.hpp
@@ -23,4 +23,47 @@ namespace zhuravleva
   };
 }
 
+template< class T >
+void zhuravleva::Stack< T >::push(const T& value)
+{
+  data.addEnd(value);
+}
+
+template< class T >
+void zhuravleva::Stack< T >::pop()
+{
+  if(data.empty())
+  {
+    throw std::runtime_error("empty data error");
+  }
+  data.deleteEnd();
+}
+
+template< class T >
+T zhuravleva::Stack< T >::top()
+{
+  if(data.empty())
+  {
+    throw std::runtime_error("empty data error");
+  }
+  auto cur = data.begin();
+  while(cur -> next != data.end())
+  {
+    cur = cur -> next;
+  }
+  return *cur;
+}
+
+template< class T >
+bool zhuravleva::Stack< T >::empty() const
+{
+  return data.empty();
+}
+
+template< class T >
+void zhuravleva::Stack< T >::clear()
+{
+  data.clear();
+}
+
 #endif
