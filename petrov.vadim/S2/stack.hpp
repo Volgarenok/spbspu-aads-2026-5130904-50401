09ec3f338051320639855ccb18cab7ea6c8e8675
Petrov Vadim <vadikdydy@mail.ru>
2026-03-24 16:32:06 +0300

    implementation Stacks methods

--- petrov.vadim/S2/stack.hpp
+++ petrov.vadim/S2/stack.hpp
@@ -25,4 +25,101 @@ namespace petrov
   private:
     List<T> list_;
   };
+
+  template<typename T>
+  Stack<T>::Stack():
+    list_()
+  {}
+
+  template<typename T>
+  Stack<T>::Stack(const Stack<T>& s):
+    list_(s.list_)
+  {}
+
+  template<typename T>
+  Stack<T>::Stack(Stack<T>&& s):
+    list_(std::move(s.list_))
+  {}
+
+  template<typename T>
+  Stack<T>& Stack<T>::operator=(const Stack<T>& s)
+  {
+    if (this != &s)
+    {
+      list_ = s.list_;
+    }
+    return *this;
+  }
+
+  template<typename T>
+  Stack<T>& Stack<T>::operator=(Stack<T>&& s)
+  {
+    if (this != &s)
+    {
+      list_ = std::move(s.list_);
+    }
+    return *this;
+  }
+
+  template<typename T>
+  void Stack<T>::push(const T& rhs)
+  {
+    list_.insert(list_.begin(), rhs);
+  }
+
+  template<typename T>
+  T Stack<T>::drop()
+  {
+    if (empty())
+    {
+      throw std::runtime_error("Stack is empty");
+    }
+    T res = top();
+    pop();
+    return res;
+  }
+
+  template<typename T>
+  const T& Stack<T>::top() const
+  {
+    if (empty())
+    {
+      throw std::runtime_error("Stack is empty");
+    }
+    return *list_.begin();
+  }
+
+  template<typename T>
+  void Stack<T>::pop()
+  {
+    if (empty())
+    {
+      throw std::runtime_error("Stack is empty");
+    }
+    list_.popStart();
+  }
+
+  template<typename T>
+  bool Stack<T>::empty() const
+  {
+    return list_.size() == 0;
+  }
+
+  template<typename T>
+  size_t Stack<T>::getSize() const
+  {
+    return list_.size();
+  }
+
+  template<typename T>
+  void Stack<T>::clear()
+  {
+    list_.clear();
+  }
+
+  template<typename T>
+  Stack<T>::~Stack()
+  {
+    clear();
+  }
 }
