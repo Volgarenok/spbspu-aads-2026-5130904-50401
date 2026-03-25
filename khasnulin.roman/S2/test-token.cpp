ba99a3860e1b06b618c3e266436e952017ad99ee
Spider-has <romanh998@gmail.com>
2026-03-25 23:44:07 +0000

    add test cases for token reading from stream

---
+++ khasnulin.roman/S2/test-token.cpp
@@ -0,0 +1,81 @@
+#include <boost/test/tools/old/interface.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_suite.hpp>
+
+#include "Token.hpp"
+
+BOOST_AUTO_TEST_SUITE(token_reading_suite)
+
+using namespace khasnulin;
+
+BOOST_AUTO_TEST_CASE(test_read_basic_tokens)
+{
+  std::stringstream ss("123 + ( ) *");
+
+  Token t1 = readToken(ss);
+  BOOST_CHECK_EQUAL(t1.type, TokenType::Number);
+  BOOST_CHECK_EQUAL(t1.value, 123);
+
+  Token t2 = readToken(ss);
+  BOOST_CHECK_EQUAL(t2.type, TokenType::Operator);
+  BOOST_CHECK_EQUAL(t2.op, '+');
+  BOOST_CHECK_EQUAL(t2.priority, 1);
+
+  Token t3 = readToken(ss);
+  BOOST_CHECK_EQUAL(t3.type, TokenType::LeftParen);
+
+  Token t4 = readToken(ss);
+  BOOST_CHECK_EQUAL(t4.type, TokenType::RightParen);
+
+  Token t5 = readToken(ss);
+  BOOST_CHECK_EQUAL(t5.op, '*');
+  BOOST_CHECK_EQUAL(t5.priority, 2);
+}
+
+BOOST_AUTO_TEST_CASE(test_read_end_of_stream)
+{
+  std::stringstream ss("42");
+  readToken(ss);
+  Token t = readToken(ss);
+
+  BOOST_CHECK_EQUAL(t.type, TokenType::End);
+}
+
+BOOST_AUTO_TEST_CASE(test_number_overflow)
+{
+  std::stringstream ss("99999999999999999999");
+  BOOST_CHECK_THROW(readToken(ss), std::overflow_error);
+}
+
+BOOST_AUTO_TEST_CASE(test_max_int)
+{
+  std::stringstream ss("2147483647");
+  Token t = readToken(ss);
+  BOOST_CHECK_EQUAL(t.value, 2147483647);
+}
+
+BOOST_AUTO_TEST_CASE(test_invalid_tokens)
+{
+  std::stringstream ss("123a @ ?");
+
+  BOOST_CHECK_THROW(readToken(ss), std::runtime_error);
+
+  BOOST_CHECK_THROW(readToken(ss), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(test_multichar_operators)
+{
+  std::stringstream ss("++ == **");
+  BOOST_CHECK_THROW(readToken(ss), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(test_whitespace_handling)
+{
+  std::stringstream ss("   42      +     ");
+  Token t1 = readToken(ss);
+  BOOST_CHECK_EQUAL(t1.value, 42);
+  Token t2 = readToken(ss);
+  BOOST_CHECK_EQUAL(t2.op, '+');
+}
+
+BOOST_AUTO_TEST_SUITE_END()
