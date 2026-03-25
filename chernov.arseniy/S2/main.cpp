cbd684409c164ac22dbe27d48da81d9314fd7e9b
aaaa24 <chernov.arseniy@yandex.ru>
2026-03-25 16:01:19 +0300

    write begin of main

--- chernov.arseniy/S2/main.cpp
+++ chernov.arseniy/S2/main.cpp
@@ -1,2 +1,21 @@
-int main()
-{}
+#include <fstream>
+#include <iostream>
+
+#include <stack.hpp>
+#include <queue.hpp>
+
+int main(int args, char ** argv)
+{
+  using namespace chernov;
+
+  std::istream * input = &std::cin;
+  if (args > 1) {
+    static std::ifstream file_input(argv[1]);
+    input = &file_input;
+  }
+
+  std::string line;
+  while (*input) {
+    *input >> line;
+  }
+}
