9b4f3fabd467bbae4756d9b35e35b6041c36a35b
Arkadiy <99073279+Deflnam@users.noreply.github.com>
2026-03-25 21:27:17 +0300

    add input reading from file or stdin in main

--- burukov.arkadiy/S2/main.cpp
+++ burukov.arkadiy/S2/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <fstream>
 #include "mathFunctions.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
-  std::cout << burukov::isOperand("42") << "\n";
-  std::cout << burukov::getPriority("+") << "\n";
+  using namespace burukov;
+  Stack< Queue< std::string > > infix;
+  if (argc < 2)
+  {
+    try
+    {
+      getInfix(std::cin, infix);
+    }
+    catch (...)
+    {
+      std::cerr << "Input error\n";
+      return 1;
+    }
+  }
+  else if (argc == 2)
+  {
+    std::ifstream input(argv[1]);
+    if (!input)
+    {
+      std::cerr << "Cannot open file\n";
+      return 1;
+    }
+    try
+    {
+      getInfix(input, infix);
+    }
+    catch (...)
+    {
+      std::cerr << "Read error\n";
+      return 1;
+    }
+  }
+
+  if (infix.empty())
+  {
+    std::cout << "\n";
+    return 0;
+  }
+
+  std::cout << "Expressions: " << infix.size() << "\n";
   return 0;
 }
