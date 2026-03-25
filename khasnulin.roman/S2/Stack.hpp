915383415c90b23ffe0d7488cc8f8484191ece45
Spider-has <romanh998@gmail.com>
2026-03-25 21:32:04 +0000

    add stack interface

--- khasnulin.roman/S2/Stack.hpp
+++ khasnulin.roman/S2/Stack.hpp
@@ -1,10 +1,44 @@
 #ifndef STACK_HPP
 #define STACK_HPP
 
+#include "List.hpp"
+#include <cstddef>
+
 namespace khasnulin
 {
   template < class T > class Stack
   {
+  public:
+    Stack() = default;
+
+    Stack(const Stack< T > &rhs) = default;
+    Stack(Stack< T > &&rhs) noexcept = default;
+
+    Stack< T > &operator=(const Stack< T > &rhs) = default;
+    Stack< T > &operator=(Stack< T > &&rhs) noexcept = default;
+
+    bool operator==(const Stack< T > &rhs) const noexcept;
+    bool operator!=(const Stack< T > &rhs) const noexcept;
+
+    ~Stack() = default;
+
+    T &top();
+    const T &top() const;
+
+    bool empty() const noexcept;
+    size_t size() const noexcept;
+
+    void push(const T &elem);
+    void push(T &&elem);
+
+    template < class... Args > void emplace(Args &&...args);
+
+    void pop();
+
+    void swap(Stack< T > &rhs) noexcept;
+
+  private:
+    BiList< T > list_;
   };
 }
 
