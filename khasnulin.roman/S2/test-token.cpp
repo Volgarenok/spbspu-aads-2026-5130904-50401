9a689ecfb5e7f40f4bd8bfc563b2b45afa9dfb18
Spider-has <romanh998@gmail.com>
2026-03-26 00:29:45 +0000

    change test cases, add test cases for multyline tokens

--- khasnulin.roman/S2/test-token.cpp
+++ khasnulin.roman/S2/test-token.cpp
@@ -1,6 +1,8 @@
 #include <boost/test/tools/old/interface.hpp>
 #include <boost/test/unit_test.hpp>
 #include <boost/test/unit_test_suite.hpp>
+#include <sstream>
+#include <string>
 
 #include "Token.hpp"
 
@@ -10,72 +12,115 @@ using namespace khasnulin;
 
 BOOST_AUTO_TEST_CASE(test_read_basic_tokens)
 {
-  std::stringstream ss("123 + ( ) *");
+  std::string ss("123 + ( ) *");
+  size_t pos = 0;
 
-  Token t1 = readToken(ss);
+  Token t1 = readToken(ss, pos);
   BOOST_CHECK_EQUAL(t1.type, TokenType::Number);
   BOOST_CHECK_EQUAL(t1.value, 123);
 
-  Token t2 = readToken(ss);
+  Token t2 = readToken(ss, pos);
   BOOST_CHECK_EQUAL(t2.type, TokenType::Operator);
   BOOST_CHECK_EQUAL(t2.op, '+');
   BOOST_CHECK_EQUAL(t2.priority, 1);
 
-  Token t3 = readToken(ss);
+  Token t3 = readToken(ss, pos);
   BOOST_CHECK_EQUAL(t3.type, TokenType::LeftParen);
 
-  Token t4 = readToken(ss);
+  Token t4 = readToken(ss, pos);
   BOOST_CHECK_EQUAL(t4.type, TokenType::RightParen);
 
-  Token t5 = readToken(ss);
+  Token t5 = readToken(ss, pos);
   BOOST_CHECK_EQUAL(t5.op, '*');
   BOOST_CHECK_EQUAL(t5.priority, 2);
 }
 
 BOOST_AUTO_TEST_CASE(test_read_end_of_stream)
 {
-  std::stringstream ss("42");
-  readToken(ss);
-  Token t = readToken(ss);
+  std::string ss("42");
+  size_t pos = 0;
+  readToken(ss, pos);
+  Token t = readToken(ss, pos);
 
-  BOOST_CHECK_EQUAL(t.type, TokenType::End);
+  BOOST_CHECK_EQUAL(t.type, TokenType::EndOfExpr);
 }
 
 BOOST_AUTO_TEST_CASE(test_number_overflow)
 {
-  std::stringstream ss("99999999999999999999");
-  BOOST_CHECK_THROW(readToken(ss), std::overflow_error);
+  std::string ss("99999999999999999999");
+  size_t pos = 0;
+  BOOST_CHECK_THROW(readToken(ss, pos), std::overflow_error);
 }
 
 BOOST_AUTO_TEST_CASE(test_max_int)
 {
-  std::stringstream ss("2147483647");
-  Token t = readToken(ss);
+  std::string ss("2147483647");
+  size_t pos = 0;
+  Token t = readToken(ss, pos);
   BOOST_CHECK_EQUAL(t.value, 2147483647);
 }
 
 BOOST_AUTO_TEST_CASE(test_invalid_tokens)
 {
-  std::stringstream ss("123a @ ?");
+  std::string ss("123a @ ?");
+  size_t pos = 0;
+  BOOST_CHECK_THROW(readToken(ss, pos), std::runtime_error);
 
-  BOOST_CHECK_THROW(readToken(ss), std::runtime_error);
-
-  BOOST_CHECK_THROW(readToken(ss), std::runtime_error);
+  BOOST_CHECK_THROW(readToken(ss, pos), std::runtime_error);
 }
 
 BOOST_AUTO_TEST_CASE(test_multichar_operators)
 {
-  std::stringstream ss("++ == **");
-  BOOST_CHECK_THROW(readToken(ss), std::runtime_error);
+  std::string ss("++ == **");
+  size_t pos = 0;
+  BOOST_CHECK_THROW(readToken(ss, pos), std::runtime_error);
 }
 
 BOOST_AUTO_TEST_CASE(test_whitespace_handling)
 {
-  std::stringstream ss("   42      +     ");
-  Token t1 = readToken(ss);
+  std::string ss("   42      +     ");
+  size_t pos = 0;
+  Token t1 = readToken(ss, pos);
   BOOST_CHECK_EQUAL(t1.value, 42);
-  Token t2 = readToken(ss);
+  Token t2 = readToken(ss, pos);
   BOOST_CHECK_EQUAL(t2.op, '+');
+}
+
+BOOST_AUTO_TEST_CASE(test_multiline_input)
+{
+  std::stringstream ss("1 + 2\n3 * 4");
+
+  std::string s;
+
+  std::getline(ss, s);
+  size_t pos = 0;
+
+  BOOST_CHECK_EQUAL(readToken(s, pos).value, 1);
+  BOOST_CHECK_EQUAL(readToken(s, pos).op, '+');
+  BOOST_CHECK_EQUAL(readToken(s, pos).value, 2);
+  BOOST_CHECK_EQUAL(readToken(s, pos).type, TokenType::EndOfExpr);
+
+  std::getline(ss, s);
+  pos = 0;
+  BOOST_CHECK_EQUAL(readToken(s, pos).value, 3);
+  BOOST_CHECK_EQUAL(readToken(s, pos).op, '*');
+  BOOST_CHECK_EQUAL(readToken(s, pos).value, 4);
+  BOOST_CHECK_EQUAL(readToken(s, pos).type, TokenType::EndOfExpr);
+}
+
+BOOST_AUTO_TEST_CASE(test_empty_line)
+{
+  std::stringstream ss("\n\n");
+  std::string s;
+  std::getline(ss, s);
+  size_t pos = 0;
+  BOOST_CHECK_EQUAL(readToken(s, pos).type, TokenType::EndOfExpr);
+  std::getline(ss, s);
+  pos = 0;
+  BOOST_CHECK_EQUAL(readToken(s, pos).type, TokenType::EndOfExpr);
+  std::getline(ss, s);
+  pos = 0;
+  BOOST_CHECK_EQUAL(readToken(s, pos).type, TokenType::EndOfExpr);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
