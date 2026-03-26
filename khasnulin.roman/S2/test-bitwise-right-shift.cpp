32f1dd77f0d4db8ea6848032f9733c8429b33a47
Spider-has <romanh998@gmail.com>
2026-03-26 23:15:33 +0000

    add test cases for large expressions with bitwise right shift

--- khasnulin.roman/S2/test-bitwise-right-shift.cpp
+++ khasnulin.roman/S2/test-bitwise-right-shift.cpp
@@ -30,3 +30,39 @@ BOOST_AUTO_TEST_CASE(test_large_shift_count_negative)
 }
 
 BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(bitwise_right_shift_in_expressions_suite)
+
+using namespace khasnulin;
+
+BOOST_AUTO_TEST_CASE(test_shift_priority_lower_than_add)
+{
+  BOOST_CHECK_EQUAL(readAndProcessExpressionLine("2 + 2 >> 2"), 1);
+}
+
+BOOST_AUTO_TEST_CASE(test_long_chain_precedence)
+{
+  BOOST_CHECK_EQUAL(readAndProcessExpressionLine("10 + 5 * 2 >> 3 + 1"), 1);
+}
+
+BOOST_AUTO_TEST_CASE(test_shift_associativity)
+{
+  BOOST_CHECK_EQUAL(readAndProcessExpressionLine("64 >> 2 >> 1"), 8);
+}
+
+BOOST_AUTO_TEST_CASE(test_modulo_and_shift)
+{
+  BOOST_CHECK_EQUAL(readAndProcessExpressionLine("100 % 30 >> 2"), 2);
+}
+
+BOOST_AUTO_TEST_CASE(test_ultimate_chain)
+{
+  BOOST_CHECK_EQUAL(readAndProcessExpressionLine("2 * ( 3 + 5 ) >> 1 + 1 % 2"), 4);
+}
+
+BOOST_AUTO_TEST_CASE(test_ultimate_chain_bitwise_in_parents)
+{
+  BOOST_CHECK_EQUAL(readAndProcessExpressionLine("2 * ( 3 + 5 * 3 >> 2 * 1 )"), 8);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
