3fc42caf9d0c52728e313a19d805307d613c8d43
aaaa24 <chernov.arseniy@yandex.ru>
2026-03-26 10:32:44 +0300

    add math operations

---
+++ chernov.arseniy/S2/math_funcs.cpp
@@ -0,0 +1,90 @@
+#include "math_funcs.hpp"
+
+#include <stdexcept>
+
+long long chernov::abs(long long a)
+{
+  if (a == MIN_LLI) {
+    throw std::overflow_error("overflow error");
+  } else if (a >= 0) {
+    return a;
+  } else {
+    return -a;
+  }
+}
+
+long long chernov::add(long long a, long long b)
+{
+  if (a > 0 && b > 0 && a > MAX_LLI - b) {
+    throw std::overflow_error("overflow error");
+  } else if (a < 0 && b < 0 && a < MIN_LLI - b) {
+    throw std::underflow_error("underflow error");
+  } else {
+    return a + b;
+  }
+}
+
+long long chernov::sub(long long a, long long b)
+{
+  if (a > 0 && b < 0 && a > MAX_LLI + b) {
+    throw std::overflow_error("overflow error");
+  } else if (a < 0 && b > 0 && a < MIN_LLI + b) {
+    throw std::underflow_error("underflow error");
+  } else {
+    return a - b;
+  }
+}
+
+long long chernov::mod(long long a, long long b)
+{
+  if (b == 0) {
+    throw std::logic_error("division by zero");
+  } else {
+    return a % b;
+  }
+}
+
+long long chernov::div(long long a, long long b)
+{
+  if (b == 0) {
+    throw std::logic_error("division by zero");
+  } else if (a == MIN_LLI && b == -1) {
+    throw std::overflow_error("overflow error");
+  } else {
+    return a / b;
+  }
+}
+
+long long chernov::mul(long long a, long long b)
+{
+  if (a == 0 || b == 0) {
+    return 0;
+  } else if ((a > 0 && b > 0 && a > MAX_LLI / b) || (a < 0 && b < 0 && a < MAX_LLI / b)) {
+    throw std::overflow_error("overflow_error");
+  } else if ((a > 0 && b < 0 && a > MIN_LLI / b) || (a < 0 && b > 0 && a < MIN_LLI / b)) {
+    throw std::underflow_error("underflow error");
+  } else {
+    return a * b;
+  }
+}
+
+long long chernov::gcd(long long a, long long b)
+{
+  a = abs(a);
+  b = abs(b);
+  while (a > 0 && b > 0) {
+    if (a > b) {
+      a = a % b;
+    } else {
+      b = b % a;
+    }
+  }
+  return a + b;
+}
+
+long long chernov::lcm(long long a, long long b)
+{
+  long long ab_gcd = gcd(a, b);
+  long long a_div = a / ab_gcd;
+  return mul(a_div, b);
+}
---
+++ chernov.arseniy/S2/math_funcs.hpp
@@ -0,0 +1,20 @@
+#ifndef MATH_FUNCS_HPP
+#define MATH_FUNCS_HPP
+
+#include <limits>
+
+namespace chernov {
+  constexpr long long MIN_LLI = std::numeric_limits< long long >::min();
+  constexpr long long MAX_LLI = std::numeric_limits< long long >::max();
+
+  long long abs(long long a);
+  long long add(long long a, long long b);
+  long long sub(long long a, long long b);
+  long long mod(long long a, long long b);
+  long long div(long long a, long long b);
+  long long mul(long long a, long long b);
+  long long gcd(long long a, long long b);
+  long long lcm(long long a, long long b);
+}
+
+#endif
