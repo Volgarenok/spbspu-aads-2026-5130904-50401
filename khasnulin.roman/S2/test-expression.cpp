80c40a5e85ca733a628c224e19181286d82262a9
Spider-has <romanh998@gmail.com>
2026-03-26 00:50:23 +0000

    add suite test cases for read all expressions func

--- khasnulin.roman/S2/test-expression.cpp
+++ khasnulin.roman/S2/test-expression.cpp
@@ -4,7 +4,7 @@
 
 #include "ExpressionProcess.hpp"
 
-BOOST_AUTO_TEST_SUITE(stack_test_suite)
+BOOST_AUTO_TEST_SUITE(expression_read_suite)
 
 using namespace khasnulin;
 
@@ -49,6 +49,49 @@ BOOST_AUTO_TEST_CASE(test_read_only_one_line)
   std::string remaining;
   std::getline(ss, remaining);
   BOOST_CHECK_EQUAL(remaining, "3 + 4");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(input_stream_reading_suite)
+
+using namespace khasnulin;
+
+BOOST_AUTO_TEST_CASE(test_expressions_order_inversion)
+{
+  std::stringstream ss("1 + 1\n2 + 2\n3 + 3");
+
+  Stack< Queue< Token > > allExprs = readAllExpressions(ss);
+
+  BOOST_REQUIRE_EQUAL(allExprs.size(), 3);
+
+  Queue< Token > lastExpr = allExprs.top();
+  BOOST_CHECK_EQUAL(lastExpr.front().value, 3);
+  allExprs.pop();
+
+  Queue< Token > middleExpr = allExprs.top();
+  BOOST_CHECK_EQUAL(middleExpr.front().value, 2);
+  allExprs.pop();
+
+  Queue< Token > firstExpr = allExprs.top();
+  BOOST_CHECK_EQUAL(firstExpr.front().value, 1);
+}
+
+BOOST_AUTO_TEST_CASE(test_skip_empty_lines)
+{
+  std::stringstream ss("1 + 1\n\n2 + 2");
+  Stack< Queue< Token > > allExprs = readAllExpressions(ss);
+
+  BOOST_CHECK_EQUAL(allExprs.size(), 2);
+}
+
+BOOST_AUTO_TEST_CASE(test_eof_handling)
+{
+  std::stringstream ss("1 + 1\n");
+  Stack< Queue< Token > > allExprs = readAllExpressions(ss);
+
+  BOOST_CHECK_EQUAL(allExprs.size(), 1);
+  BOOST_CHECK(!allExprs.empty());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
