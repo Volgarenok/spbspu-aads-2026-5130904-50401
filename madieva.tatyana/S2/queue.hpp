1c4b7544b6818fcb13e354d9ed504eeb99bb4f6c
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2026-03-26 21:08:28 +0300

    class queue, size

--- madieva.tatyana/S2/queue.hpp
+++ madieva.tatyana/S2/queue.hpp
@@ -4,69 +4,66 @@
 #include <stdexcept>
 namespace madieva {
   template< class T >
-  class Stack {
+  class Queue {
     List< T > data;
   public:
     void push(const T & val);
-    T & top();
-    const T & top() const;
+    T & front();
+    const T & front() const;
     size_t size() const noexcept;
     void pop();
     void clear() noexcept;
     bool empty() const noexcept;
   };
 
-
   template< class T >
-  void Stack< T >::push(const T & val)
+  void Queue< T >::push(const T & val)
   {
     data.push_back(val);
   }
 
   template< class T >
-  T & Stack< T >::top()
+  T & Queue< T >::front()
   {
     if (!empty()) {
       LIter< T > it = data.begin();
-      --it;
       return (*it);
     } else {
-      throw std::runtime_error("Stack is empty");
+      throw std::runtime_error("Queue is empty");
     }
   }
 
   template< class T >
-  const T & Stack< T >::top() const
+  const T & Queue< T >::front() const
   {
     if (!empty()) {
       LCIter< T > it = data.begin();
-      --it;
       return (*it);
     } else {
-      throw std::runtime_error("Stack is empty");
+      throw std::runtime_error("Queue is empty");
     }
   }
 
   template< class T >
-  size_t Stack< T >::size() const noexcept
+  size_t Queue< T >::size() const noexcept
   {
     return data.size();
   }
 
   template< class T >
-  void Stack< T >::pop()
+  void Queue< T >::pop()
   {
-    data.pop_back();
+    data.pop_front();
   }
 
   template< class T >
-  void Stack< T >::clear() noexcept
+  void Queue< T >::clear() noexcept
   {
     data.clear();
   }
 
   template< class T >
-  bool Stack< T >::empty() const noexcept
+  bool Queue< T >::empty() const noexcept
   {
     return data.size() == 0;
   }
