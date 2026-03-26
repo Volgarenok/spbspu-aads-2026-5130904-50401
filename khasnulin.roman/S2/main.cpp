bd2f425491863a9fd5413f260ef1c1f187e15279
Spider-has <romanh998@gmail.com>
2026-03-26 21:40:55 +0000

    add main function

--- khasnulin.roman/S2/main.cpp
+++ khasnulin.roman/S2/main.cpp
@@ -1,5 +1,51 @@
 
+#include <exception>
+#include <fstream>
+#include <iostream>
 
-int main()
+#include "ExpressionProcess.hpp"
+
+int main(int argc, char **argv)
 {
+  if (argc > 2)
+  {
+    std::cerr << "too many comand line params, must be zero or one!\n";
+    return 1;
+  }
+  khasnulin::Stack< int > answers;
+  try
+  {
+    if (argc == 2)
+    {
+      std::ifstream file(argv[1]);
+      if (!file.is_open())
+      {
+        std::cerr << "can't open file " << argv[1] << "\n";
+        return 1;
+      }
+      answers = khasnulin::readAndProcessAllExpressions(file);
+    }
+    else
+    {
+      answers = khasnulin::readAndProcessAllExpressions(std::cin);
+    }
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << "get error, while reading and processing expressions: " << e.what() << "\n";
+    return 1;
+  }
+  catch (...)
+  {
+    std::cerr << "get unknown error\n";
+    return 1;
+  }
+
+  while (!answers.empty())
+  {
+    std::cout << answers.top() << "\n";
+    answers.pop();
+  }
+
+  return 0;
 }
