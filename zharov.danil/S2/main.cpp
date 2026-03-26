f11628901dd42773114603dc4d16be51049fbe47
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-03-26 22:04:05 +0300

    Added input from file

--- zharov.danil/S2/main.cpp
+++ zharov.danil/S2/main.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "general_functions.hpp"
 
 using ll_t = long long;
 
-int main()
+int main(int argc, char ** argv)
 {
-  std::string line = "2 + 2 * 2";
   zharov::Stack< ll_t > results;
-  while (!std::getline(std::cin, line).eof()) {
-    try {
-      zharov::Queue< std::string> queue = zharov::getQueue(line);
-      queue = zharov::getPostfix(queue);
-      ll_t result = zharov::getResult(queue);
-      results.push(result);
-    } catch (const std::overflow_error & e) {
+  try {
+    if (argc > 1) {
+      std::ifstream d(argv[1]);
+      zharov::getResults(d, results);
+    } else {
+      zharov::getResults(std::cin, results);
+    }
+  } catch (const std::overflow_error & e) {
       std::cerr << e.what() << "\n";
       return 1;
     } catch (const std::logic_error & e) {
       std::cerr << e.what() << "\n";
       return 1;
     }
-  }
+  bool is_first = true;
   while (!results.empty()) {
-    std::cout << results.drop() << " ";
+    if (!is_first) {
+      std::cout << " ";
+    } else {
+      is_first = false;
+    }
+    std::cout << results.drop();
   }
   std::cout << "\n";
 }
