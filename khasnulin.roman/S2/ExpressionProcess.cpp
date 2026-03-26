44b0cfaaa6a48406a63b390341c7bb4ed384fa6f
Spider-has <romanh998@gmail.com>
2026-03-26 20:29:10 +0000

    add basic impl of process single Token function, all tests passed

--- khasnulin.roman/S2/ExpressionProcess.cpp
+++ khasnulin.roman/S2/ExpressionProcess.cpp
@@ -1,9 +1,39 @@
 #include "ExpressionProcess.hpp"
-#include "Queue.hpp"
-#include "Stack.hpp"
-#include "Token.hpp"
-#include <string>
-#include <utility>
+
+namespace
+{
+  int calculateBinaryOp(int v1, int v2, char op)
+  {
+    switch (op)
+    {
+    case '+':
+      return v2 + v1;
+    case '-':
+      return v2 - v1;
+    case '*':
+      return v2 * v1;
+    case '/':
+      return v2 / v1;
+    case '%':
+      return v2 % v1;
+    default:
+      return v2;
+    }
+  }
+
+  void makeSingleCalculation(khasnulin::Stack< khasnulin::Token > &values,
+                             khasnulin::Stack< khasnulin::Token > &operations)
+  {
+    char op = operations.top().op;
+    operations.pop();
+    int v1 = values.top().value;
+    values.pop();
+    int v2 = values.top().value;
+    values.pop();
+    int result = calculateBinaryOp(v1, v2, op);
+    values.push(khasnulin::Token{khasnulin::TokenType::Number, result, '0', 0});
+  }
+}
 
 khasnulin::Queue< khasnulin::Token > khasnulin::readExpression(std::istream &in)
 {
@@ -21,6 +51,27 @@ khasnulin::Queue< khasnulin::Token > khasnulin::readExpression(std::istream &in)
   return result;
 }
 
+int khasnulin::readAndProcessExpressionLine(const std::string &line)
+{
+  size_t pos = 0;
+  Stack< Token > values;
+  Stack< Token > operations;
+  Token lastToken = readToken(line, pos);
+
+  while (lastToken.type != TokenType::EndOfExpr)
+  {
+    processToken(lastToken, values, operations);
+    lastToken = readToken(line, pos);
+  }
+
+  while (!operations.empty())
+  {
+    makeSingleCalculation(values, operations);
+  }
+
+  return values.top().value;
+}
+
 khasnulin::Stack< khasnulin::Queue< khasnulin::Token > > khasnulin::readAllExpressions(std::istream &in)
 {
   Stack< Queue< Token > > result;
@@ -33,4 +84,32 @@ khasnulin::Stack< khasnulin::Queue< khasnulin::Token > > khasnulin::readAllExpre
     }
   }
   return result;
+}
+
+void khasnulin::processToken(Token &token, Stack< Token > &values, Stack< Token > &operations)
+{
+  if (token.type == TokenType::Number)
+  {
+    values.push(token);
+  }
+  if (token.type == TokenType::LeftParen)
+  {
+    operations.push(token);
+  }
+  else if (token.type == TokenType::Operator)
+  {
+    while (!operations.empty() && token.priority <= operations.top().priority)
+    {
+      makeSingleCalculation(values, operations);
+    }
+    operations.push(token);
+  }
+  else if (token.type == TokenType::RightParen)
+  {
+    while (!operations.empty() && operations.top().type != TokenType::LeftParen)
+    {
+      makeSingleCalculation(values, operations);
+    }
+    operations.pop();
+  }
 }
