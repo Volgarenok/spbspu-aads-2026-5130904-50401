28dcc7787053d58ba31ff18f66e29245ba4c8b29
kstudilova <kstudilova@gmail.com>
2026-03-27 01:24:03 +0300

    add basic main

--- studilova.ksenia/S2/main.cpp
+++ studilova.ksenia/S2/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <string>
+
+#include "math-expression.hpp"
+#include "stack.hpp"
 
 int main()
 {
+  studilova::Stack< long long > results;
+  std::string line;
+
+  while(std::getline(std::cin, line))
+  {
+    if (line.empty())
+    {
+      continue;
+    }
+    try
+    {
+      long long res = studilova::evaluateExpression(line);
+      results.push(res);
+    }
+    catch(const std::exception& e)
+    {
+      std::cerr << e.what() << '\n';
+      return 1;
+    }
+  }
   return 0;
 }
