07a2ee831692aa7db870294517d16315d376bd35
Spider-has <romanh998@gmail.com>
2026-03-26 00:50:06 +0000

    add interface of func that read all expr from input and convert them to stack of queue of token

--- khasnulin.roman/S2/ExpressionProcess.hpp
+++ khasnulin.roman/S2/ExpressionProcess.hpp
@@ -2,11 +2,14 @@
 #define EXPRESSION_PROCESS_HPP
 
 #include "Queue.hpp"
+#include "Stack.hpp"
 #include "Token.hpp"
 
 namespace khasnulin
 {
   Queue< Token > readExpression(std::istream &in);
+
+  Stack< Queue< Token > > readAllExpressions(std::istream &in);
 }
 
 #endif
