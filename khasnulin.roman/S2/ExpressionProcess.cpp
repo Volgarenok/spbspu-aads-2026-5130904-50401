6743237b165385addc0ab140c3fb343c7bb0704b
Spider-has <romanh998@gmail.com>
2026-03-26 00:50:45 +0000

    add read all expressions impl, all tests passed

--- khasnulin.roman/S2/ExpressionProcess.cpp
+++ khasnulin.roman/S2/ExpressionProcess.cpp
@@ -1,7 +1,9 @@
 #include "ExpressionProcess.hpp"
 #include "Queue.hpp"
+#include "Stack.hpp"
 #include "Token.hpp"
 #include <string>
+#include <utility>
 
 khasnulin::Queue< khasnulin::Token > khasnulin::readExpression(std::istream &in)
 {
@@ -15,6 +17,20 @@ khasnulin::Queue< khasnulin::Token > khasnulin::readExpression(std::istream &in)
   {
     result.push(lastToken);
     lastToken = readToken(line, pos);
+  }
+  return result;
+}
+
+khasnulin::Stack< khasnulin::Queue< khasnulin::Token > > khasnulin::readAllExpressions(std::istream &in)
+{
+  Stack< Queue< Token > > result;
+  while (!in.eof() && !in.fail())
+  {
+    Queue< Token > expression = readExpression(in);
+    if (!expression.empty())
+    {
+      result.push(std::move(expression));
+    }
   }
   return result;
 }
