e228b0cc184a1a4969b3d6ba94e387afbe5bfe68
PodySIN <daniil2007xboctob@gmail.com>
2026-03-29 15:25:37 +0300

    Add base of stack (constructors, operator=)

--- hvostov.daniil/S2/stack.hpp
+++ hvostov.daniil/S2/stack.hpp
@@ -27,5 +27,39 @@ namespace hvostov {
   };
 }
 
-#endif
+template< class T >
+hvostov::Stack< T >::Stack():
+  list_()
+{}
 
+template< class T >
+hvostov::Stack< T >::Stack(const Stack< T >& stack):
+  list_(stack.list_)
+{}
+
+template< class T >
+hvostov::Stack< T >::Stack(Stack< T >&& stack) noexcept:
+  list_(std::move(stack.list_))
+{}
+
+template< class T >
+hvostov::Stack< T >& hvostov::Stack< T >::operator=(const Stack< T >& stack)
+{
+  if (this == &stack) {
+    return *this;
+  }
+  list_ = stack.list_;
+  return *this;
+}
+
+template< class T >
+hvostov::Stack< T >& hvostov::Stack< T >::operator=(Stack< T >&& stack) noexcept
+{
+  if (this == &stack) {
+    return *this;
+  }
+  list_ = std::move(stack.list_);
+  return *this;
+}
+
+#endif
