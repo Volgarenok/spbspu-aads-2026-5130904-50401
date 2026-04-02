4602e2aa1b750921f3d88fb2e19458afa4e99482
Degr3e <glebyal2007@gmail.com>
2026-04-02 22:40:48 +0300

    handle empty input in main

--- yalovsky.gleb/S1/main.cpp
+++ yalovsky.gleb/S1/main.cpp
@@ -10,6 +10,13 @@ int main()
   try
   {
     yalovsky::readSequences(std::cin, sequences);
+
+    if (sequences.empty())
+    {
+      std::cout << "0\n";
+      return 0;
+    }
+    
     return 0;
   }
   catch (const std::exception& e)
