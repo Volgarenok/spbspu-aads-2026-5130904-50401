d73e3c3ebff16c6bed3dd16aac7c1f7fa67f40a0
Degr3e <glebyal2007@gmail.com>
2026-04-02 22:43:34 +0300

    finish main workflow

--- yalovsky.gleb/S1/main.cpp
+++ yalovsky.gleb/S1/main.cpp
@@ -16,12 +16,26 @@ int main()
       std::cout << "0\n";
       return 0;
     }
-    
-    return 0;
+
+    yalovsky::printNames(std::cout, sequences);
+
+    yalovsky::Matrix matrix;
+    yalovsky::transposeSequences(sequences, matrix);
+    yalovsky::printMatrix(std::cout, matrix);
+
+    yalovsky::NumberList sums;
+    yalovsky::calculateSums(matrix, sums);
+
+    if (!sums.empty())
+    {
+      yalovsky::printNumberList(std::cout, sums);
+    }
   }
   catch (const std::exception& e)
   {
     std::cerr << e.what() << '\n';
     return 1;
   }
+
+  return 0;
 }
