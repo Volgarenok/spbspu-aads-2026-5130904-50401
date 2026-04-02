62432005ef3a2c7a142f705a37b28d533b97aaf7
Degr3e <glebyal2007@gmail.com>
2026-04-02 22:27:55 +0300

    implement sequence input parsing

---
+++ yalovsky.gleb/S1/input-output.cpp
@@ -0,0 +1,43 @@
+#include "input-output.hpp"
+
+#include <algorithm>
+#include <istream>
+#include <limits>
+#include <ostream>
+#include <stdexcept>
+#include <utility>
+
+void yalovsky::readSequences(std::istream& in, SequenceList& sequences)
+{
+  while (true)
+  {
+    std::string name;
+    if (!(in >> name))
+    {
+      return;
+    }
+
+    NumberList numbers;
+
+    while (true)
+    {
+      std::size_t value = 0;
+      if (in >> value)
+      {
+        numbers.pushBack(value);
+      }
+      else
+      {
+        if (in.eof())
+        {
+          sequences.pushBack(std::make_pair(name, std::move(numbers)));
+          return;
+        }
+        in.clear();
+        break;
+      }
+    }
+
+    sequences.pushBack(std::make_pair(name, std::move(numbers)));
+  }
+}
