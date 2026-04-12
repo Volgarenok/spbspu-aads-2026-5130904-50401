d0fad79ab4e03a5cee0c613ccde0920a84e0c09c
grisha-git <donkeevgrisa@gmail.com>
2026-04-13 00:21:36 +0300

    Implementation stack

--- donkeev.grigoriy/S2/stack.hpp
+++ donkeev.grigoriy/S2/stack.hpp
@@ -26,5 +26,65 @@ namespace donkeev
 
     ~Queue() = default;
   };
+
+  template< class T >
+  Stack< T >::Stack(std::initializer_list< T > il):
+    list_()
+  {
+    for (auto it = il.begin(); it != il.end(); ++it)
+    {
+      list_.popFront(*it);
+    }
+  }
+
+  template< class T >
+  void Stack< T >::push(const T& value)
+  {
+    list_.pushFront(value);
+  }
+  template< class T >
+  void Stack< T >::pop()
+  {
+    if (list_.isEmpty())
+    {
+      throw std::underflow_error("Empty queue");
+    }
+    list_.popFront();
+  }
+
+  template< class T >
+  T& Stack< T >::top()
+  {
+    if (list_.isEmpty())
+    {
+      throw std::underflow_error("Empty queue");
+    }
+    return *list_.begin;
+  }
+  template< class T >
+  const T& Stack< T >::top() const
+  {
+    if (list_.isEmpty())
+    {
+      throw std::underflow_error("Empty queue");
+    }
+    return *list_.cbegin;
+  }
+
+  template< class T >
+  bool Stack< T >::isEmpty() const
+  {
+    return list_.isEmpty();
+  }
+  template< class T >
+  size_t Stack< T >::size() const
+  {
+    return list_.size();
+  }
+  template< class T >
+  void Stack< T >::clear()
+  {
+    return list_.clearAll();
+  }
 }
 #endif
