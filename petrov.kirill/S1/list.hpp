306920ee5ff0b2e0786b03b63737007d3a067b36
kirill <kirill@kirill.kirill>
2026-04-12 22:47:05 +0300

    list

--- petrov.kirill/S1/list.hpp
+++ petrov.kirill/S1/list.hpp
@@ -26,6 +26,11 @@ namespace petrov
 		
 		List<T>& operator=(List<T>&& l) noexcept;
 		List<T>& operator=(const List<T>& l);
+
+		bool IsEmpty() const noexcept;
+		size_t getSize() const noexcept;
+		ListIterator<T> begin();
+		ListIterator<T> end();
 	};
 	template<class T>
 	class ListIterator
@@ -38,17 +43,22 @@ namespace petrov
 		bool operator!=(const ListIterator<T>& i) const;
 		T& operator*() const;
 		ListIterator<T>& operator++();
-		ListIterator<T>& operator+(size_t s);
 		ListIterator<T>& operator--();
-		ListIterator<T>& operator-(size_t s);
-
-		ListIterator<T> const_begin() const;
-		ListIterator<T> const_end() const;
-		ListIterator<T> begin();
-		ListIterator<T> end();
-		
-		bool IsEmpty() const noexcept;
-		size_t getSize() const noexcept;
+		T* operator->() const;
+	};
+	template<class T>
+	class ListIteratorConst
+	{
+		friend class List<T>;
+		const node<T>* cur;
+		ListIteratorConst(const node<T>* n);
+	public:
+		bool operator==(const ListIteratorConst<T>& i) const;
+		bool operator!=(const ListIteratorConst<T>& i) const;
+		const T& operator*() const;
+		ListIteratorConst<T>& operator++();
+		ListIteratorConst<T>& operator--();
+		const T* operator->() const;
 	};
 };
 #endif
