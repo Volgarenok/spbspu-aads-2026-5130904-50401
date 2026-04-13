c2dfe646e8d0fffc3eb1039c31e316224fd0e058
kirill <kirill@kirill.kirill>
2026-04-13 04:27:05 +0300

    github mobile thanks for tabs

--- petrov.kirill/S1/list.hpp
+++ petrov.kirill/S1/list.hpp
@@ -1,5 +1,6 @@
 #ifndef LIST_HPP
 #define LIST_HPP
+#include <cstddef>
 
 namespace petrov
 {
@@ -10,6 +11,12 @@ namespace petrov
 		node<T>*	next;
 		node<T>*	prev;
 	};
+	
+	template<class	T>
+	class ListIterator;
+	
+	template<class T>
+	class ListIteratorConst;
 	
 	template<class T>
 	class List
@@ -31,7 +38,9 @@ namespace petrov
 		size_t getSize() const noexcept;
 		ListIterator<T> begin();
 		ListIterator<T> end();
-		
+		ListIteratorConst<T> begin() const;
+		ListIteratorConst<T> end() const;
+
 		void push_back(const T& d);
 		void push_front(const T& d);
 		void pop_back();
@@ -67,309 +76,321 @@ namespace petrov
 		ListIteratorConst<T>& operator--();
 		const T* operator->() const;
 	};
-};
-
-template<class T>
-	List<T>::List():
-	h(nullptr),
-	t(nullptr),
-	s(0)
-{}
-
-template<class T>
-bool ListIteratorConst<T>::operator==(const ListIteratorConst<T>& i) const
-{
-	!(return cur != i.cur);
-}
-
-template<class T>
-bool ListIteratorConst<T>::operator!=(const ListIteratorConst<T>& i) const
-{
-	return cur != i.cur;
-}
-
-template<class T>
-const T& ListIteratorConst<T>::operator*() const
-{
-	return cur->val;
-}
-
-template<class T>
-const T* ListIteratorConst<T>::operator->() const
-{
-	return &(cur->val);
-}
-
-template<class T>
-ListIteratorConst<T>& ListIteratorConst<T>::operator++()
-{
-	if (cur != nullptr)
+	
+	template<class T>
+		List<T>::List():
+		h(nullptr),
+		t(nullptr),
+		s(0)
+	{}
+	
+	template<class T>
+	bool ListIteratorConst<T>::operator==(const ListIteratorConst<T>& i) const
 	{
-		cur = cur->next;
+		return !(cur != i.cur);
 	}
-	return *this;
-}
-
-template<class T>
-ListIteratorConst<T>& ListIteratorConst<T>::operator--()
-{
-	if (cur != nullptr)
+	
+	template<class T>
+	bool ListIteratorConst<T>::operator!=(const ListIteratorConst<T>& i) const
 	{
-		cur = cur->prev;
+		return cur != i.cur;
 	}
-	return *this;
-}
-
-template<class T>
-bool ListIterator<T>::operator==(const ListIterator<T>& i) const
-{
-	return !(cur != i.cur);
-}
-
-template<class T>
-bool ListIterator<T>::operator!=(const ListIterator<T>& i) const
-{
-	return cur != i.cur;
-}
-
-template<class T>
-ListIterator<T>::ListIterator(node<T>* n):
+	
+	template<class T>
+	const T& ListIteratorConst<T>::operator*() const
+	{
+		return cur->val;
+	}
+	
+	template<class T>
+	const T* ListIteratorConst<T>::operator->() const
+	{
+		return &(cur->val);
+	}
+	
+	template<class T>
+	ListIteratorConst<T>& ListIteratorConst<T>::operator++()
+	{
+		if (cur != nullptr)
+		{
+			cur = cur->next;
+		}
+		return *this;
+	}
+	
+	template<class T>
+	ListIteratorConst<T>& ListIteratorConst<T>::operator--()
+	{
+		if (cur != nullptr)
+		{
+			cur = cur->prev;
+		}
+		return *this;
+	}
+	
+	template<class T>
+	bool ListIterator<T>::operator==(const ListIterator<T>& i) const
+	{
+		return !(cur != i.cur);
+	}
+	
+	template<class T>
+	bool ListIterator<T>::operator!=(const ListIterator<T>& i) const
+	{
+		return cur != i.cur;
+	}
+	
+	template<class T>
+	ListIterator<T>::ListIterator(node<T>* n):
+		cur(n)
+	{}
+	
+	template<class T>
+	ListIteratorConst<T>::ListIteratorConst(const node<T>* n):
 	cur(n)
-{}
-
-template<class T>
-ListIteratorConst<T>::ListIteratorConst(const node<T>* n):
-cur(n)
-{}
-
-template<class T>
-void List<T>::clear()
-{
-	if (h != nullpt)
+	{}
+	
+	template<class T>
+	void List<T>::clear()
 	{
-		while (h->next != nullptr)
+		if (h != nullptr)
 		{
-			node<T>*	promej = h->next;
+			while (h->next != nullptr)
+			{
+				node<T>*	promej = h->next;
+				delete h;
+				h = promej;
+			}
 			delete h;
-			h = promej;
 		}
-		delete h;
+		h = nullptr;
+		t = nullptr;
+		s = 0;
+		return;
 	}
-	h = nullptr;
-	t = nullptr;
-	s = 0;
-	return;
-}
-
-template<class T>
-List<T>::~List()
-{
-	clear();
-}
-
-template<class T>
-void List<T>::pop_front()
-{
-	if (!IsEmpty())
+	
+	template<class T>
+	List<T>::~List()
 	{
-		node<T>*	new_h = h->next;
-		delete h;
-		h = new_h;
-		if (h == nullptr)
+		clear();
+	}
+	
+	template<class T>
+	void List<T>::pop_front()
+	{
+		if (!IsEmpty())
 		{
-			t = nullptr;
-			s = 0; 
+			node<T>*	new_h = h->next;
+			delete h;
+			h = new_h;
+			if (h == nullptr)
+			{
+				t = nullptr;
+				s = 0; 
+				return;
+			}
+			h->prev = nullptr;
+			s--;
+		}
+	}
+	
+	template<class T>
+	void List<T>::pop_back()
+	{
+		if (!IsEmpty())
+		{
+			node<T>*	new_t = t->prev;
+			delete t;
+			t = new_t;
+			if (t == nullptr)
+			{
+				h = nullptr;
+				s = 0;
+				return;
+			}
+			t->next = nullptr;
+			s--;
+		}
+	}
+	
+	template<class	T>
+	bool List<T>::IsEmpty() const noexcept
+	{
+		return (!s || h == nullptr);
+	}
+	
+	template<class T>
+	size_t List<T>::getSize() const noexcept
+	{
+		return s;
+	}
+	
+	template<class T>
+	void List<T>::push_back(const T& d)
+	{
+		if (IsEmpty())
+		{
+			h = new node<T>;
+			h->val = d;
+			h->next = nullptr;
+			h->prev = nullptr;
+			t = h;
+			s = 1;
 			return;
 		}
-		h->prev = nullptr;
-		s--;
-	}
-}
-
-template<class T>
-void List<T>::pop_back()
-{
-	if (!IsEmpty())
-	{
-		node<T>*	new_t = t->prev;
-		delete t;
-		t = new_t;
-		if (t == nullptr)
-		{
-			h = nullptr;
-			s = 0;
-			return;
-		}
+		t->next = new node<T>;
+		t->next->val = d;
+		t->next->prev = t;
+		t = t->next;
 		t->next = nullptr;
-		s--;
+		s++;
 	}
-}
-
-template<class	T>
-bool List<T>::IsEmpty() const noexcept
-{
-	return (!s || h == nullptr);
-}
-
-template<class T>
-size_t List<T>::getSize() const noexcept
-{
-	return s;
-}
-
-template<class T>
-void List<T>::push_back(const T& d)
-{
-	if (IsEmpty())
+	
+	template<class T>
+	void List<T>::push_front(const T& d)
 	{
-		h = new node<T>;
-		h->val = d;
-		h->next = nullptr;
-		h->prev = nullptr;
-		t = h;
-		s = 1;
-		return;
-	}
-	t->next = new node<T>;
-	t->next->val = d;
-	t->next->prev = t;
-	t = t->next;
-	t->next = nullptr;
-	s++;
-}
-
-template<class T>
-void List<T>::push_front(const T& d)
-{
-	if (IsEmpty())
-	{
-		h = new node<T>;
-		h->val = d;
-		h->next = nullptr;
-		h->prev = nullptr;
-		t = h;
-		s = 1;
-		return;
-	}
-	h->prev = new node<T>;
-	h->prev->val = d;
-	h->prev->next = h;
-	h = h->prev;
-	h->prev = nullptr;
-	s++;
-}
-
-template<class T>
-ListIterator<T> List<T>::begin()
-{
-	return ListIterator(h);
-}
-
-template<class T>
-ListIterator<T> List<T>::end()
-{
-	return ListIterator(nullptr);
-}
-
-template<class T>
-ListIterator<T>& ListIterator<T>::operator++()
-{
-	if (cur != nullptr)
-	{
-		cur = cur->next;
-	}
-	return *this;
-}
-
-template<class T>
-ListIterator<T>& ListIterator<T>::operator--()
-{
-	if (cur != nullptr)
-	{
-		cur = cur->prev;
-	}
-	return *this;
-}
-
-template<class T>
-T&	ListIterator<T>::operator*() const
-{
-	return cur->val;
-}
-
-template<class T>
-T* ListIterator<T>::operator->() const
-{
-	return &(cur->val);
-}
-
-template<class T>
-List<T>::List(const List<T>& l):
-	h(nullptr)
-	t(nullptr),
-	s(0)
-{
-	node<T>*	nod = l.h;
-	while (nod != nullptr)
-	{
-		push_back(nod->val);
-		nod = nod->next;
-	}
-}
-
-template<class T>
-List<T>& List<T>::operator=(const List<T>& l)
-{
-	if (*this != l)
-	{
-		clear();
-		node<T>* now = l.h;
-		while (now != nullptr)
+		if (IsEmpty())
 		{
-			push_back(now->val);
-			now = now->next;
+			h = new node<T>;
+			h->val = d;
+			h->next = nullptr;
+			h->prev = nullptr;
+			t = h;
+			s = 1;
+			return;
+		}
+		h->prev = new node<T>;
+		h->prev->val = d;
+		h->prev->next = h;
+		h = h->prev;
+		h->prev = nullptr;
+		s++;
+	}
+	
+	template<class T>
+	ListIterator<T> List<T>::begin()
+	{
+		return ListIterator(h);
+	}
+	
+	template<class T>
+	ListIterator<T> List<T>::end()
+	{
+		return ListIterator<T>(nullptr);
+	}
+	
+	template<class T>
+	ListIterator<T>& ListIterator<T>::operator++()
+	{
+		if (cur != nullptr)
+		{
+			cur = cur->next;
+		}
+		return *this;
+	}
+	
+	template<class T>
+	ListIterator<T>& ListIterator<T>::operator--()
+	{
+		if (cur != nullptr)
+		{
+			cur = cur->prev;
+		}
+		return *this;
+	}
+	
+	template<class T>
+	T&	ListIterator<T>::operator*() const
+	{
+		return cur->val;
+	}
+	
+	template<class T>
+	T* ListIterator<T>::operator->() const
+	{
+		return &(cur->val);
+	}
+	
+	template<class T>
+	List<T>::List(const List<T>& l):
+		h(nullptr),
+		t(nullptr),
+		s(0)
+	{
+		node<T>*	nod = l.h;
+		while (nod != nullptr)
+		{
+			push_back(nod->val);
+			nod = nod->next;
 		}
 	}
-	return *this;
-}
-
-template<class T>
-List<T>::List(List<T>&& l) noexcept:
-	h(l.h),
-	t(l.t),
-	s(l.s)
-{
-	l.h = nulltpr;
-	l.t = nullptr;
-	l.s = 0;
-}
-
-template<class T>
-List<T>& List<T>::operator=(List<T>&& l) noexcept
-{
-	if (*this != l)
+	
+	template<class T>
+	List<T>& List<T>::operator=(const List<T>& l)
 	{
-		clear();
-		h = l.h;
-		s = l.s;
-		t = l.t;
+		if (this != &l)
+		{
+			clear();
+			node<T>* now = l.h;
+			while (now != nullptr)
+			{
+				push_back(now->val);
+				now = now->next;
+			}
+		}
+		return *this;
+	}
+	
+	template<class T>
+	List<T>::List(List<T>&& l) noexcept:
+		h(l.h),
+		t(l.t),
+		s(l.s)
+	{
 		l.h = nullptr;
 		l.t = nullptr;
 		l.s = 0;
 	}
-	return *this;
-}
-
-template<class T>
-List<T>::List(size_t s, const T& init):
-	h(nullptr),
-	t(nullptr),
-	s(0)
-{
-	for (size_t i = 0; i <	s; ++i)
+	
+	template<class T>
+	List<T>& List<T>::operator=(List<T>&& l) noexcept
 	{
-		push_back(init);
+		if (this != &l)
+		{
+			clear();
+			h = l.h;
+			s = l.s;
+			t = l.t;
+			l.h = nullptr;
+			l.t = nullptr;
+			l.s = 0;
+		}
+		return *this;
 	}
-}
+	
+	template<class T>
+	List<T>::List(size_t s, const T& init):
+		h(nullptr),
+		t(nullptr),
+		s(0)
+	{
+		for (size_t i = 0; i <	s; ++i)
+		{
+			push_back(init);
+		}
+	}
+	
+	template<class T>
+	ListIteratorConst<T> List<T>::begin() const
+	{
+		return ListIteratorConst<T>(h);
+	}
+	
+	template<class T>
+	ListIteratorConst<T> List<T>::end() const
+	{
+		return ListIteratorConst<T>(nullptr);
+	}
+};
 
 #endif
