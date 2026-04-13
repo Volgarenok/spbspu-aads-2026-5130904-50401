78ea77bab93da801460143f8e1898c801fe09200
kirill <kirill@kirill.kirill>
2026-04-13 04:45:59 +0300

    tests

--- petrov.kirill/S1/test-list.cpp
+++ petrov.kirill/S1/test-list.cpp
@@ -1,84 +1,16 @@
-#define BOOST_TEST_MODULE ListTest
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 #include "list.hpp"
 
-BOOST_AUTO_TEST_SUITE(ConstructionSuite)
+BOOST_AUTO_TEST_SUITE(Petrov_List_Test_Suite)
 
-BOOST_AUTO_TEST_CASE(DefaultConstructor)
+BOOST_AUTO_TEST_CASE(test_init)
 {
-  petrov::List< int > list;
-  BOOST_CHECK(list.isEmpty());
-  BOOST_CHECK_EQUAL(list.getSize(), 0);
-}
-
-BOOST_AUTO_TEST_CASE(TestEmptyList)
-{
-  petrov::List< int > list;
-  BOOST_CHECK(list.isEmpty());
-  BOOST_CHECK_EQUAL(list.getSize(), 0);
-  BOOST_CHECK(list.begin() == list.end());
-}
-
-BOOST_AUTO_TEST_CASE(TestPushBack)
-{
-  petrov::List< int > list;
-  list.pushBack(-1);
-  list.pushBack(-33);
-  list.pushBack(27);
-  list.pushBack(77);
-  list.pushBack(9);
-
-  BOOST_CHECK(!list.isEmpty());
-  BOOST_CHECK_EQUAL(list.getSize(), 5);
-  BOOST_CHECK_EQUAL(list.getBack(), 9);
-}
-
-BOOST_AUTO_TEST_CASE(TestClear)
-{
-  petrov::List< int > list;
-  list.pushBack(0);
-  list.pushBack(1);
-  list.pushBack(-1);
-  list.clear();
-
-  BOOST_CHECK(list.isEmpty());
-  BOOST_CHECK_EQUAL(list.getSize(), 0);
-  BOOST_CHECK(list.begin() == list.end());
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(ListIteratorTests)
-
-BOOST_AUTO_TEST_CASE(TestIncrement)
-{
-  petrov::List< int > list;
-  list.pushBack(1);
-  list.pushBack(2);
-
-  petrov::List< int >::Iterator i = list.begin();
-
-  BOOST_REQUIRE(i != list.end());
-  BOOST_CHECK_EQUAL(*i, 1);
-
-  ++i;
-  BOOST_REQUIRE(i != list.end());
-  BOOST_CHECK_EQUAL(*i, 2);
-
-  ++i;
-  BOOST_CHECK(i == list.end());
-}
-
-BOOST_AUTO_TEST_CASE(TestConstIterator)
-{
-  petrov::List< int > list;
-  list.pushBack(1);
-
-  const petrov::List< int > & const_list = list;
-
-  petrov::List< int >::ConstIterator i = const_list.begin();
-
-  BOOST_CHECK_EQUAL(*i, 1);
+	Petrov::List<size_t> empt_l;
+	BOOST_CHECK(empt_l.IsEmpty());
+	BOOST_CHECK_EQUAL(empt_l.getSize(), 0);
+	
+	Petrov::List<size_t> unempt_l(2, 5);
+	BOOST_CHECK_EQUAL(*unempt_l.begin(), 100);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
