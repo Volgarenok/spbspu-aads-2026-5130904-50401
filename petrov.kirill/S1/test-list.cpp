b5a1d777039db924e81428ed24eb7327f8530ab2
kirill <kirillpetrov56333@gmail.com>
2026-04-17 01:48:25 +0300

    tests

--- petrov.kirill/S1/test-list.cpp
+++ petrov.kirill/S1/test-list.cpp
@@ -39,4 +39,26 @@ BOOST_AUTO_TEST_CASE(test_push_front)
   BOOST_CHECK_EQUAL(*(++i), 73);
 }
 
+BOOST_AUTO_TEST_CASE(test_iter_equal)
+{
+  petrov::List<int> c;
+  c.push_back(11);
+
+  petrov::LIter<int> i = c.begin();
+  petrov::LIter<int> i2 = c.begin();
+  BOOST_CHECK(i == i2);
+}
+
+BOOST_AUTO_TEST_CASE(test_iter_not_eq)
+{
+  petrov::List<int> c;
+  c.push_back(11);
+  c.push_back(73);
+
+  petrov::LIter<int> i1 = c.begin();
+  petrov::LIter<int> i2 = c.begin();
+  ++i2;
+  BOOST_CHECK(i1 != i2);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
