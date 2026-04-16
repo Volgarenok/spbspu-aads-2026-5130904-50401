d5fa8b6498a6db95ac1d87c2afe05628a28d7929
kirill <kirillpetrov56333@gmail.com>
2026-04-17 02:51:13 +0300

    tests

--- petrov.kirill/S1/test-list.cpp
+++ petrov.kirill/S1/test-list.cpp
@@ -81,4 +81,70 @@ BOOST_AUTO_TEST_CASE(test_nullptr)
   BOOST_CHECK(c.end() == nullptr);
 }
 
+BOOST_AUTO_TEST_CASE(test_copy_constructor)
+{
+  petrov::List<int> i1;
+  i1.push_back(10);
+  i1.push_back(20);
+
+  petrov::List<int> i2(i1);
+  BOOST_CHECK_EQUAL(i1.IsEmpty(), i2.IsEmpty());
+
+  petrov::LIter<int> l1 = i1.begin();
+  petrov::LIter<int> l2 = i2.begin();
+  BOOST_CHECK_EQUAL(*l1, *l2);;
+}
+
+BOOST_AUTO_TEST_CASE(test_copy_assignment)
+{
+  petrov::List<int> l1;
+  l1.push_back(1);
+  petrov::List<int> l2;
+  l2.push_back(2);
+
+  l2 = l1;
+  BOOST_CHECK_EQUAL(*l2.begin(), 1);
+
+  l1.clear();
+  BOOST_CHECK(!l2.IsEmpty());
+  BOOST_CHECK_EQUAL(*l2.begin(), 1);
+}
+
+BOOST_AUTO_TEST_CASE(test_move_semantics)
+{
+  petrov::List<int> l1;
+  l1.push_back(100);
+  l1.push_back(200);
+
+  petrov::List<int> l2(std::move(l1));
+  BOOST_CHECK(l1.IsEmpty());
+  BOOST_CHECK_EQUAL(*l2.begin(), 100);
+
+  petrov::LIter<int> i = l2.begin();
+  BOOST_CHECK_EQUAL(*(++i), 200);
+}
+
+BOOST_AUTO_TEST_CASE(test_string_list)
+{
+  petrov::List<std::string> l;
+  l.push_back("ab");
+  l.push_back("ba");
+
+  petrov::LIter<std::string> i = l.begin();
+  BOOST_CHECK_EQUAL(*i, "ab");
+  BOOST_CHECK_EQUAL(*(++i), "ba");
+}
+
+BOOST_AUTO_TEST_CASE(test_pop_back)
+{
+  petrov::List<int> l;
+  l.push_back(1);
+  l.push_back(2);
+  l.pop_back();
+
+  BOOST_CHECK_EQUAL(*l.begin(), 1);
+  petrov::LIter<int> it = l.begin();
+  BOOST_CHECK(++it == l.end());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
