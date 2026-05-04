bab5ae8b7b28389436fbc170e9ca08793cfb1970
yahada <niadzy14@gmail.com>
2026-05-04 23:39:43 +0300

    add realisations to some other funcs. Added Hash struct

--- malashenko.dmitrii/S3/hash_table.hpp
+++ malashenko.dmitrii/S3/hash_table.hpp
@@ -1,11 +1,18 @@
 #ifndef HASH_TABLE_HPP
 #define HASH_TABLE_HPP
 #include <common/list/list.hpp>
+#include <openssl/hmac.h>
 namespace malashenko
 {
+
+  template< class T >
+  class Hash {
+    size_t operator()(const T& key);
+  };
+
   template< class T >
   class Equal {
-    bool operator(const T& lhs, const T& rhs);
+    bool operator()(const T& lhs, const T& rhs);
   };
 
 
@@ -13,6 +20,7 @@ namespace malashenko
   class HashTable {
   public:
     HashTable();
+    HashTable(const size_t& size);
     HashTable(const HashTable< Key, Value, Hash, Equal >& rhs);
     HashTable(HashTable< Key, Value, Hash, Equal >&& rhs);
     HashTable< Key, Value, Hash, Equal >& operator=(const HashTable< Key, Value, Hash, Equal >& rhs);
@@ -27,12 +35,12 @@ namespace malashenko
     Value operator[](const Key& k);
     const Value operator[](const Key& k) const;
 
-    void rehash(const size_t& slots);
+    void rehash(const size_t& newSize);
     size_t size() const noexcept;
     void swap(HashTable< Key, Value, Hash, Equal >& rhs)
   private:
-    size_t k;
-    List< Key >* slots[];
+    size_t k_;
+    List< Key >* slots_[];
   };
 
   template< class Key, class Value, class Hash, class Equal >
@@ -46,17 +54,61 @@ namespace malashenko
   }
 
   template< class Key, class Value, class Hash, class Equal >
-  HashTable< class Key, class Value, class Hash, class Equal >::HashTable()
+  HashTable< class Key, class Value, class Hash, class Equal >::HashTable():
+    slots_(new List< Key >[1]),
+    k_(1)
+  {}
+
+  template< class Key, class Value, class Hash, class Equal >
+  HashTable< class Key, class Value, class Hash, class Equal >::HashTable(const size_t& size):
+    slots_(size ? List< Key >[size] : nullptr),
+    k_(size)
+  {}
+
+  template< class Key, class Value, class Hash, class Equal >
+  HashTable< class Key, class Value, class Hash, class Equal >::HashTable(const HashTable< Key, Value, Hash, Equal >& rhs)
   {
-    slots = new List< Key >[8];
-    k = 8;
+    if (this == std::addressof(rhs))
+    {
+      return *this;
+    }
+    HashTable tmp(rhs);
+    swap(tmp);
+    return *this;
   }
 
+  template< class Key, class Value, class Hash, class Equal >
+  HashTable< class Key, class Value, class Hash, class Equal >::HashTable(HashTable< Key, Value, Hash, Equal >&& rhs)
+  {
+    if (this == std::addressof(rhs))
+    {
+      return *this;
+    }
+    HashTable tmp(std::move(rhs));
+    swap(tmp);
+    return *this;
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  void HashTable< class Key, class Value, class Hash, class Equal >::swap(HashTable< Key, Value, Hash, Equal >& rhs)
+  {
+    using std::swap;
+    std::swap(slots_, rhs.slots_);
+    std::swap(k_, rhs.k_);
+  }
 
   template< class Key, class Value, class Hash, class Equal >
   size_t HashTable< class Key, class Value, class Hash, class Equal >::size() const noexcept
   {
     return k;
+  }
+
+
+
+  template< class T >
+  bool Equal< T >::operator()(const T& lhs, const T& rhs)
+  {
+    return lhs == rhs;
   }
 }
 
