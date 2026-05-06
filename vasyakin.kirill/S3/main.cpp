b683fa6001cd22a41983503e5c3feb40fce020a7
potomnazovy <skeleton39876@mail.ru>
2026-05-06 23:16:07 +0300

    complete main.cpp

--- vasyakin.kirill/S3/main.cpp
+++ vasyakin.kirill/S3/main.cpp
@@ -18,6 +18,68 @@ int main(int argc, char* argv[])
     std::cerr << "Error: cannot open file '" << argv[1] << "'\n";
     return 1;
   }
+  
+  vasyakin::GraphsMap graphs(16);
+  std::string graph_name;
+  size_t edge_count = 0;
+
+  while (file >> graph_name >> edge_count)
+  {
+    graphs.add(graph_name, vasyakin::Graph{});
+
+    for (size_t i = 0; i < edge_count; ++i)
+    {
+      std::string u, v;
+      size_t w = 0;
+      file >> u >> v >> w;
+
+      auto& g = graphs.get(graph_name);
+      if (g.adj.has(u))
+      {
+        g.adj.get(u).push_back({v, w});
+      }
+      else
+      {
+        vasyakin::List< vasyakin::Edge > edges;
+        edges.push_back({v, w});
+        g.adj.add(u, std::move(edges));
+      }
+    }
+  }
+  file.close();
+
+  using cmd_t = void(*)(std::istream&, std::ostream&, vasyakin::GraphsMap&);
+  vasyakin::HashTable< std::string, cmd_t, vasyakin::HMACHash, std::equal_to< std::string > > cmds(9);
+
+  cmds.add("graphs", vasyakin::graphs);
+  cmds.add("vertexes", vasyakin::vertexes);
+  cmds.add("outbound", vasyakin::outbound);
+  cmds.add("inbound", vasyakin::inbound);
+  cmds.add("bind", vasyakin::bind);
+  cmds.add("cut", vasyakin::cut);
+  cmds.add("create", vasyakin::create);
+  cmds.add("merge", vasyakin::merge);
+  cmds.add("extract", vasyakin::extract);
+
+  
+  std::string cmd;
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      cmds.get(cmd)(std::cin, std::cout, graphs);
+    }
+    catch (...)
+    {
+      std::cout << "<INVALID COMMAND>" << '\n';
+    }
+
+    if (!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
 
   return 0;
 }
