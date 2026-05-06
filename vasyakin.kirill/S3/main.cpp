719b5f461488aef0c12e85725affdee8c3b5bd7d
potomnazovy <skeleton39876@mail.ru>
2026-05-06 23:15:16 +0300

    check main argument

--- vasyakin.kirill/S3/main.cpp
+++ vasyakin.kirill/S3/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
+#include "commands.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
+  if (argc != 2)
+  {
+    std::cerr << "Usage: " << argv[0] << " <filename>\n";
+    return 1;
+  }
+
+  std::ifstream file(argv[1]);
+  if (!file.is_open())
+  {
+    std::cerr << "Error: cannot open file '" << argv[1] << "'\n";
+    return 1;
+  }
 
   return 0;
 }
