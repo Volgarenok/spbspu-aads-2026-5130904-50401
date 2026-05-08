8fe021044159835951c6855b7c1eee82ca2acd51
aaaa24 <chernov.arseniy@yandex.ru>
2026-05-08 12:33:21 +0300

    move hashtable.hpp to common/; add private fields

--- chernov.arseniy/common/hashtable.hpp
+++ chernov.arseniy/common/hashtable.hpp
@@ -5,10 +5,18 @@ namespace chernov {
   template< class Key, class Value, class Hash, class Equal >
   class HashTable {
   public:
+    HashTable();
+    ~HashTable();
     void add(Key k, Value v);
     Value drop(Key k);
     bool has(Key k);
     void rehash(size_t slots);
+  private:
+    T * _data;
+    size_t * _bucket_sizes;
+    size_t _num_buckets;
+    size_t _bucker_cap;
+    size_t _overflow_cap;
   };
 }
 
