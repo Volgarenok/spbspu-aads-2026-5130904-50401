d51792e54b07ba7428e96e3b8d275a05b61ccaaa
potomnazovy <skeleton39876@mail.ru>
2026-05-10 18:16:27 +0300

    complete commands.cpp

--- vasyakin.kirill/S4/commads.cpp
+++ vasyakin.kirill/S4/commads.cpp
@@ -73,3 +73,82 @@ void vasyakin::cmdComplement(std::istream& in, std::ostream& out, vasyakin::Data
     out << "Dataset not found" << '\n';
   }
 }
+
+void vasyakin::cmdIntersect(std::istream& in, std::ostream& out, vasyakin::Datasets& datasets)
+{
+  std::string new_map_name, map_name1, map_name2;
+  if (!(in >> new_map_name >> map_name1 >> map_name2))
+  {
+    out << "Invalid input" << '\n';
+    return;
+  }
+
+  try
+  {
+    const auto& ds1 = datasets.get(map_name1);
+    const auto& ds2 = datasets.get(map_name2);
+    vasyakin::Dataset new_ds;
+
+    for (auto cit = ds1.cbegin(); cit != ds1.cend(); ++cit)
+    {
+      const auto& pair = *cit;
+
+      try
+      {
+        ds2.get(pair.first);
+        new_ds.push(pair.first, pair.second);
+      }
+      catch (const std::out_of_range&)
+      {
+      }
+    }
+
+    datasets.push(new_map_name, new_ds);
+  }
+  catch (const std::out_of_range&)
+  {
+    out << "Dataset not found" << '\n';
+  }
+}
+
+void vasyakin::cmdUnion(std::istream& in, std::ostream& out, vasyakin::Datasets& datasets)
+{
+  std::string new_map_name, map_name1, map_name2;
+  if (!(in >>new_map_name >> map_name1 >> map_name2))
+  {
+    out << "Invalid input" << '\n';
+    return;
+  }
+
+  try
+  {
+    const auto& ds1 = datasets.get(map_name1);
+    const auto& ds2 = datasets.get(map_name2);
+    vasyakin::Dataset new_ds;
+
+    for (auto cit = ds1.cbegin(); cit != ds1.cend(); ++cit)
+    {
+      const auto& pair = *cit;
+      new_ds.push(pair.first, pair.second);
+    }
+
+    for (auto cit = ds2.cbegin(); cit != ds2.cend(); ++cit)
+    {
+      const auto& pair = *cit;
+      try
+      {
+        new_ds.get(pair.first);
+      }
+      catch (const std::out_of_range&)
+      {
+        new_ds.push(pair.first, pair.second);
+      }
+    }
+
+    datasets.push(new_map_name, new_ds);
+  }
+  catch (const std::out_of_range&)
+  {
+    out << "Dataset not found" << '\n';
+  }
+}
