b46c44fd860c7aeb1839c48080370514422c6b41
aaaa24 <chernov.arseniy@yandex.ru>
2026-05-11 15:57:41 +0300

    add default constructor and destructor

--- chernov.arseniy/common/hashtable.hpp
+++ chernov.arseniy/common/hashtable.hpp
@@ -1,6 +1,8 @@
 #ifndef HASHTABLE_HPP
 #define HASHTABLE_HPP
 
+#include <cstddef>
+
 namespace chernov {
   template< class Key, class Value, class Hash, class Equal >
   class HashTable {
@@ -12,12 +14,30 @@ namespace chernov {
     bool has(Key k);
     void rehash(size_t slots);
   private:
-    T * _data;
+    Value * _data;
     size_t * _bucket_sizes;
     size_t _num_buckets;
-    size_t _bucker_cap;
+    size_t _bucket_cap;
+    size_t _overflow_size;
     size_t _overflow_cap;
   };
+}
+
+template< class Key, class Value, class Hash, class Equal >
+chernov::HashTable< Key, Value, Hash, Equal >::HashTable():
+  _data(nullptr),
+  _bucket_sizes(nullptr),
+  _num_buckets(0),
+  _bucket_cap(0),
+  _overflow_size(0),
+  _overflow_cap(0)
+{}
+
+template< class Key, class Value, class Hash, class Equal >
+chernov::HashTable< Key, Value, Hash, Equal >::~HashTable()
+{
+  delete [] _data;
+  delete [] _bucket_sizes;
 }
 
 #endif
