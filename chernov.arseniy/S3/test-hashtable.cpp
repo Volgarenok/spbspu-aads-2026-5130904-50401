5b1c87bedfdf68b64db38aaf50f6b515b2221f91
aaaa24 <chernov.arseniy@yandex.ru>
2026-05-11 23:03:41 +0300

    add test files

---
+++ chernov.arseniy/S3/test-hashtable.cpp
@@ -0,0 +1,10 @@
+#include <boost/test/unit_test.hpp>
+#include <hashtable.hpp>
+
+BOOST_AUTO_TEST_SUITE(hashtable_special_members_tests)
+
+BOOST_AUTO_TEST_CASE(test_default_constructor)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
