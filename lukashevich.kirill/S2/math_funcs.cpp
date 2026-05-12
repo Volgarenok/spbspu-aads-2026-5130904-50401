ef0a0c657e0a4aa20b5e2db3c3cb39ccdd613b8c
kirusha-13 <kirillukashevich2007@gmail.com>
2026-05-13 02:25:04 +0300

    implement infixToPostfix converter

--- lukashevich.kirill/S2/math_funcs.cpp
+++ lukashevich.kirill/S2/math_funcs.cpp
@@ -39,6 +39,7 @@ namespace lukashevich
   {
     List< std::string > res;
     std::string cur_token = "";
+
     for (size_t i = 0; i < str.size(); ++i) {
       if (str[i] != ' ') {
         cur_token += str[i];
@@ -52,5 +53,53 @@ namespace lukashevich
       res.pushBack(cur_token);
     }
     return res;
+  }
+
+  List< std::string > infixToPostfix(const List< std::string >& tokens)
+  {
+    List< std::string > output;
+    Stack< std::string > op_stack;
+
+    for (auto it = tokens.begin(); it != tokens.end(); ++it) {
+      const std::string& token = *it;
+      if (token == "(") {
+        op_stack.push(token);
+      }
+
+      else if (token == ")") {
+        while (!op_stack.empty() && op_stack.first() != "(") {
+          output.pushBack(op_stack.drop());
+        }
+        if (op_stack.empty()) {
+          throw std::runtime_error("invalid brackets");
+        }
+        op_stack.drop();
+      }
+
+      else if (isOperator(token)) {
+        while (!op_stack.empty() && op_stack.first() != "(") {
+          size_t top_prec = getPriority(op_stack.first());
+          size_t cur_prec = getPriority(token);
+          if (top_prec > cur_prec || (top_prec == cur_prec && token != "!")) {
+            output.pushBack(op_stack.drop());
+          }
+          else {
+            break;
+          }
+        }
+        op_stack.push(token);
+      }
+      else {
+        output.pushBack(token);
+      }
+    }
+
+    while (!op_stack.empty()) {
+      if (op_stack.first() == "(") {
+        throw std::runtime_error("invalid brackets");
+      }
+      output.pushBack(op_stack.drop());
+    }
+    return output;
   }
 }
