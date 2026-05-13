84e576d2e0dbd5e2db97803a2123bc343cc90dfe
kirusha-13 <kirillukashevich2007@gmail.com>
2026-05-13 03:35:07 +0300

    implement main

--- lukashevich.kirill/S2/main.cpp
+++ lukashevich.kirill/S2/main.cpp
@@ -1,4 +1,51 @@
+#include "math_funcs.hpp"
+#include "../common/list.hpp"
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main()
-{}
+int main(int argc, char* argv[])
+{
+  using namespace lukashevich;
+
+  if (argc > 2) {
+    std::cerr << "invalid arguments\n";
+    return 1;
+  }
+  Stack< ll > results;
+  try {
+    std::istream* input = &std::cin;
+    std::ifstream file;
+    if (argc == 2) {
+      file.open(argv[1]);
+      if (!file.is_open()) {
+        std::cerr << "error in open file\n";
+        return 1;
+      }
+      input = &file;
+    }
+
+    std::string line;
+    while (std::getline(*input, line)) {
+      if (line.empty()) {
+        continue;
+      }
+      results.push(evaluate_expression(line));
+    }
+
+    bool first = true;
+    while (!results.empty()) {
+      if (!first) {
+        std::cout << " ";
+      }
+      std::cout << results.drop();
+      first = false;
+    }
+    std::cout << "\n";
+    return 0;
+  }
+  catch (const std::exception& e) {
+    std::cerr << "error: " << e.what() << "\n";
+    return 1;
+  }
+}
