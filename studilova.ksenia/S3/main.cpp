eb9e65ee817d58aaa4363d29c4488cbad48704f7
kstudilova <kstudilova@gmail.com>
2026-05-16 00:05:24 +0300

    file loading and command processing in main

--- studilova.ksenia/S3/main.cpp
+++ studilova.ksenia/S3/main.cpp
@@ -1,6 +1,78 @@
-#include <iostream>
+#include "commands.hpp"
 
-int main()
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
 {
-    
+  if (argc != 2)
+  {
+    std::cerr << "Filename is required\n";
+    return 1;
+  }
+
+  studilova::GraphsMap graphs(128);
+
+  std::ifstream input(argv[1]);
+
+  if (input)
+  {
+    std::string graphName;
+
+    while (input >> graphName)
+    {
+      size_t edgesCount = 0;
+
+      if (!(input >> edgesCount))
+      {
+        return 0;
+      }
+
+      studilova::Graph graph(edgesCount * 2 + 16);
+
+      for (size_t i = 0; i < edgesCount; ++i)
+      {
+        std::string from;
+        std::string to;
+        size_t weight = 0;
+
+        input >> from >> to >> weight;
+
+        if (!input)
+        {
+          return 0;
+        }
+
+        graph.bind(from, to, weight);
+      }
+
+      graphs.add(graphName, graph);
+    }
+  }
+
+  studilova::CommandsMap commands(32);
+  studilova::initCommands(commands);
+
+  std::string commandName;
+
+  while (std::cin >> commandName)
+  {
+    if (!commands.has(commandName))
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      continue;
+    }
+
+    try
+    {
+      commands.get(commandName)(std::cin, std::cout, graphs);
+    }
+    catch (...)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+    }
+  }
+
+  return 0;
 }
