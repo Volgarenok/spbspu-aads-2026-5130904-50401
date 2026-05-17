2f4a7a9eb50f6de584b7c519c3f79b39ba9e64d4
elizazha <lizokbella@mail.ru>
2026-05-17 22:23:50 +0300

    add iterators for myHash

--- zhuravleva.elizaveta/S3/myHash.hpp
+++ zhuravleva.elizaveta/S3/myHash.hpp
@@ -18,6 +18,15 @@ namespace zhuravleva
       myVector< List< std::pair<Key, Value> > > table;
       size_t getIndex(const Key& key) const;
     public:
+      class Iterator;
+      class ConstIterator;
+      Iterator begin();
+      Iterator end();
+      ConstIterator begin() const;
+      ConstIterator end() const;
+      ConstIterator cbegin() const;
+      ConstIterator cend() const;
+
       HashTable(size_t bucket_count = 8);
       size_t size() const noexcept;
       size_t bucket_count() const noexcept;
@@ -30,6 +39,300 @@ namespace zhuravleva
       bool empty() const noexcept;
       void clear();  
   };
+
+  template< class Key, class Value, class Hash, class Equal >
+  class HashTable< Key, Value, Hash, Equal >::Iterator
+  {
+    private:
+      HashTable< Key, Value, Hash, Equal >* hashTable;
+      size_t bucketIndex;
+      LIter< std::pair< Key, Value > > listIter;
+      void skipEmpty();
+    public:
+      Iterator();
+      Iterator(HashTable< Key, Value, Hash, Equal >* tablePtr, size_t index, LIter< std::pair< Key, Value > > iter);
+      std::pair< Key, Value >& operator*() const;
+      std::pair< Key, Value >* operator->() const;
+      Iterator& operator++();
+      Iterator operator++(int);
+      bool operator==(const Iterator& other) const noexcept;
+      bool operator!=(const Iterator& other) const noexcept;
+  };
+
+  template< class Key, class Value, class Hash, class Equal >
+  class HashTable< Key, Value, Hash, Equal >::ConstIterator
+  {
+    private:
+      const HashTable< Key, Value, Hash, Equal >* hashTable;
+      size_t bucketIndex;
+      LCIter< std::pair< Key, Value > > listIter;
+      void skipEmpty();
+    public:
+      ConstIterator();
+      ConstIterator(const HashTable< Key, Value, Hash, Equal >* tablePtr, size_t index, LCIter< std::pair< Key, Value > > iter);
+      const std::pair< Key, Value >& operator*() const;
+      const std::pair< Key, Value >* operator->() const;
+      ConstIterator& operator++();
+      ConstIterator operator++(int);
+      bool operator==(const ConstIterator& other) const noexcept;
+      bool operator!=(const ConstIterator& other) const noexcept;
+  };
+}
+
+template< class Key, class Value, class Hash, class Equal >
+typename zhuravleva::HashTable< Key, Value, Hash, Equal >::Iterator
+zhuravleva::HashTable< Key, Value, Hash, Equal >::begin()
+{
+  if (table.empty())
+  {
+    return end();
+  }
+  return Iterator(this, 0, table[0].begin());
+}
+
+template< class Key, class Value, class Hash, class Equal >
+typename zhuravleva::HashTable< Key, Value, Hash, Equal >::Iterator
+zhuravleva::HashTable< Key, Value, Hash, Equal >::end()
+{
+  return Iterator(this, table.size(), LIter< std::pair< Key, Value > >());
+}
+
+template< class Key, class Value, class Hash, class Equal >
+typename zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator
+zhuravleva::HashTable< Key, Value, Hash, Equal >::begin() const
+{
+  if (table.empty())
+  {
+    return end();
+  }
+  return ConstIterator(this, 0, table[0].cbegin());
+}
+
+template< class Key, class Value, class Hash, class Equal >
+typename zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator
+zhuravleva::HashTable< Key, Value, Hash, Equal >::end() const
+{
+  return ConstIterator(this, table.size(), LCIter< std::pair< Key, Value > >());
+}
+
+template< class Key, class Value, class Hash, class Equal >
+typename zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator
+zhuravleva::HashTable< Key, Value, Hash, Equal >::cbegin() const
+{
+  return begin();
+}
+
+template< class Key, class Value, class Hash, class Equal >
+typename zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator
+zhuravleva::HashTable< Key, Value, Hash, Equal >::cend() const
+{
+  return end();
+}
+
+template< class Key, class Value, class Hash, class Equal >
+zhuravleva::HashTable< Key, Value, Hash, Equal >::Iterator::Iterator():
+  hashTable(nullptr),
+  bucketIndex(0),
+  listIter()
+{}
+
+template< class Key, class Value, class Hash, class Equal >
+zhuravleva::HashTable< Key, Value, Hash, Equal >::Iterator::Iterator(
+    HashTable< Key, Value, Hash, Equal >* tablePtr, size_t index,
+    LIter< std::pair< Key, Value > > iter):
+  hashTable(tablePtr),
+  bucketIndex(index),
+  listIter(iter)
+{
+  skipEmpty();
+}
+
+template< class Key, class Value, class Hash, class Equal >
+void zhuravleva::HashTable< Key, Value, Hash, Equal >::Iterator::skipEmpty()
+{
+  if (!hashTable)
+  {
+    return;
+  }
+
+  while (bucketIndex < hashTable->table.size() &&
+      listIter == hashTable->table[bucketIndex].end())
+  {
+    bucketIndex++;
+
+    if (bucketIndex < hashTable->table.size())
+    {
+      listIter = hashTable->table[bucketIndex].begin();
+    }
+  }
+}
+
+template< class Key, class Value, class Hash, class Equal >
+std::pair< Key, Value >&
+zhuravleva::HashTable< Key, Value, Hash, Equal >::Iterator::operator*() const
+{
+  if (!hashTable || bucketIndex >= hashTable->table.size())
+  {
+    throw std::runtime_error("invalid iterator");
+  }
+  return *listIter;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+std::pair< Key, Value >*
+zhuravleva::HashTable< Key, Value, Hash, Equal >::Iterator::operator->() const
+{
+  return &(**this);
+}
+
+template< class Key, class Value, class Hash, class Equal >
+typename zhuravleva::HashTable< Key, Value, Hash, Equal >::Iterator&
+zhuravleva::HashTable< Key, Value, Hash, Equal >::Iterator::operator++()
+{
+  if (!hashTable || bucketIndex >= hashTable->table.size())
+  {
+    return *this;
+  }
+  ++listIter;
+  skipEmpty();
+  return *this;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+typename zhuravleva::HashTable< Key, Value, Hash, Equal >::Iterator
+zhuravleva::HashTable< Key, Value, Hash, Equal >::Iterator::operator++(int)
+{
+  Iterator temp(*this);
+  ++(*this);
+  return temp;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+bool zhuravleva::HashTable< Key, Value, Hash, Equal >::Iterator::operator==(
+    const Iterator& other) const noexcept
+{
+  if (hashTable != other.hashTable)
+  {
+    return false;
+  }
+  if (bucketIndex != other.bucketIndex)
+  {
+    return false;
+  }
+  if (!hashTable || bucketIndex >= hashTable->table.size())
+  {
+    return true;
+  }
+  return listIter == other.listIter;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+bool zhuravleva::HashTable< Key, Value, Hash, Equal >::Iterator::operator!=(
+    const Iterator& other) const noexcept
+{
+  return !(*this == other);
+}
+
+template< class Key, class Value, class Hash, class Equal >
+zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator::ConstIterator():
+  hashTable(nullptr),
+  bucketIndex(0),
+  listIter()
+{}
+
+template< class Key, class Value, class Hash, class Equal >
+zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator::ConstIterator(
+    const HashTable< Key, Value, Hash, Equal >* tablePtr, size_t index,
+    LCIter< std::pair< Key, Value > > iter):
+  hashTable(tablePtr),
+  bucketIndex(index),
+  listIter(iter)
+{
+  skipEmpty();
+}
+
+template< class Key, class Value, class Hash, class Equal >
+void zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator::skipEmpty()
+{
+  if (!hashTable)
+  {
+    return;
+  }
+  while (bucketIndex < hashTable->table.size() &&
+      listIter == hashTable->table[bucketIndex].cend())
+  {
+    bucketIndex++;
+    if (bucketIndex < hashTable->table.size())
+    {
+      listIter = hashTable->table[bucketIndex].cbegin();
+    }
+  }
+}
+
+template< class Key, class Value, class Hash, class Equal >
+const std::pair< Key, Value >&
+zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator::operator*() const
+{
+  if (!hashTable || bucketIndex >= hashTable->table.size())
+  {
+    throw std::runtime_error("invalid iterator");
+  }
+  return *listIter;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+const std::pair< Key, Value >*
+zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator::operator->() const
+{
+  return &(**this);
+}
+
+template< class Key, class Value, class Hash, class Equal >
+typename zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator&
+zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator::operator++()
+{
+  if (!hashTable || bucketIndex >= hashTable->table.size())
+  {
+    return *this;
+  }
+  ++listIter;
+  skipEmpty();
+  return *this;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+typename zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator
+zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator::operator++(int)
+{
+  ConstIterator temp(*this);
+  ++(*this);
+  return temp;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+bool zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator::operator==(
+    const ConstIterator& other) const noexcept
+{
+  if (hashTable != other.hashTable)
+  {
+    return false;
+  }
+  if (bucketIndex != other.bucketIndex)
+  {
+    return false;
+  }
+  if (!hashTable || bucketIndex >= hashTable->table.size())
+  {
+    return true;
+  }
+  return listIter == other.listIter;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+bool zhuravleva::HashTable< Key, Value, Hash, Equal >::ConstIterator::operator!=(
+    const ConstIterator& other) const noexcept
+{
+  return !(*this == other);
 }
 
 template <class Key, class Value, class Hash, class Equal>
@@ -187,3 +490,4 @@ void zhuravleva::HashTable< Key, Value, Hash, Equal >::clear()
 }
 
 #endif
+ 
