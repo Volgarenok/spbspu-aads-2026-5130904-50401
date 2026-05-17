67d9fb97958efbe4f770965333b0314b1180efb8
aaaa24 <chernov.arseniy@yandex.ru>
2026-05-17 18:40:48 +0300

    delete hash for pair

--- chernov.arseniy/S3/hasher.hpp
+++ chernov.arseniy/S3/hasher.hpp
@@ -22,24 +22,6 @@ namespace chernov {
   private:
     size_t seed_;
   };
-
-  template< class F, class S >
-  struct HasherXx< std::pair< F, S > > {
-    HasherXx(size_t s = 0):
-      seed_(s)
-    {}
-
-    size_t operator()(const std::pair< F, S > & p) const
-    {
-      boost::hash2::xxhash_64 h(seed_);
-      voost::hash2::hash_append(h, {}, p.first);
-      voost::hash2::hash_append(h, {}, p.second);
-      return r.result();
-    }
-
-  private:
-    size_t seed_;
-  };
 }
 
 #endif
