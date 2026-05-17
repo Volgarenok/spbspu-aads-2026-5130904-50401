975e5f0f978c500bfeda99fa39653aadaeca52b3
kstudilova <kstudilova@gmail.com>
2026-05-18 01:09:52 +0300

    add constructors and destructor with clear

--- studilova.ksenia/S4/bstree.hpp
+++ studilova.ksenia/S4/bstree.hpp
@@ -11,6 +11,7 @@ namespace studilova
   {
     public:
       BSTree();
+      explicit BSTree(const Compare& cmp);
       ~BSTree();
 
       bool empty() const;
@@ -29,6 +30,8 @@ namespace studilova
 
       Node* root_;
       Compare cmp_;
+
+      void clear(Node* node);
   };
 }
 
@@ -40,5 +43,35 @@ studilova::BSTree< Key, Value, Compare >::Node::Node(const Key& key, const Value
   right_(nullptr),
   parent_(nullptr)
 {}
+
+template< class Key, class Value, class Compare >
+studilova::BSTree< Key, Value, Compare >::BSTree() :
+  root_(nullptr),
+  cmp_(Compare())
+{}
+
+template< class Key, class Value, class Compare >
+studilova::BSTree< Key, Value, Compare >::BSTree(const Compare& cmp) :
+  root_(nullptr),
+  cmp_(cmp)
+{}
+
+template< class Key, class Value, class Compare >
+studilova::BSTree< Key, Value, Compare >::~BSTree()
+{
+  clear(root_);
+}
+
+template< class Key, class Value, class Compare >
+void studilova::BSTree< Key, Value, Compare >::clear(Node* node)
+{
+  if (!node)
+  {
+    return;
+  }
+  clear(node->left_);
+  clear(node->right_);
+  delete node;
+}
 
 #endif
