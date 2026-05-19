fad32c1825ef19776da749a4c57d91812fd43670
kstudilova <kstudilova@gmail.com>
2026-05-19 03:27:48 +0300

    add const iterator for BSTree

--- studilova.ksenia/S4/bstree_iterators.hpp
+++ studilova.ksenia/S4/bstree_iterators.hpp
@@ -8,6 +8,9 @@
 namespace studilova
 {
   template< class Key, class Value >
+  class BSTreeCIt;
+
+  template< class Key, class Value >
   class BSTreeIt
   {
     private:
@@ -27,9 +30,43 @@ namespace studilova
       bool operator==(const BSTreeIt& other) const;
       bool operator!=(const BSTreeIt& other) const;
 
+      operator BSTreeCIt< Key, Value >() const;
+
     private:
       Node* node_;
       Node* root_;
+
+      template< class K, class V, class C >
+      friend class BSTree;
+
+      template< class K, class V >
+      friend class BSTreeCIt;
+  };
+
+  template< class Key, class Value >
+  class BSTreeCIt
+  {
+    private:
+      using Node = studilova::Node< Key, Value >;
+
+    public:
+      BSTreeCIt();
+      BSTreeCIt(const Node* node, const Node* root);
+      BSTreeCIt(const BSTreeIt< Key, Value >& other);
+
+      std::pair< const Key&, const Value& > operator*() const;
+
+      BSTreeCIt& operator++();
+      BSTreeCIt operator++(int);
+      BSTreeCIt& operator--();
+      BSTreeCIt operator--(int);
+
+      bool operator==(const BSTreeCIt& other) const;
+      bool operator!=(const BSTreeCIt& other) const;
+
+    private:
+      const Node* node_;
+      const Node* root_;
 
       template< class K, class V, class C >
       friend class BSTree;
@@ -71,7 +108,7 @@ studilova::BSTreeIt< Key, Value >& studilova::BSTreeIt< Key, Value >::operator++
     }
   } else {
     Node* parent = node_->parent_;
-    while(parent && node_ == parent->right_)
+    while (parent && node_ == parent->right_)
     {
       node_ = parent;
       parent = parent->parent_;
@@ -137,6 +174,123 @@ bool studilova::BSTreeIt< Key, Value >::operator==(const BSTreeIt& other) const
 
 template< class Key, class Value >
 bool studilova::BSTreeIt< Key, Value >::operator!=(const BSTreeIt& other) const
+{
+  return !(*this == other);
+}
+
+template< class Key, class Value >
+studilova::BSTreeIt< Key, Value >::operator BSTreeCIt< Key, Value >() const
+{
+  return BSTreeCIt< Key, Value >(node_, root_);
+}
+
+template< class Key, class Value >
+studilova::BSTreeCIt< Key, Value >::BSTreeCIt() :
+  node_(nullptr),
+  root_(nullptr)
+{}
+
+template< class Key, class Value >
+studilova::BSTreeCIt< Key, Value >::BSTreeCIt(const Node* node, const Node* root) :
+  node_(node),
+  root_(root)
+{}
+
+template< class Key, class Value >
+studilova::BSTreeCIt< Key, Value >::BSTreeCIt(const BSTreeIt< Key, Value >& other) :
+  node_(other.node_),
+  root_(other.root_)
+{}
+
+template< class Key, class Value >
+std::pair< const Key&, const Value& > studilova::BSTreeCIt< Key, Value >::operator*() const
+{
+  return { node_->key_, node_->value_ };
+}
+
+template< class Key, class Value >
+studilova::BSTreeCIt< Key, Value >& studilova::BSTreeCIt< Key, Value >::operator++()
+{
+  if (!node_)
+  {
+    return *this;
+  }
+
+  if (node_->right_)
+  {
+    node_ = node_->right_;
+    while (node_->left_)
+    {
+      node_ = node_->left_;
+    }
+  } else {
+    const Node* parent = node_->parent_;
+    while (parent && node_ == parent->right_)
+    {
+      node_ = parent;
+      parent = parent->parent_;
+    }
+    node_ = parent;
+  }
+  return *this;
+}
+
+template< class Key, class Value >
+studilova::BSTreeCIt< Key, Value > studilova::BSTreeCIt< Key, Value >::operator++(int)
+{
+  BSTreeCIt temp(*this);
+  ++(*this);
+  return temp;
+}
+
+template< class Key, class Value >
+studilova::BSTreeCIt< Key, Value >& studilova::BSTreeCIt< Key, Value >::operator--()
+{
+  if (!node_)
+  {
+    node_ = root_;
+    while (node_ && node_->right_)
+    {
+      node_ = node_->right_;
+    }
+    return *this;
+  }
+
+  if (node_->left_)
+  {
+    node_ = node_->left_;
+    while (node_->right_)
+    {
+      node_ = node_->right_;
+    }
+  } else {
+    const Node* parent = node_->parent_;
+    while (parent && node_ == parent->left_)
+    {
+      node_ = parent;
+      parent = parent->parent_;
+    }
+    node_ = parent;
+  }
+  return *this;
+}
+
+template< class Key, class Value >
+studilova::BSTreeCIt< Key, Value > studilova::BSTreeCIt< Key, Value >::operator--(int)
+{
+  BSTreeCIt temp(*this);
+  --(*this);
+  return temp;
+}
+
+template< class Key, class Value >
+bool studilova::BSTreeCIt< Key, Value >::operator==(const BSTreeCIt& other) const
+{
+  return node_ == other.node_;
+}
+
+template< class Key, class Value >
+bool studilova::BSTreeCIt< Key, Value >::operator!=(const BSTreeCIt& other) const
 {
   return !(*this == other);
 }
