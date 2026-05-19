cd6a3b8754012d7a7bd1b7b1af4ea809e37eacb1
elizazha <lizokbella@mail.ru>
2026-05-19 20:20:14 +0300

    add graph command declarations

---
+++ zhuravleva.elizaveta/S3/commands.hpp
@@ -0,0 +1,24 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+#include <iosfwd>
+#include <string>
+#include "graph.hpp"
+#include "myHash.hpp"
+#include "hasher.hpp"
+
+namespace zhuravleva
+{
+  typedef HashTable< std::string, Graph, Blake2Hasher< std::string >, KeyEqual > GraphTable;
+  void graphs(std::ostream& out, std::istream& in, const GraphTable& graphs);
+  void vertexes(std::ostream& out, std::istream& in, const GraphTable& graphs);
+  void outbound(std::ostream& out, std::istream& in, const GraphTable& graphs);
+  void inbound(std::ostream& out, std::istream& in, const GraphTable& graphs);
+  
+  void bind(std::ostream& out, std::istream& in, GraphTable& graphs);
+  void cut(std::ostream& out, std::istream& in, GraphTable& graphs);
+  void create(std::ostream& out, std::istream& in, GraphTable& graphs);
+  void merge(std::ostream& out, std::istream& in, GraphTable& graphs);
+  void extract(std::ostream& out, std::istream& in, GraphTable& graphs);
+}
+
+#endif
