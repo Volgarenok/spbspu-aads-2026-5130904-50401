77158b646685b48ee0b84f9064f92eeff509e36c
kstudilova <kstudilova@gmail.com>
2026-05-19 18:10:13 +0300

    add complement command

--- studilova.ksenia/S4/commands.cpp
+++ studilova.ksenia/S4/commands.cpp
@@ -60,3 +60,33 @@ void studilova::Complement(std::istream& in, std::ostream&, Datasets& datasets)
   }
   datasets.push(new_name, result);
 }
+
+void studilova::Intersect(std::istream& in, std::ostream&, Datasets& datasets)
+{
+  std::string new_name;
+  std::string first_name;
+  std::string second_name;
+
+  if (!(in >> new_name >> first_name >> second_name))
+  {
+    throw std::runtime_error("invalid input");
+  }
+
+  const Dataset& first = datasets.get(first_name);
+  const Dataset& second = datasets.get(second_name);
+
+  Dataset result;
+
+  for (Dataset::CIt it = first.cbegin(); it != first.cend(); ++it)
+  {
+    std::pair< const int&, const std::string& > pair = *it;
+    try
+    {
+      second.get(pair.first);
+      result.push(pair.first, pair.second);
+    }
+    catch(const std::out_of_range&)
+    {}
+  }
+  datasets.push(new_name, result);
+}
