d1700bdf17752c6e04c47019582a84f992dc9b9e
kstudilova <kstudilova@gmail.com>
2026-05-19 17:59:43 +0300

    add intersect command

--- studilova.ksenia/S4/commands.cpp
+++ studilova.ksenia/S4/commands.cpp
@@ -29,3 +29,34 @@ void studilova::Print(std::istream& in, std::ostream& out, Datasets& datasets)
 
   out << '\n';
 }
+
+void studilova::Complement(std::istream& in, std::ostream&, Datasets& datasets)
+{
+  std::string new_name;
+  std::string first_name;
+  std::string second_name;
+
+  if (!(in >> new_name >> first_name >> second_name))
+  {
+    throw std::runtime_error("invalid input");
+  }
+
+  const Dataset& first = datasets.get(first_name);
+  const Dataset& second = datasets.get(second_name);
+
+  Dataset result;
+
+  for (Dataset::CIt it = first.cbegin(); it != first.cend(); ++it)
+  {
+    std::pair< const int&, const std::string& > pair = *it;
+    try
+    {
+      second.get(pair.first);
+    }
+    catch (const std::out_of_range&)
+    {
+      result.push(pair.first, pair.second);
+    }
+  }
+  datasets.push(new_name, result);
+}
