d04686bf4a8849182c228f35a9785b00a4a069e4
kstudilova <kstudilova@gmail.com>
2026-05-19 19:04:52 +0300

    isInteger help function

--- studilova.ksenia/S4/main.cpp
+++ studilova.ksenia/S4/main.cpp
@@ -1,4 +1,43 @@
+#include "bstree.hpp"
+#include "commands.hpp"
+
+#include <fstream>
 #include <iostream>
+#include <limits>
+#include <stdexcept>
+#include <string>
+
+namespace
+{
+  bool isInteger(const std::string& str)
+  {
+    if (str.empty())
+    {
+      return false;
+    }
+
+    size_t pos = 0;
+    if (str[pos] == '-' || str[pos] == '+')
+    {
+      ++pos;
+    }
+
+    if (pos == str.size())
+    {
+      return false;
+    }
+
+    while (pos < str.size())
+    {
+      if (str[pos] < '0' || str[pos] > '9')
+      {
+        return false;
+      }
+      ++pos;
+    }
+    return true;
+  }
+}
 
 int main()
 {
