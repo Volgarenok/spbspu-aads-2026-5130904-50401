9ded8bd71ae646fb0244a3a122b25db39d3931c3
elizazha <lizokbella@mail.ru>
2026-05-19 23:33:01 +0300

    add boost tests for main and myHash

---
+++ zhuravleva.elizaveta/S3/test-myHash.cpp
@@ -0,0 +1,76 @@
+#include <boost/test/unit_test.hpp>
+#include <stdexcept>
+#include "myHash.hpp"
+#include "hasher.hpp"
+
+BOOST_AUTO_TEST_CASE(hash_table_add_get_has_test)
+{
+  zhuravleva::HashTable< std::string, int, zhuravleva::Blake2Hasher< std::string >, zhuravleva::KeyEqual > table;
+  BOOST_TEST(table.empty());
+  table.add("one", 1);
+  table.add("two", 2);
+  BOOST_TEST(table.size() == 2);
+  BOOST_TEST(table.has("one"));
+  BOOST_TEST(table.has("two"));
+  BOOST_TEST(!table.has("three"));
+  BOOST_TEST(table.get("one") == 1);
+  BOOST_TEST(table.get("two") == 2);
+}
+
+BOOST_AUTO_TEST_CASE(hash_table_replace_value_test)
+{
+  zhuravleva::HashTable< std::string, int, zhuravleva::Blake2Hasher< std::string >, zhuravleva::KeyEqual > table;
+  table.add("key", 1);
+  table.add("key", 10);
+  BOOST_TEST(table.size() == 1);
+  BOOST_TEST(table.get("key") == 10);
+}
+
+BOOST_AUTO_TEST_CASE(hash_table_drop_test)
+{
+  zhuravleva::HashTable< std::string, int, zhuravleva::Blake2Hasher< std::string >, zhuravleva::KeyEqual > table;
+  table.add("a", 1);
+  table.add("b", 2);
+  BOOST_TEST(table.drop("a"));
+  BOOST_TEST(!table.has("a"));
+  BOOST_TEST(table.has("b"));
+  BOOST_TEST(table.size() == 1);
+  BOOST_TEST(!table.drop("a"));
+}
+
+BOOST_AUTO_TEST_CASE(hash_table_get_missing_key_test)
+{
+  zhuravleva::HashTable< std::string, int, zhuravleva::Blake2Hasher< std::string >, zhuravleva::KeyEqual > table;
+  table.add("a", 1);
+  BOOST_CHECK_THROW(table.get("b"), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(hash_table_rehash_test)
+{
+  zhuravleva::HashTable< std::string, int, zhuravleva::Blake2Hasher< std::string >, zhuravleva::KeyEqual > table(2);
+  table.add("a", 1);
+  table.add("b", 2);
+  table.add("c", 3);
+  table.rehash(16);
+  BOOST_TEST(table.bucket_count() == 16);
+  BOOST_TEST(table.size() == 3);
+  BOOST_TEST(table.get("a") == 1);
+  BOOST_TEST(table.get("b") == 2);
+  BOOST_TEST(table.get("c") == 3);
+}
+
+BOOST_AUTO_TEST_CASE(hash_table_iterator_test)
+{
+  zhuravleva::HashTable< std::string, int, zhuravleva::Blake2Hasher< std::string >, zhuravleva::KeyEqual > table;
+  table.add("a", 1);
+  table.add("b", 2);
+  table.add("c", 3);
+  size_t count = 0;
+  for (zhuravleva::HashTable< std::string, int, zhuravleva::Blake2Hasher< std::string >, zhuravleva::KeyEqual >::Iterator it = table.begin();
+      it != table.end();
+      ++it)
+  {
+    count++;
+  }
+  BOOST_TEST(count == 3);
+}
