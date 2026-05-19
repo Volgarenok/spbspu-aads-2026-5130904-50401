2f9a574bbe37a45273393807549d22b11ee14b55
kstudilova <kstudilova@gmail.com>
2026-05-19 19:15:45 +0300

    file input processing

--- studilova.ksenia/S4/main.cpp
+++ studilova.ksenia/S4/main.cpp
@@ -39,7 +39,54 @@ namespace
   }
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+  if (argc != 2)
+  {
+    std::cerr << "invalid argument";
+    return 1;
+  }
+
+  std::ifstream file(argv[1]);
+  if (!file)
+  {
+    std::cerr << "cannot open file\n";
+    return 1;
+  }
+
+  studilova::Datasets datasets;
+
+  std::string token;
+  std::string current_name;
+
+  while (file >> token)
+  {
+    if (!isInteger(token))
+    {
+      current_name = token;
+      try
+      {
+        datasets.get(current_name);
+      }
+      catch(const std::out_of_range&)
+      {
+        datasets.push(current_name, studilova::Dataset());
+      }
+    } else {
+      if (current_name.empty())
+      {
+        continue;
+      }
+
+      int key = std::stoi(token);
+
+      std::string value;
+      if (!(file >> value))
+      {
+        break;
+      }
+      datasets.get(current_name).push(key, value);
+    }
+  }
   return 0;
 }
