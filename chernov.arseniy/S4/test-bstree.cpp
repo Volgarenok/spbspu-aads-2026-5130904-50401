73fb81bf1bf8c494c6f114a76b8205f405e08826
aaaa24 <chernov.arseniy@yandex.ru>
2026-05-21 19:16:19 +0300

    add tests for swap and push

--- chernov.arseniy/S4/test-bstree.cpp
+++ chernov.arseniy/S4/test-bstree.cpp
@@ -100,3 +100,46 @@ BOOST_AUTO_TEST_CASE(test_move_operator)
 }
 
 BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(bstree_modifiers_tests)
+
+BOOST_AUTO_TEST_CASE(test_swap)
+{
+  chernov::BSTree< int, int, std::less< int > > bst1;
+  bst1.push(123, 42);
+  bst1.push(321, 52);
+
+  chernov::BSTree< int, int, std::less< int > > bst2;
+  bst1.swap(bst2);
+
+  BOOST_CHECK(bst1.empty());
+  BOOST_CHECK_THROW(bst1.at(123), std::out_of_range);
+
+  BOOST_CHECK_EQUAL(bst2.size(), 2);
+  BOOST_CHECK_EQUAL(bst2.at(123), 42);
+}
+
+BOOST_AUTO_TEST_CASE(test_push)
+{
+  chernov::BSTree< int, int, std::less< int > > bst;
+
+  bst.push(123, 42);
+  BOOST_CHECK_EQUAL(bst.size(), 1);
+  BOOST_CHECK_EQUAL(bst.at(123), 42);
+
+  bst.push(321, 52);
+  BOOST_CHECK_EQUAL(bst.size(), 2);
+  BOOST_CHECK_EQUAL(bst.at(321), 52);
+
+  bst.push(123, 78);
+  BOOST_CHECK_EQUAL(bst.size(), 2);
+  BOOST_CHECK_EQUAL(bst.at(123), 78);
+
+  int key = 456;
+  int value = 67;
+  bst.push(key, value);
+  BOOST_CHECK_EQUAL(bst.size(), 3);
+  BOOST_CHECK_EQUAL(bst.at(456), 67);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
