3586388cd6a1bd43616d2ddc27079cc7266dbb08
aaaa24 <chernov.arseniy@yandex.ru>
2026-05-21 16:54:02 +0300

    add rotateRight

--- chernov.arseniy/common/bstree.hpp
+++ chernov.arseniy/common/bstree.hpp
@@ -59,6 +59,9 @@ namespace chernov {
 
     void clear() noexcept;
 
+    const_iterator rotateLeft(const_iterator iter);
+    const_iterator rotateRight(const_iterator iter);
+
     Value & at(const Key & k);
     const Value & at(const Key & k) const;
 
@@ -347,6 +350,40 @@ namespace chernov {
     fake_root_->left = fake_leaf_;
     fake_root_->right = fake_leaf_;
     size_ = 0;
+  }
+
+  template< class Key, class Value, class Compare >
+  typename BSTree< Key, Value, Compare >::const_iterator
+  BSTree< Key, Value, Compare >::rotateRight(const_iterator iter)
+  {
+    detail::NodeBase * root = iter->node_;
+    detail::NodeBase * new_root = root->left;
+    if (new_root == fake_leaf_) {
+      throw std::logic_error("rotation is not possible");
+    }
+
+    root->left = new_root->right;
+    if (new_root->right != fake_leaf_) {
+      new_root->right->parent = root;
+    }
+
+    new_root->parent = root->parent;
+    if (root->parent == fake_root_) {
+      fake_root_->left = new_root;
+      fake_root_->right = new_root;
+    } else if (root->parent->left == root) {
+      root->parent->left = new_root;
+    } else {
+      root->parent->right = new_root;
+    }
+
+    root->parent = new_root;
+    new_root->right = root;
+
+    updateHeights(root);
+    updateHeights(new_root);
+
+    return const_iterator(new_root, fake_root_, fake_leaf_);
   }
 
   template< class Key, class Value, class Compare >
