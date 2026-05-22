bdd2fb04cec6998f0cc371bb363c450157136e49
aaaa24 <chernov.arseniy@yandex.ru>
2026-05-22 08:20:26 +0300

    add test for rotateLeft

--- chernov.arseniy/S4/test-bstree.cpp
+++ chernov.arseniy/S4/test-bstree.cpp
@@ -177,6 +177,37 @@ BOOST_AUTO_TEST_CASE(test_clear)
   BOOST_CHECK_THROW(bst.at(123), std::out_of_range);
 }
 
+BOOST_AUTO_TEST_CASE(test_rotate_left)
+{
+  chernov::BSTree< int, int, std::less< int > > bst;
+  bst.push(10, 210);
+  bst.push(5, 205);
+  bst.push(2, 202);
+  bst.push(9, 209);
+  bst.push(15, 215);
+  bst.push(13, 213);
+  bst.push(12, 212);
+  bst.push(14, 214);
+  bst.push(16, 216);
+
+  auto iter = bst.cbegin();
+  auto new_iter = bst.rotateLeft(iter);
+
+  BOOST_CHECK_EQUAL(iter->first, 10);
+  BOOST_CHECK_EQUAL((--iter)->first, 9);
+  BOOST_CHECK_EQUAL((--iter)->first, 5);
+  ++iter;
+  ++iter;
+
+  BOOST_CHECK_EQUAL((++iter)->first, 12);
+  BOOST_CHECK_EQUAL((++iter)->first, 13);
+  BOOST_CHECK_EQUAL((++iter)->first, 14);
+  BOOST_CHECK((++iter) == new_iter);
+
+  BOOST_CHECK_EQUAL(new_iter->first, 15);
+  BOOST_CHECK_EQUAL((++new_iter)->first, 16);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE(bstree_getters_tests)
