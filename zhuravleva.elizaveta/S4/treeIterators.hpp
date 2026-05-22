cb6864157213488bf66a6392cec327888af8a6e7
elizazha <lizokbella@mail.ru>
2026-05-22 16:30:50 +0300

    add iterator implementation

--- zhuravleva.elizaveta/S4/treeIterators.hpp
+++ zhuravleva.elizaveta/S4/treeIterators.hpp
@@ -30,6 +30,84 @@ namespace zhuravleva
   private:
     TreeNode< Key, Value >* current_;
   };
+
+  template< class Key, class Value >
+  BSTIterator< Key, Value >::BSTIterator() noexcept:
+    current_(nullptr)
+  {}
+
+  template< class Key, class Value >
+  BSTIterator< Key, Value >::BSTIterator(TreeNode< Key, Value >* node) noexcept:
+    current_(node)
+  {}
+
+  template< class Key, class Value >
+  std::pair< Key, Value >& BSTIterator< Key, Value >::operator*() const
+  {
+    if (!current_)
+    {
+      throw std::runtime_error("null iterator");
+    }
+    return current_->data_;
+  }
+
+  template< class Key, class Value >
+  std::pair< Key, Value >* BSTIterator< Key, Value >::operator->() const
+  {
+    if (!current_)
+    {
+      throw std::runtime_error("null iterator");
+    }
+    return &(current_->data_);
+  }
+
+  template< class Key, class Value >
+  bool BSTIterator< Key, Value >::operator==(const BSTIterator& other) const noexcept
+  {
+    return current_ == other.current_;
+  }
+
+  template< class Key, class Value >
+  bool BSTIterator< Key, Value >::operator!=(const BSTIterator& other) const noexcept
+  {
+    return current_ != other.current_;
+  }
+
+  template< class Key, class Value >
+  BSTIterator< Key, Value >& BSTIterator< Key, Value >::operator++()
+  {
+    if (!current_)
+    {
+      throw std::runtime_error("null iterator");
+    }
+    if (current_->right_)
+    {
+      current_ = current_->right_;
+      while (current_->left_)
+      {
+        current_ = current_->left_;
+      }
+    }
+    else
+    {
+      TreeNode< Key, Value >* parent = current_->parent_;
+      while (parent && current_ == parent->right_)
+      {
+        current_ = parent;
+        parent = parent->parent_;
+      }
+      current_ = parent;
+    }
+    return *this;
+  }
+
+  template< class Key, class Value >
+  BSTIterator< Key, Value > BSTIterator< Key, Value >::operator++(int)
+  {
+    BSTIterator< Key, Value > temp(*this);
+    ++(*this);
+    return temp;
+  }
 }
 
 #endif
