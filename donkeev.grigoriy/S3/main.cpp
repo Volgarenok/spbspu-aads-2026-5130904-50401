13d465edccd395659681b8516300b9487f02c6b2
grisha-git <donkeevgrisa@gmail.com>
2026-05-24 00:17:49 +0300

    Read the file

--- donkeev.grigoriy/S3/main.cpp
+++ donkeev.grigoriy/S3/main.cpp
@@ -1,6 +1,89 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "Hash-table.hpp"
+#include "hashTableIt.hpp"
+#include "Graph.hpp"
+#include "hash-structs.hpp"
 
-int main()
+std::string nextWord(const std::string& line, size_t& position)
 {
-  
+  while (position < line.size() && line[position] == ' ')
+  {
+    ++position;
+  }
+  size_t start = position;
+  while (position < line.size() && line[position] != ' ')
+  {
+    ++position;
+  }
+
+  return line.substr(start, position - start);
+}
+void readGraphs(const std::string& filename, donkeev::HashTable<std::string, donkeev::Graph, donkeev::GraphNameHash, donkeev::GraphEqual>& graphs)
+{
+  std::ifstream file(filename);
+  if (!file.is_open())
+  {
+    throw std::runtime_error("Cannot open file");
+  }
+
+  std::string line;
+  while (std::getline(file, line)) {
+    if (line.empty())
+    {
+      continue;
+    }
+
+    size_t position = 0;
+    std::string grapgName = nextWord(line, position);
+
+    std::string edgesCountStr = nextWord(line, position);
+    if (edgesCountStr.empty())
+    {
+      throw std::runtime_error("Bad input file");
+    }
+    size_t edgesCount = std::stoull(edgesCountStr);
+
+    donkeev::Graph graph(16, 4);
+    
+    for (size_t i = 0; i < edgesCount; ++i)
+    {
+      if (!std::getline(file, line))
+      {
+        throw std::runtime_error("Bad input file");
+      }
+      do
+      {
+        std::getline(file, line);
+      } while (line.empty());
+
+      position = 0;
+      std::string from = nextWord(line, position);
+      std::string to = nextWord(line, position);
+      std::string weightStr = nextWord(line, position);
+
+      if (from.empty() || to.empty() || weightStr.empty())
+      {
+        throw std::runtime_error("Bad input file");
+      }
+
+      size_t weight = std::stoull(weightStr);
+      graph.addEdge(from, to, weight);
+    }
+  }
+}
+int main(int argc, char* argv[])
+{
+  if (argc < 2)
+  {
+    std::cerr << "There no filename\n";
+    return 1;
+  }
+  std::string filename(argv[1]);
+
+  donkeev::HashTable< std::string, donkeev::Graph, donkeev::GraphNameHash, donkeev::GraphEqual> graphsTable(16, 4);
+  readGraphs(filename, graphsTable);
+  size_t a;
+  std::cin >> a;
 }
