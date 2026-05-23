41a8bd91b42ee65fe1101d9de3295deb9c0ad9ac
grisha-git <donkeevgrisa@gmail.com>
2026-05-24 00:14:25 +0300

    Rename the node file

---
+++ donkeev.grigoriy/S3/HTNode.hpp
@@ -0,0 +1,48 @@
+#ifndef HTNODE_HPP
+#define HTNODE_HPP
+
+#include <iostream>
+#include <utility>
+
+namespace donkeev
+{
+  template<class Key, class Value>
+  struct HTNode
+  {
+    std::pair<Key, Value> data_;
+    bool isOccupied_;
+
+    HTNode();
+    HTNode(const Key&, const Value&);
+    bool isEmpty() const;
+    Value dropNode();
+  };
+
+  template<class Key, class Value>
+  HTNode<Key, Value>::HTNode():
+    data_(),
+    isOccupied_(false)
+  {}
+
+  template<class Key, class Value>
+  HTNode<Key, Value>::HTNode(const Key& key, const Value& value):
+    data_(key, value),
+    isOccupied_(true)
+  {}
+
+  template<class Key, class Value>
+  bool HTNode<Key, Value>::isEmpty() const
+  {
+    return !isOccupied_;
+  }
+
+  template<class Key, class Value>
+  Value HTNode<Key, Value>::dropNode()
+  {
+    Value tmp = data_.second;
+    isOccupied_ = false;
+    return tmp;
+  }
+}
+
+#endif
