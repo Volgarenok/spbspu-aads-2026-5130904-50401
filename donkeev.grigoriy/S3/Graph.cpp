747c1859060eaa4a6b5fc9ff13166b87e28681fc
grisha-git <donkeevgrisa@gmail.com>
2026-05-24 00:13:00 +0300

    Add  edge and constructor

--- donkeev.grigoriy/S3/Graph.cpp
+++ donkeev.grigoriy/S3/Graph.cpp
@@ -1,12 +1,22 @@
-#include "Hash-table.hpp"
-#include "hash-structs.hpp"
+#include "Graph.hpp"
 
-namespace donkeev
+donkeev::Graph::Graph(const size_t bucketCount, const size_t bucketSize):
+  edgesCount_(0),
+  table_(bucketCount, bucketSize)
+{}
+
+void donkeev::Graph::addEdge(const std::string from, const std::string to, const size_t weight)
 {
-  class Graph
+  std::pair< std::string, std::string > thisKey = std::make_pair(from, to);
+  donkeev::List< size_t >* thisValue = table_.find(thisKey);
+  if (thisValue == nullptr)
   {
-    size_t edgesCount_;
-    size_t vertexCount_;
-    donkeev::HashTable< std::pair< std::string, std::string >, topit::Vector< size_t >, donkeev::VertexPairHash, donkeev::VertexPairEqual > table_;
-  };
+    donkeev::List< size_t > weights;
+    weights.pushBack(weight);
+
+    table_.add(std::make_pair(from, to), std::move(weights));
+    return;
+  }
+  
+  thisValue->pushBack(weight);
 }
