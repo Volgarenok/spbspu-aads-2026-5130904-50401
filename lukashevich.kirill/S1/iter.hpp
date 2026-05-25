57d38087e9a594f3b8ff72bdefda4d9c6d961256
kirusha-13 <kirillukashevich2007@gmail.com>
2026-05-25 17:27:11 +0300

    Add noexcept and detail in iter

--- lukashevich.kirill/S1/iter.hpp
+++ lukashevich.kirill/S1/iter.hpp
@@ -20,32 +20,35 @@ namespace lukashevich
     friend class List< T >;
 
     public:
-      LIter();
-      LIter< T >& operator++();
-      LIter< T > operator++(int);
-      LIter< T >& operator--();
-      LIter< T > operator--(int);
+      LIter() noexcept;
+      LIter< T >& operator++() noexcept;
+      LIter< T > operator++(int) noexcept;
+      LIter< T >& operator--() noexcept;
+      LIter< T > operator--(int) noexcept;
 
-      bool operator==(const LIter< T >& rhs) const;
-      bool operator!=(const LIter< T >& rhs) const;
+      bool operator==(const LIter< T >& rhs) const noexcept;
+      bool operator!=(const LIter< T >& rhs) const noexcept;
 
-      T& operator*();
-      T* operator->();
+      T& operator*() noexcept;
+      T* operator->() noexcept;
 
     private:
-      explicit LIter(detail::Node< T >* node):
-        node_(node)
-      {}
-      Node< T >* node_;
+      explicit LIter(detail::Node< T >* node) noexcept;
+      detail::Node< T >* node_;
   };
 
   template< class T >
-  LIter< T >::LIter():
+  LIter< T >::LIter(detail::Node< T >* node) noexcept:
+    node_(node)
+  {}
+
+  template< class T >
+  LIter< T >::LIter() noexcept:
     node_(nullptr)
   {}
 
   template< class T >
-  LIter< T >& LIter< T >::operator++()
+  LIter< T >& LIter< T >::operator++() noexcept
   {
     assert(node_ != nullptr);
     node_ = node_->next;
@@ -53,7 +56,7 @@ namespace lukashevich
   }
 
   template< class T >
-  LIter< T > LIter< T >::operator++(int)
+  LIter< T > LIter< T >::operator++(int) noexcept
   {
     LIter< T > temp = *this;
     ++(*this);
@@ -61,7 +64,7 @@ namespace lukashevich
   }
 
   template< class T >
-  LIter< T >& LIter< T >::operator--()
+  LIter< T >& LIter< T >::operator--() noexcept
   {
     assert(node_ != nullptr);
     node_ = node_->prev;
@@ -69,7 +72,7 @@ namespace lukashevich
   }
 
   template< class T >
-  LIter< T > LIter< T >::operator--(int)
+  LIter< T > LIter< T >::operator--(int) noexcept
   {
     LIter< T > temp = *this;
     --(*this);
@@ -77,26 +80,26 @@ namespace lukashevich
   }
 
   template< class T >
-  bool LIter< T >::operator==(const LIter< T >& rhs) const
+  bool LIter< T >::operator==(const LIter< T >& rhs) const noexcept
   {
     return node_ == rhs.node_;
   }
 
   template< class T >
-  bool LIter< T >::operator!=(const LIter< T >& rhs) const
+  bool LIter< T >::operator!=(const LIter< T >& rhs) const noexcept
   {
     return !(node_ == rhs.node_);
   }
 
   template< class T >
-  T& LIter< T >::operator*()
+  T& LIter< T >::operator*() noexcept
   {
     assert(node_ != nullptr);
     return node_->val;
   }
 
   template< class T >
-  T* LIter< T >::operator->()
+  T* LIter< T >::operator->() noexcept
   {
     assert(node_ != nullptr);
     return &node_->val;
