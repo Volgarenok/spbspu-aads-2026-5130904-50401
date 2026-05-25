e97bd7f5c4779fa005607edffa7c1aac521c489a
grisha-git <donkeevgrisa@gmail.com>
2026-05-25 23:06:59 +0300

    Command system

--- donkeev.grigoriy/S3/main.cpp
+++ donkeev.grigoriy/S3/main.cpp
@@ -5,6 +5,29 @@
 #include "hashTableIt.hpp"
 #include "Graph.hpp"
 #include "hash-structs.hpp"
+#include "graphs-commands.hpp"
+
+//(graphsHashTable_t, const std::string&, const std::string&, const size_t, 
+//const topit::Vector< std::string >&, std::ostream&
+
+namespace donkeev
+{
+  using graphsHashTable_t = donkeev::HashTable< std::string, donkeev::Graph, donkeev::GraphNameHash, donkeev::GraphEqual>;
+  using commandFunc = void (*)(graphsHashTable_t, const std::string&, std::ostream&);
+}
+const topit::Vector< std::pair< std::string, donkeev::commandFunc > > commandsVector{
+  std::initializer_list<std::pair<std::string, donkeev::commandFunc>>{
+  {"graphs", donkeev::printGrapsNames},
+  {"vertexes", donkeev::printVertexesNames},
+  {"outbound", donkeev::printOutboundVertexesNames},
+  {"inbound", donkeev::printInboundVertexesNames},
+  {"bind", donkeev::createEdge},
+  {"cut", donkeev::deleteEdge},
+  {"create", donkeev::createGraph},
+  {"merge", donkeev::mergeGraphs},
+  {"extract", donkeev::extractGraph}
+  }
+};
 
 std::string nextWord(const std::string& line, size_t& position)
 {
@@ -83,5 +106,19 @@ int main(int argc, char* argv[])
   std::string filename(argv[1]);
 
   donkeev::HashTable< std::string, donkeev::Graph, donkeev::GraphNameHash, donkeev::GraphEqual> graphsTable(16, 4);
-  readGraphs(filename, graphsTable);
+  try
+  {
+    readGraphs(filename, graphsTable);
+  }
+  catch (const std::runtime_error& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+  catch (const std::overflow_error& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+  
+  donkeev::HashTable< std::string, void (*)(), donkeev::CommandsHash, donkeev::CommandsEqual > commands(16, 4);
+
 }
