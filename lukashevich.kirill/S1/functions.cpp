f214afadfaa167becc63d6b69da2d86455771df4
kirusha-13 <kirillukashevich2007@gmail.com>
2026-05-25 19:22:24 +0300

    fix2

--- lukashevich.kirill/S1/functions.cpp
+++ lukashevich.kirill/S1/functions.cpp
@@ -101,7 +101,7 @@ void lukashevich::printAll(List< pair_t >& data, List< size_t >& sums)
 {
   while (hasNum(data)) {
     size_t sum = printOneRow(std::cout, data);
-    std::cout << '\n';
+    //std::cout << '\n';
     sums.pushBack(sum);
   }
 }
