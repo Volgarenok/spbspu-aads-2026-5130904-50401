cd8d004b2b46c1020355b645b83d959c164eef4a
kstudilova <kstudilova@gmail.com>
2026-05-31 02:20:59 +0300

    add test for ** math operation

--- studilova.ksenia/S2/test-stack.cpp
+++ studilova.ksenia/S2/test-stack.cpp
@@ -1,5 +1,8 @@
 #include <boost/test/unit_test.hpp>
+
 #include "stack.hpp"
+#include "math-expression.hpp"
+
 #include <string>
 
 BOOST_AUTO_TEST_SUITE(stack_tests)
@@ -73,6 +76,35 @@ BOOST_AUTO_TEST_CASE(string_stack)
   s.pop();
 
   BOOST_CHECK_EQUAL(s.top(), "hello");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(math_expression_tests)
+
+BOOST_AUTO_TEST_CASE(power_simple)
+{
+  BOOST_CHECK_EQUAL(studilova::evaluateExpression("2 ** 3"), 8);
+}
+
+BOOST_AUTO_TEST_CASE(power_priority)
+{
+  BOOST_CHECK_EQUAL(studilova::evaluateExpression("2 + 3 ** 2"), 11);
+}
+
+BOOST_AUTO_TEST_CASE(power_right_associative)
+{
+  BOOST_CHECK_EQUAL(studilova::evaluateExpression("2 ** 3 ** 2"), 512);
+}
+
+BOOST_AUTO_TEST_CASE(power_with_parentheses)
+{
+  BOOST_CHECK_EQUAL(studilova::evaluateExpression("( 2 ** 3 ) ** 2"), 64);
+}
+
+BOOST_AUTO_TEST_CASE(power_zero_exponent)
+{
+  BOOST_CHECK_EQUAL(studilova::evaluateExpression("5 ** 0"), 1);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
