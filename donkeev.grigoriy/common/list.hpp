ae01f827607a36808f5eff8922a7aac68043e58e
grisha-git <donkeevgrisa@gmail.com>
2026-06-05 22:13:36 +0300

    Fix list

--- donkeev.grigoriy/common/list.hpp
+++ donkeev.grigoriy/common/list.hpp
@@ -35,6 +35,7 @@ namespace donkeev
     LIter< T > pushAfter(LIter< T >, const T&);
     void pushFront(const T&);
     void pushBack(const T&);
+    void pushBack(T&&);
 
     void popFront();
     void cutAfter(LIter< T >&);
@@ -166,6 +167,23 @@ namespace donkeev
   void List< T >::pushBack(const T& value)
   {
     Node< T >* tmp = new Node< T >{value, head_};
+    if (length_ == 0)
+    {
+      head_ = tmp;
+      tail_ = tmp;
+      tail_->next = tmp;
+      head_->next = tmp;
+      ++length_;
+      return;
+    }
+    tail_->next = tmp;
+    tail_ = tmp;
+    ++length_;
+  }
+  template< class T >
+  void List< T >::pushBack(T&& value)
+  {
+    Node< T >* tmp = new Node< T >{std::move(value), head_};
     if (length_ == 0)
     {
       head_ = tmp;
