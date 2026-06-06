5849dc6365f8f37c6739609adf77a3824ef8eca2
grisha-git <donkeevgrisa@gmail.com>
2026-06-06 21:05:04 +0300

    Moved print info func to the main

--- donkeev.grigoriy/S1/functions.hpp
+++ donkeev.grigoriy/S1/functions.hpp
@@ -9,6 +9,5 @@ namespace donkeev
   bool areAnyvalues(const List< std::pair< std::string, List< size_t > > >&);
   size_t getMaxSize(const List< std::pair< std::string, List< size_t > > >&);
   bool checkOnOverflow(const List< std::pair< std::string, List< size_t > > >&);
-  void printInfo(const List< std::pair< std::string, List< size_t > > >&, std::ostream&);
 }
 #endif
--- donkeev.grigoriy/S1/main.cpp
+++ donkeev.grigoriy/S1/main.cpp
@@ -7,13 +7,108 @@ int main()
   using namespace donkeev;
   List< std::pair< std::string, List< size_t > > > data{};
   read(data, std::cin);
-  try
+  if (data.empty())
   {
-    printInfo(data, std::cout);
+    std::cout << "0" << '\n';
+    return 0;
   }
-  catch (std::exception& e)
+  size_t maximumIteration = getMaxSize(data);
+  size_t* sumArray = nullptr;
+  if (maximumIteration)
   {
-    std::cerr << e.what() << "\n";
-    return 1;
+    sumArray = new size_t[maximumIteration]{0};
   }
+  size_t sumIteration = 0;
+  LCIter< std::pair< std::string, List< size_t > > > outIt = data.cbegin();
+  std::cout << outIt->first;
+  outIt++;
+  for (size_t i = 1; i < data.size(); ++i)
+  {
+    std::cout << " " << outIt->first;
+    outIt++;
+  }
+  std::cout << "\n";
+  if (checkOnOverflow(data))
+  {
+    size_t iterationCount = 0;
+    while (iterationCount < maximumIteration)
+    {
+      outIt = data.cbegin();
+      bool firstPrint = true;
+      for (size_t i = 0; i < data.size(); ++i)
+      {
+        LCIter< size_t > innerIt = outIt->second.cbegin();
+        if (outIt->second.size() > iterationCount)
+        {
+          if (firstPrint)
+          {
+            innerIt += iterationCount;
+            std::cout << *innerIt;
+            firstPrint = false;
+          }
+          else
+          {
+            innerIt += iterationCount;
+            std::cout << " " << *innerIt;
+          }
+        }
+        ++outIt;
+      }
+      std::cout << "\n";
+      ++iterationCount;
+    }
+
+    delete [] sumArray;
+
+    std::cerr << "Sum of elements is overflow\n";
+  }
+  size_t iterationCount = 0;
+  while (iterationCount < maximumIteration)
+  {
+    outIt = data.cbegin();
+    bool firstPrint = true;
+    for (size_t i = 0; i < data.size(); ++i)
+    {
+      LCIter< size_t > innerIt = outIt->second.cbegin();
+      if (outIt->second.size() > iterationCount)
+      {
+        if (firstPrint)
+        {
+          innerIt += iterationCount;
+          sumArray[sumIteration] += *innerIt;
+          std::cout << *innerIt;
+          firstPrint = false;
+        }
+        else
+        {
+          innerIt += iterationCount;
+          sumArray[sumIteration] += *innerIt;
+          std::cout << " " << *innerIt;
+        }
+      }
+      ++outIt;
+    }
+    ++sumIteration;
+    std::cout << "\n";
+    ++iterationCount;
+  }
+  if (!areAnyvalues(data))
+  {
+    std::cout << 0 << "\n";
+  }
+  size_t i = 0;
+  if (maximumIteration)
+  {
+    std::cout << sumArray[i];
+  }
+  ++i;
+  for (; i < maximumIteration; ++i)
+  {
+    std::cout << " " << sumArray[i];
+  }
+  if (maximumIteration)
+  {
+    std::cout << "\n";
+  }
+  delete [] sumArray;
 }
