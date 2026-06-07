4a2a2379458ab63d1fc0fb1d53c883d9d3f9e9f8
grisha-git <donkeevgrisa@gmail.com>
2026-06-07 15:55:38 +0300

    Fix the creation an iterator

--- donkeev.grigoriy/S1/main.cpp
+++ donkeev.grigoriy/S1/main.cpp
@@ -77,7 +77,8 @@ int main()
     ++outIt;
 
     for (size_t i = 1; i < data.size(); ++i)
-    {
+    { 
+      LCIter< size_t > innerIt = outIt->second.cbegin();
       if (outIt->second.size() > iterationCount)
       {
         innerIt += iterationCount;
