df5e07655c2cb653f011ecc8144d2b382edb9a9d
legleb <sedovgleb2570@gmail.com>
2026-06-09 19:20:02 +0300

    declarate class of avl tree

---
+++ sedov.gleb/F0/avltree.hpp
@@ -0,0 +1,82 @@
+#ifndef AVLTREE_HPP
+#define AVLTREE_HPP
+#include <iostream>
+
+namespace sedov
+{
+  template< class Key, class Value, class Compare = std::less< Key > >
+  class AVLTree
+  {
+  public:
+      using valueType = std::pair< const Key, Value >;
+      using iter = AVLiter< Key, Value >;
+      using constIter = AVLConstiter< Key, Value >;
+
+      AVLTree() noexcept;
+      AVLTree(const AVLTree & h);
+      AVLTree(AVLTree && h) noexcept;
+      ~AVLTree();
+
+      AVLTree& operator=(const AVLTree & h);
+      AVLTree& operator=(AVLTree && h) noexcept;
+
+      bool empty() const noexcept;
+      size_t size() const noexcept;
+      void swap(AVLTree & h) noexcept;
+      void clear() noexcept;
+
+      bool find(const Key & k, Value & outValue) const;
+      const Value & at(const Key & k) const;
+      Value & at(const Key & k);
+      bool contains(const Key & k) const;
+
+      bool insert(const Key & k, const Value & v);
+      bool insert(Key && k, Value && v);
+      bool erase(const Key & k);
+      Value drop(const Key & k);
+
+      void push(const Key & k, const Value & v)
+      void push(Key && k, Value && v)
+
+      iter begin() noexcept;
+      iter end() noexcept;
+      constIter begin() const noexcept;
+      constIter end() const noexcept;
+      constIter cbegin() const noexcept;
+      constIter cend() const noexcept;
+
+      size_t height() const noexcept;
+      void collectInRange(const Key & from, const Key & to, List< valueType > & result) const;
+      void getAll(List< value_type > & result) const;
+
+  private:
+      AVLTreeNode< Key, Value > * root_;
+      size_t size_;
+      Key comp_;
+
+      AVLTreeNode< Key, Value > * clone(AVLTreeNode< Key, Value > * src, AVLTreeNode< Key, Value > * parent);
+      void clearImpl(AVLTreeNode< Key, Value > * node) noexcept;
+      AVLTreeNode< Key, Value > * fallLeft(AVLTreeNode< Key, Value > * node) const noexcept;
+      AVLTreeNode< Key, Value > * fallRight(AVLTreeNode< Key, Value > * node) const noexcept;
+
+      void updateHeight(AVLTreeNode< Key, Value > * node) noexcept;
+      AVLTreeNode< Key, Value > * balanceNode(AVLTreeNode< Key, Value > * node) noexcept;
+      AVLTreeNode< Key, Value > * rotateLeft(AVLTreeNode< Key, Value > * x) noexcept;
+      AVLTreeNode< Key, Value > * rotateRight(AVLTreeNode< Key, Value > * y) noexcept;
+      AVLTreeNode< Key, Value > * rotateLargeLeft(AVLTreeNode< Key, Value > * node) noexcept;
+      AVLTreeNode< Key, Value > * rotateLargeRight(AVLTreeNode< Key, Value > * node) noexcept;
+
+      std::pair< AVLTreeNode< Key, Value > *, bool > insertNode(AVLTreeNode< Key, Value > * node, const Key & k,
+        const Value & v);
+      std::pair< AVLTreeNode< Key, Value > *, bool > insertNode(AVLTreeNode< Key, Value > * node, Key && k,
+        Value && v);
+      AVLTreeNode< Key, Value > * removeNode(AVLTreeNode< Key, Value > * node, const Key & k, bool & found) noexcept;
+      const AVLTreeNode< Key, Value > * findNode(const Key & k) const noexcept;
+
+      void collectInRangeImpl(AVLTreeNode< Key, Value > * node, const Key & from, const Key & to,
+        List< valueType> & result) const;
+      void getAllImpl(AVLTreeNode< Key, Value > * node, List< valueType> & result) const;
+  };
+}
+
+#endif
