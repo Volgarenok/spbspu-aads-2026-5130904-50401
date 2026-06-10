0b5983a7ef66b8a7279d8b5e03c6f90b068c0e0e
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-06-10 23:32:46 +0300

    fix unit

--- ulanova.maria/S2/test-math.cpp
+++ ulanova.maria/S2/test-math.cpp
@@ -14,18 +14,7 @@ BOOST_AUTO_TEST_CASE(test_xor)
   BOOST_TEST_CHECK(ulanova::apply_op(5, 3, '^') == 6);
   BOOST_TEST_CHECK(ulanova::apply_op(6, 3, '^') == 5);
 }
-BOOST_AUTO_TEST_CASE(test_priority)
-{
-  BOOST_TEST_CHECK(ulanova::priority('+') == 1);
-  BOOST_TEST_CHECK(ulanova::priority('*') == 2);
-  BOOST_TEST_CHECK(ulanova::priority('^') == 1);
-}
-BOOST_AUTO_TEST_CASE(test_is_operator)
-{
-  BOOST_TEST_CHECK(ulanova::is_operator('+'));
-  BOOST_TEST_CHECK(ulanova::is_operator('^'));
-  BOOST_TEST_CHECK(!ulanova::is_operator('u'));
-}
+
 BOOST_AUTO_TEST_CASE(test_div_zero)
 {
   BOOST_CHECK_THROW(ulanova::apply_op(1, 0, '/'), std::runtime_error);
