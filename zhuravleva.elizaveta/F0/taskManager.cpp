4ec544f5326597d00cab2e61cabcbf1b4498ca82
elizazha <lizokbella@mail.ru>
2026-06-12 20:49:47 +0300

    add basic task mahager operations

--- zhuravleva.elizaveta/F0/taskManager.cpp
+++ zhuravleva.elizaveta/F0/taskManager.cpp
@@ -1,16 +1,56 @@
 #include "taskManager.hpp"
+#include <stdexcept>
 
-zhuravleva::TaskManager::TaskManager():
-  globalTasks_(),
-  lists_()
-{}
-
-bool zhuravleva::TaskManager::hasTask(const std::string& taskId) const
+namespace zhuravleva
 {
-  return globalTasks_.contains(taskId);
-}
+  TaskManager::TaskManager():
+    globalTasks_(),
+    lists_()
+  {}
 
-bool zhuravleva::TaskManager::hasList(const std::string& listName) const
-{
-  return lists_.contains(listName);
+  bool TaskManager::hasTask(const std::string& taskId) const
+  {
+    return globalTasks_.contains(taskId);
+  }
+
+  bool TaskManager::hasList(const std::string& listName) const
+  {
+    return lists_.contains(listName);
+  }
+
+  void TaskManager::createList(const std::string& listName, size_t maxLabor)
+  {
+    if (lists_.contains(listName))
+    {
+      throw std::logic_error("list already exists");
+    }
+    lists_.insert(listName, TaskList(listName, maxLabor));
+  }
+
+  void TaskManager::deleteList(const std::string& listName)
+  {
+    if (!lists_.contains(listName))
+    {
+      throw std::logic_error("list not found");
+    }
+    lists_.erase(listName);
+  }
+
+  void TaskManager::addTaskGlobal(const std::string& taskId, size_t labor)
+  {
+    if (globalTasks_.contains(taskId))
+    {
+      throw std::logic_error("task already exists");
+    }
+    globalTasks_.insert(taskId, Task(taskId, labor));
+  }
+
+  void TaskManager::removeTaskGlobal(const std::string& taskId)
+  {
+    if (!globalTasks_.contains(taskId))
+    {
+      throw std::logic_error("task not found");
+    }
+    globalTasks_.erase(taskId);
+  }
 }
--- zhuravleva.elizaveta/F0/taskManager.hpp
+++ zhuravleva.elizaveta/F0/taskManager.hpp
@@ -15,6 +15,10 @@ namespace zhuravleva
 
     bool hasTask(const std::string& taskId) const;
     bool hasList(const std::string& listName) const;
+    void createList(const std::string& listName, size_t maxLabor);
+    void deleteList(const std::string& listName);
+    void addTaskGlobal(const std::string& taskId, size_t labor);
+    void removeTaskGlobal(const std::string& taskId);
 
   private:
     CuckooHashTable< std::string, Task > globalTasks_;
