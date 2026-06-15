64b141725815d666863389079875aeae7923c019
kirusha-13 <kirillukashevich2007@gmail.com>
2026-06-16 01:38:44 +0300

    complete and remade vector.hpp

--- lukashevich.kirill/common/vector.hpp
+++ lukashevich.kirill/common/vector.hpp
@@ -3,16 +3,23 @@
 
 #include <cassert>
 #include <cstddef>
+#include <limits>
 #include <new>
 #include <stdexcept>
 #include <utility>
 
 namespace lukashevich {
   template< class T >
+  using VectorIt = T *;
+
+  template< class T >
+  using VectorConstIt = const T *;
+
+  template< class T >
   class Vector {
   public:
-    using iterator = T *;
-    using const_iterator = const T *;
+    using iterator = VectorIt< T >;
+    using const_it = VectorConstIt< T >;
 
     Vector();
     explicit Vector(size_t count);
@@ -40,10 +47,10 @@ namespace lukashevich {
 
     iterator begin() noexcept;
     iterator end() noexcept;
-    const_iterator begin() const noexcept;
-    const_iterator end() const noexcept;
-    const_iterator cbegin() const noexcept;
-    const_iterator cend() const noexcept;
+    const_it begin() const noexcept;
+    const_it end() const noexcept;
+    const_it cbegin() const noexcept;
+    const_it cend() const noexcept;
 
   private:
     static T * allocate(size_t capacity);
@@ -55,100 +62,316 @@ namespace lukashevich {
     size_t size_;
     size_t capacity_;
   };
-}
 
-template< class T >
-lukashevich::Vector< T >::Vector():
-  data_(nullptr),
-  size_(0),
-  capacity_(0)
-{}
+  template< class T >
+  Vector< T >::Vector():
+    data_(nullptr),
+    size_(0),
+    capacity_(0)
+  {}
 
-template< class T >
-lukashevich::Vector< T >::Vector(size_t count):
-  data_(nullptr),
-  size_(0),
-  capacity_(0)
-{
-  reserve(count);
+  template< class T >
+  Vector< T >::Vector(size_t count):
+    data_(nullptr),
+    size_(0),
+    capacity_(0)
+  {
+    reserve(count);
 
-  try {
-    for (size_t i = 0; i < count; ++i) {
-      new (data_ + i) T();
+    try {
+      for (size_t i = 0; i < count; ++i) {
+        new (data_ + i) T();
+        ++size_;
+      }
+    } catch (...) {
+      clear();
+      deallocate(data_);
+      data_ = nullptr;
+      capacity_ = 0;
+      throw;
     }
-    size_ = count;
-  } catch (...) {
-    destroyRange(data_, 0, size_);
-    deallocate(data_);
-    data_ = nullptr;
-    capacity_ = 0;
-    throw;
   }
-}
 
-template< class T >
-lukashevich::Vector< T >::Vector(size_t count, const T & value):
-  data_(nullptr),
-  size_(0),
-  capacity_(0)
-{
-  reserve(count);
+  template< class T >
+  Vector< T >::Vector(size_t count, const T & value):
+    data_(nullptr),
+    size_(0),
+    capacity_(0)
+  {
+    reserve(count);
 
-  try {
-    for (size_t i = 0; i < count; ++i) {
-      new (data_ + i) T(value);
-      ++size_;
+    try {
+      for (size_t i = 0; i < count; ++i) {
+        new (data_ + i) T(value);
+        ++size_;
+      }
+    } catch (...) {
+      clear();
+      deallocate(data_);
+      data_ = nullptr;
+      capacity_ = 0;
+      throw;
     }
-  } catch (...) {
+  }
+
+  template< class T >
+  Vector< T >::Vector(const Vector & rhs):
+    data_(nullptr),
+    size_(0),
+    capacity_(0)
+  {
+    reserve(rhs.size_);
+
+    try {
+      for (size_t i = 0; i < rhs.size_; ++i) {
+        pushBack(rhs[i]);
+      }
+    } catch (...) {
+      clear();
+      deallocate(data_);
+      data_ = nullptr;
+      capacity_ = 0;
+      throw;
+    }
+  }
+
+  template< class T >
+  Vector< T >::Vector(Vector && rhs) noexcept:
+    data_(rhs.data_),
+    size_(rhs.size_),
+    capacity_(rhs.capacity_)
+  {
+    rhs.data_ = nullptr;
+    rhs.size_ = 0;
+    rhs.capacity_ = 0;
+  }
+
+  template< class T >
+  Vector< T >::~Vector()
+  {
     clear();
     deallocate(data_);
-    data_ = nullptr;
-    capacity_ = 0;
-    throw;
-  }
-}
-
-template< class T >
-lukashevich::Vector< T >::~Vector()
-{
-  clear();
-  deallocate(data_);
-}
-
-template< class T >
-T * lukashevich::Vector< T >::allocate(size_t capacity)
-{
-  if (capacity == 0) {
-    return nullptr;
   }
 
-  return static_cast< T * >(operator new(sizeof(T) * capacity));
-}
+  template< class T >
+  Vector< T > & Vector< T >::operator=(const Vector & rhs)
+  {
+    if (this != &rhs) {
+      Vector temp(rhs);
+      swap(temp);
+    }
 
-template< class T >
-void lukashevich::Vector< T >::deallocate(T * data) noexcept
-{
-  operator delete(data);
-}
-
-template< class T >
-void lukashevich::Vector< T >::destroyRange(T * data, size_t begin, size_t end) noexcept
-{
-  for (size_t i = begin; i < end; ++i) {
-    data[i].~T();
-  }
-}
-
-template< class T >
-size_t lukashevich::Vector< T >::nextCapacity(size_t needed)
-{
-  size_t capacity = 1;
-
-  while (capacity < needed) {
-    capacity *= 2;
+    return *this;
   }
 
-  return capacity;
+  template< class T >
+  Vector< T > & Vector< T >::operator=(Vector && rhs) noexcept
+  {
+    if (this != &rhs) {
+      clear();
+      deallocate(data_);
+
+      data_ = rhs.data_;
+      size_ = rhs.size_;
+      capacity_ = rhs.capacity_;
+
+      rhs.data_ = nullptr;
+      rhs.size_ = 0;
+      rhs.capacity_ = 0;
+    }
+
+    return *this;
+  }
+
+  template< class T >
+  void Vector< T >::pushBack(const T & value)
+  {
+    if (size_ == capacity_) {
+      reserve(nextCapacity(size_ + 1));
+    }
+
+    new (data_ + size_) T(value);
+    ++size_;
+  }
+
+  template< class T >
+  void Vector< T >::popBack()
+  {
+    if (size_ == 0) {
+      throw std::out_of_range("pop from empty vector");
+    }
+
+    --size_;
+    data_[size_].~T();
+  }
+
+  template< class T >
+  void Vector< T >::erase(size_t index)
+  {
+    if (index >= size_) {
+      throw std::out_of_range("bad vector index");
+    }
+
+    for (size_t i = index; i + 1 < size_; ++i) {
+      data_[i] = data_[i + 1];
+    }
+
+    popBack();
+  }
+
+  template< class T >
+  void Vector< T >::clear() noexcept
+  {
+    destroyRange(data_, 0, size_);
+    size_ = 0;
+  }
+
+  template< class T >
+  void Vector< T >::reserve(size_t capacity)
+  {
+    if (capacity <= capacity_) {
+      return;
+    }
+
+    T * newData = allocate(capacity);
+    size_t index = 0;
+
+    try {
+      for (; index < size_; ++index) {
+        new (newData + index) T(data_[index]);
+      }
+    } catch (...) {
+      destroyRange(newData, 0, index);
+      deallocate(newData);
+      throw;
+    }
+
+    destroyRange(data_, 0, size_);
+    deallocate(data_);
+
+    data_ = newData;
+    capacity_ = capacity;
+  }
+
+  template< class T >
+  void Vector< T >::swap(Vector & rhs) noexcept
+  {
+    std::swap(data_, rhs.data_);
+    std::swap(size_, rhs.size_);
+    std::swap(capacity_, rhs.capacity_);
+  }
+
+  template< class T >
+  T & Vector< T >::operator[](size_t index)
+  {
+    assert(index < size_);
+    return data_[index];
+  }
+
+  template< class T >
+  const T & Vector< T >::operator[](size_t index) const
+  {
+    assert(index < size_);
+    return data_[index];
+  }
+
+  template< class T >
+  size_t Vector< T >::getSize() const noexcept
+  {
+    return size_;
+  }
+
+  template< class T >
+  size_t Vector< T >::getCapacity() const noexcept
+  {
+    return capacity_;
+  }
+
+  template< class T >
+  bool Vector< T >::isEmpty() const noexcept
+  {
+    return size_ == 0;
+  }
+
+  template< class T >
+  VectorIt< T > Vector< T >::begin() noexcept
+  {
+    return data_;
+  }
+
+  template< class T >
+  VectorIt< T > Vector< T >::end() noexcept
+  {
+    return data_ + size_;
+  }
+
+  template< class T >
+  VectorConstIt< T > Vector< T >::begin() const noexcept
+  {
+    return data_;
+  }
+
+  template< class T >
+  VectorConstIt< T > Vector< T >::end() const noexcept
+  {
+    return data_ + size_;
+  }
+
+  template< class T >
+  VectorConstIt< T > Vector< T >::cbegin() const noexcept
+  {
+    return data_;
+  }
+
+  template< class T >
+  VectorConstIt< T > Vector< T >::cend() const noexcept
+  {
+    return data_ + size_;
+  }
+
+  template< class T >
+  T * Vector< T >::allocate(size_t capacity)
+  {
+    if (capacity == 0) {
+      return nullptr;
+    }
+
+    if (capacity > std::numeric_limits< size_t >::max() / sizeof(T)) {
+      throw std::length_error("vector capacity is too large");
+    }
+
+    return static_cast< T * >(operator new(sizeof(T) * capacity));
+  }
+
+  template< class T >
+  void Vector< T >::deallocate(T * data) noexcept
+  {
+    operator delete(data);
+  }
+
+  template< class T >
+  void Vector< T >::destroyRange(T * data, size_t begin, size_t end) noexcept
+  {
+    for (size_t i = begin; i < end; ++i) {
+      data[i].~T();
+    }
+  }
+
+  template< class T >
+  size_t Vector< T >::nextCapacity(size_t needed)
+  {
+    size_t capacity = 1;
+
+    while (capacity < needed) {
+      if (capacity > std::numeric_limits< size_t >::max() / 2) {
+        return needed;
+      }
+
+      capacity *= 2;
+    }
+
+    return capacity;
+  }
 }
 
 #endif
