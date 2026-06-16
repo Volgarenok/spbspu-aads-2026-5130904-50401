ff2d2b2d84b35867e0bfcba4621cbab8c9a2e0c7
kirusha-13 <kirillukashevich2007@gmail.com>
2026-06-16 03:12:37 +0300

    add hash table search and insertion

--- lukashevich.kirill/S3/hash-table.hpp
+++ lukashevich.kirill/S3/hash-table.hpp
@@ -54,8 +54,19 @@ namespace lukashevich {
     size_t bucketSize() const noexcept;
     size_t overflowSize() const noexcept;
 
+    bool has(const Key & key) const;
+    Value & at(const Key & key);
+    const Value & at(const Key & key) const;
+
+    void add(const Key & key, const Value & value);
+    Value & operator[](const Key & key);
+
   private:
     static size_t checkedMainSize(size_t bucketCount, size_t bucketSize);
+    size_t getHomeBucket(const Key & key) const;
+    Node * findNode(const Key & key);
+    const Node * findNode(const Key & key) const;
+    Node * findFreeInHomeBucket(const Key & key);
 
     Vector< Node > buckets_;
     List< Node > overflow_;
@@ -88,17 +99,6 @@ lukashevich::HashTable< Key, Value, Hash, Equal >::HashTable(size_t bucketCount,
 }
 
 template< class Key, class Value, class Hash, class Equal >
-size_t lukashevich::HashTable< Key, Value, Hash, Equal >::checkedMainSize(size_t bucketCount, size_t bucketSize)
-{
-  if ((bucketCount != 0)
-      && (bucketSize > static_cast< size_t >(-1) / bucketCount)) {
-    throw std::overflow_error("hash table size overflow");
-  }
-
-  return bucketCount * bucketSize;
-}
-
-template< class Key, class Value, class Hash, class Equal >
 bool lukashevich::HashTable< Key, Value, Hash, Equal >::empty() const noexcept
 {
   return size_ == 0;
@@ -126,6 +126,154 @@ template< class Key, class Value, class Hash, class Equal >
 size_t lukashevich::HashTable< Key, Value, Hash, Equal >::overflowSize() const noexcept
 {
   return overflow_.size();
+}
+
+template< class Key, class Value, class Hash, class Equal >
+void lukashevich::HashTable< Key, Value, Hash, Equal >::add(
+    const Key & key,
+    const Value & value)
+{
+  if (has(key)) {
+    throw std::logic_error("duplicate key in hash table");
+  }
+
+  Node * freeNode = findFreeInHomeBucket(key);
+
+  if (freeNode != nullptr) {
+    freeNode->key_ = key;
+    freeNode->value_ = value;
+    freeNode->occupied_ = true;
+    ++size_;
+    return;
+  }
+
+  if (overflow_.size() >= bucketSize_) {
+    throw std::overflow_error("hash table overflow bucket is full");
+  }
+
+  overflow_.pushBack(Node(key, value));
+  ++size_;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+Value & lukashevich::HashTable< Key, Value, Hash, Equal >::operator[](const Key & key)
+{
+  if (!has(key)) {
+    add(key, Value());
+  }
+
+  return at(key);
+}
+
+template< class Key, class Value, class Hash, class Equal >
+size_t lukashevich::HashTable< Key, Value, Hash, Equal >::checkedMainSize(size_t bucketCount, size_t bucketSize)
+{
+  if ((bucketCount != 0)
+      && (bucketSize > static_cast< size_t >(-1) / bucketCount)) {
+    throw std::overflow_error("hash table size overflow");
+  }
+
+  return bucketCount * bucketSize;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+bool lukashevich::HashTable< Key, Value, Hash, Equal >::has(const Key & key) const
+{
+  return findNode(key) != nullptr;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+Value & lukashevich::HashTable< Key, Value, Hash, Equal >::at(const Key & key)
+{
+  Node * node = findNode(key);
+
+  if (node == nullptr) {
+    throw std::out_of_range("hash table key not found");
+  }
+
+  return node->value_;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+const Value & lukashevich::HashTable< Key, Value, Hash, Equal >::at(const Key & key) const
+{
+  const Node * node = findNode(key);
+
+  if (node == nullptr) {
+    throw std::out_of_range("hash table key not found");
+  }
+
+  return node->value_;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+size_t lukashevich::HashTable< Key, Value, Hash, Equal >::getHomeBucket(const Key & key) const
+{
+  return hash_(key) % bucketCount_;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+typename lukashevich::HashTable< Key, Value, Hash, Equal >::Node *
+    lukashevich::HashTable< Key, Value, Hash, Equal >::findNode(const Key & key)
+{
+  const size_t bucket = getHomeBucket(key);
+  const size_t begin = bucket * bucketSize_;
+  const size_t finish = begin + bucketSize_;
+
+  for (size_t index = begin; index < finish; ++index) {
+    if (buckets_[index].occupied_ && equal_(buckets_[index].key_, key)) {
+      return std::addressof(buckets_[index]);
+    }
+  }
+
+  for (typename List< Node >::iterator it = overflow_.begin(); it != overflow_.end(); ++it) {
+    if (equal_(it->key_, key)) {
+      return std::addressof(*it);
+    }
+  }
+
+  return nullptr;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+const typename lukashevich::HashTable< Key, Value, Hash, Equal >::Node *
+    lukashevich::HashTable< Key, Value, Hash, Equal >::findNode(const Key & key) const
+{
+  const size_t bucket = getHomeBucket(key);
+  const size_t begin = bucket * bucketSize_;
+  const size_t finish = begin + bucketSize_;
+
+  for (size_t index = begin; index < finish; ++index) {
+    if (buckets_[index].occupied_ && equal_(buckets_[index].key_, key)) {
+      return std::addressof(buckets_[index]);
+    }
+  }
+
+  for (typename List< Node >::const_iterator it = overflow_.cbegin(); it != overflow_.cend(); ++it) {
+    if (equal_(it->key_, key)) {
+      return std::addressof(*it);
+    }
+  }
+
+  return nullptr;
+}
+
+template< class Key, class Value, class Hash, class Equal >
+typename lukashevich::HashTable< Key, Value, Hash, Equal >::Node *
+    lukashevich::HashTable< Key, Value, Hash, Equal >::findFreeInHomeBucket(
+        const Key & key)
+{
+  const size_t bucket = getHomeBucket(key);
+  const size_t begin = bucket * bucketSize_;
+  const size_t finish = begin + bucketSize_;
+
+  for (size_t index = begin; index < finish; ++index) {
+    if (!buckets_[index].occupied_) {
+      return std::addressof(buckets_[index]);
+    }
+  }
+
+  return nullptr;
 }
 
 #endif
