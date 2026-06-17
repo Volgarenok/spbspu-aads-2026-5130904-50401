79e3ccd741dfd0351368952335199262a0392d19
potomnazovy <skeleton39876@mail.ru>
2026-06-17 17:40:09 +0300

    add create-warehouse command

--- vasyakin.kirill/F0/commands.cpp
+++ vasyakin.kirill/F0/commands.cpp
@@ -62,3 +62,39 @@ std::string unescapeSpaces(const std::string& s)
 
   return result;
 }
+
+void vasyakin::cmdCreateWarehouse(
+  std::istream& in, std::ostream& out, SystemState& state)
+{
+  state.completeTransfers();
+
+  const vasyakin::Date date = readDate(in);
+  state.current_date_ = date;
+
+  std::string name;
+  size_t capacity = 0, days_to_center = 0;
+
+  if (!(in >> name >> capacity >> days_to_center) ||
+    capacity == 0 || days_to_center == 0)
+  {
+    throw std::runtime_error("Invalid create-warehouse args");
+  }
+
+  if (state.warehouses_.has(name))
+  {
+    out << "<ERROR: WAREHOUSE ALREADY EXISTS>" << '\n';
+    return;
+  }
+
+  state.warehouses_.insert(name,
+    WarehouseState(name, capacity, days_to_center));
+
+  ++state.op_counter_;
+
+  out << "<WAREHOUSE CREATED: " << name
+    << " (capacity: " << capacity
+    << ", days-to-center: " << days_to_center << ")>" << '\n';
+
+  state.log_.insert(state.op_counter_,
+    LogEntry{state.op_counter_, state.current_date_, "CREATE_WAREHOUSE " + name});
+}
