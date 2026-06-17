03dba5b1148d19d01ea6c5c1402b72a9f2485585
potomnazovy <skeleton39876@mail.ru>
2026-06-17 17:21:33 +0300

    add AVLTree.hpp

---
+++ vasyakin.kirill/F0/AVLTree.hpp
@@ -0,0 +1,82 @@
+#ifndef AVLTREE_HPP
+#define AVLTREE_HPP
+#include <stdexcept>
+#include <cstddef>
+#include <functional>
+#include <algorithm>
+#include <memory>
+#include "node.hpp"
+#include "treeIters.hpp"
+
+namespace vasyakin
+{
+  template< class Key, class Value, class Compare = std::less< Key > >
+  class AVLTree
+  {
+  public:
+    using iterator = AVLIterator< Key, Value >;
+    using const_iterator = AVLConstIterator< Key, Value >;
+
+    explicit AVLTree(const Compare& cmp = Compare());
+    AVLTree(const AVLTree& other);
+    AVLTree(AVLTree&& other) noexcept;
+    ~AVLTree() noexcept;
+    AVLTree& operator=(const AVLTree& other);
+    AVLTree& operator=(AVLTree&& other) noexcept;
+
+    void insert(const Key& key, const Value& value);
+    bool remove(const Key& key);
+    void clear() noexcept;
+
+    iterator find(const Key& key) noexcept;
+    const_iterator find(const Key& key) const noexcept;
+
+    Value& at(const Key& key);
+    const Value& at(const Key& key) const;
+    bool has(const Key& key) const noexcept;
+
+    size_t size() const noexcept;
+    bool empty() const noexcept;
+
+    iterator lower_bound(const Key& key) noexcept;
+    const_iterator lower_bound(const Key& key) const noexcept;
+
+    iterator upper_bound(const Key& key) noexcept;
+    const_iterator upper_bound(const Key& key) const noexcept;
+
+    std::pair< iterator, iterator > equal_range(const Key& key) noexcept;
+    std::pair< const_iterator, const_iterator > equal_range(const Key& key) const noexcept;
+
+    iterator begin() noexcept;
+    iterator end() noexcept;
+    const_iterator begin() const noexcept;
+    const_iterator end() const noexcept;
+    const_iterator cbegin() const noexcept;
+    const_iterator cend() const noexcept;
+  private:
+    using Node = vasyakin::detail::Node< Key, Value >;
+    Node* root_;
+    size_t size_;
+    Compare cmp_;
+
+    int getHeight(const Node* node) const noexcept;
+    int getBalance(const Node* node) const noexcept;
+    void updateHeight(Node* node) noexcept;
+
+    Node* rotateLeft(Node* node) noexcept;
+    Node* rotateRight(Node* node) noexcept;
+    Node* rotateLargeLeft(Node* node) noexcept;
+    Node* rotateLargeRight(Node* node) noexcept;
+
+    Node* balance(Node* node) noexcept;
+    void rebalanceUp(Node* from) noexcept;
+
+    Node* cloneNode(const Node* src, Node* parent) const;
+    void swap(AVLTree& other) noexcept;
+    Node* fallLeft(Node* node) const noexcept;
+    const Node* findNode(const Key& key) const noexcept;
+    static void destroyTree(Node* node) noexcept;
+  };
+}
+
+#endif
