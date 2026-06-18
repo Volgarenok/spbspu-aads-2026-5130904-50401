7b2c3a7b2a645f96a8816603af8617e175a844d0
potomnazovy <skeleton39876@mail.ru>
2026-06-18 13:58:01 +0300

    delete beauty main((( and fix node constructor

--- vasyakin.kirill/F0/node.hpp
+++ vasyakin.kirill/F0/node.hpp
@@ -19,9 +19,6 @@ namespace vasyakin
     class Node
     {
     public:
-      Node(const Key& key, const Value& value);
-      Node(Key&& key, Value&& value);
-
       template< class K, class V >
       Node(K&& key, V&& value);
     private:
@@ -41,26 +38,6 @@ namespace vasyakin
       template< class K, class V, class C >
       friend class vasyakin::AVLTree;
     };
-
-    template< class Key, class Value >
-    Node< Key, Value >::Node(const Key& key, const Value& value):
-      key_(key),
-      value_(value),
-      left_(nullptr),
-      right_(nullptr),
-      parent_(nullptr),
-      height_(1)
-    {}
-
-    template< class Key, class Value >
-    Node< Key, Value >::Node(Key&& key, Value&& value):
-      key_(std::move(key)),
-      value_(std::move(value)),
-      left_(nullptr),
-      right_(nullptr),
-      parent_(nullptr),
-      height_(1)
-    {}
 
     template< class Key, class Value >
     template< class K, class V >
