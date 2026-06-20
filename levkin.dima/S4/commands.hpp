75ab2d68d3487286446600897106389c6ff78806
Dmitry Levkin <mit.levkin@vk.com>
2026-06-20 21:22:42 +0300

    Refactor and clean up commands module

--- levkin.dima/S4/commands.hpp
+++ levkin.dima/S4/commands.hpp
@@ -8,21 +8,16 @@
 #include "bstree.hpp"
 
 namespace levkin {
+
+  using BST = levkin::BSTree< std::string, std::string >;
+  using DB = std::map< std::string, BST >;
+  void handleError(std::ostream& out, std::istream& in);
   using cmd_t = void (*)(std::istream&, std::ostream&, DB&);
 
-  using Dataset = hvostov::BSTree< std::string, std::string >;
-  using DatasetMap = std::map< std::string, Dataset >;
-  void handleError(std::ostream& out, std::istream& in);
-
-  void loadDatasets(std::istream& in, DatasetMap& datasets);
-  void printDataset(std::istream& in, std::ostream& out, DatasetMap& datasets);
-  void complementDatasets(std::istream& in, std::ostream& out, DatasetMap& datasets);
-  void intersectDatasets(std::istream& in, std::ostream& out, DatasetMap& datasets);
-  void unionDatasets(std::istream& in, std::ostream& out, DatasetMap& datasets);}
+  void loadDatasets(std::istream& in, DB& datasets);
+  void printDataset(std::istream& in, std::ostream& out, DB& datasets);
+  void complementDatasets(std::istream& in, std::ostream& out, DB& datasets);
+  void intersectDatasets(std::istream& in, std::ostream& out, DB& datasets);
+  void unionDatasets(std::istream& in, std::ostream& out, DB& datasets);}
 
 #endif
-
-cmds.push("print", printDataset);
-cmds.push("complement", complementDatasets);
-cmds.push("intersect", intersectDatasets);
-cmds.push("union", unionDatasets);
