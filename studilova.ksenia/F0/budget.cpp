a2813f433ed579c866f3d9781693c2cc6b3ce30f
kstudilova <kstudilova@gmail.com>
2026-06-21 18:39:19 +0300

    add root category and operations vector to budget struct

--- studilova.ksenia/F0/budget.cpp
+++ studilova.ksenia/F0/budget.cpp
@@ -1,14 +1,43 @@
 #include "budget.hpp"
 
-studilova::Budget::Budget():
-  name_()
+studilova::Budget::Budget() noexcept:
+  name_(),
+  rootCategory_("root", nullptr),
+  operations_()
 {}
 
-studilova::Budget::Budget(const std::string& name):
-  name_(name)
+studilova::Budget::Budget(const std::string& name) noexcept:
+  name_(name),
+  rootCategory_("root", nullptr),
+  operations_()
 {}
 
-const std::string& studilova::Budget::getName() const
+const std::string& studilova::Budget::getName() const noexcept
 {
   return name_;
+}
+
+studilova::Category& studilova::Budget::getRootCategory() noexcept
+{
+  return rootCategory_;
+}
+
+const studilova::Category& studilova::Budget::getRootCategory() const noexcept
+{
+  return rootCategory_;
+}
+
+void studilova::Budget::addOperation(const Operation& operation)
+{
+  operations_.pushBack(operation);
+}
+
+const studilova::Vector< studilova::Operation >& studilova::Budget::getOperations() const noexcept
+{
+  return operations_;
+}
+
+studilova::Vector< studilova::Operation >& studilova::Budget::getOperations() noexcept
+{
+  return operations_;
 }
--- studilova.ksenia/F0/budget.hpp
+++ studilova.ksenia/F0/budget.hpp
@@ -3,18 +3,36 @@
 
 #include <string>
 
+#include <vector.hpp>
+
+#include "category.hpp"
+#include "operation.hpp"
+
 namespace studilova
 {
   class Budget
   {
     public:
-      Budget();
-      explicit Budget(const std::string& name);
+      Budget() noexcept;
+      explicit Budget(const std::string& name) noexcept;
+      ~Budget();
 
-      const std::string& getName() const;
+      const std::string& getName() const noexcept;
+
+      Category& getRootCategory() noexcept;
+      const Category& getRootCategory() const noexcept;
+
+      void addOperation(const Operation& operation);
+
+      const Vector< Operation >& getOperations() const noexcept;
+      Vector< Operation >& getOperations() noexcept;
 
     private:
       std::string name_;
+      Category rootCategory_;
+      Vector< Operation > operations_;
+
+      void clearCategoryChildren(Category& category);
   };
 }
 
