9963e8e8c8a955a0ec6c0fd2b784fbf479cfa69c
Dmitry Levkin <mit.levkin@vk.com>
2026-06-22 01:39:35 +0300

    Implement initLayout command

--- levkin.dima/F0/commands.cpp
+++ levkin.dima/F0/commands.cpp
@@ -1,6 +1,8 @@
 #include "commands.hpp"
 #include "node.hpp"
 #include <iostream>
+#include <sstream>
+
 namespace rl {
   std::string generateUniqueId(const Map< std::string, RLNode* >& mapOfNodes)
   {
@@ -16,12 +18,31 @@ namespace rl {
   {
     out << "Works fine\n";
   }
-  void unregisterSubtree(RLNode* node, Map< std::string, RLNode* >& mapOfNodes) {
-      for (size_t i = 0; i < node->children.getSize(); ++i) {
-        unregisterSubtree(node->children[i].get(), mapOfNodes);
-      }
-      mapOfNodes.drop(node->id);
+  void unregisterSubtree(RLNode* node, Map< std::string, RLNode* >& mapOfNodes)
+  {
+    for (size_t i = 0; i < node->children.getSize(); ++i) {
+      unregisterSubtree(node->children[i].get(), mapOfNodes);
     }
+    mapOfNodes.drop(node->id);
+  }
+
+  void initLayout(std::istream& in, std::ostream& out, RootDB& db, RLRoot*)
+  {
+    float w = 1920.0f;
+    float h = 1080.0f;
+
+    std::string line;
+    if (std::getline(in, line) && !line.empty()) {
+      std::stringstream ss(line);
+      ss >> w >> h;
+    }
+
+    RLRoot newRoot(w, h);
+    db.data.pushBack(std::move(newRoot));
+    db.selectByIndex(db.data.getSize() - 1);
+
+    out << "Layout initialized: " << w << "x" << h << ". Root node created.\n";
+  }
   Cmds getCmds()
   {
     Cmds cmds;
