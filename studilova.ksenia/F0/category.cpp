f1e3deb49dacb96c26ed73140a5eacf949f4162c
kstudilova <kstudilova@gmail.com>
2026-06-21 17:15:25 +0300

    add Category struct

---
+++ studilova.ksenia/F0/category.cpp
@@ -0,0 +1,33 @@
+#include "category.hpp"
+
+studilova::Category::Category() noexcept:
+  name_(),
+  parent_(nullptr),
+  children_()
+{}
+
+studilova::Category::Category(const std::string& name, Category* parent) noexcept:
+  name_(name),
+  parent_(parent),
+  children_()
+{}
+
+const std::string& studilova::Category::getName() const noexcept
+{
+  return name_;
+}
+
+studilova::Category* studilova::Category::getParent() const noexcept
+{
+  return parent_;
+}
+
+const studilova::Vector< studilova::Category* >& studilova::Category::getChildren() const noexcept
+{
+  return children_;
+}
+
+studilova::Vector< studilova::Category* >& studilova::Category::getChildren() noexcept
+{
+  return children_;
+}
