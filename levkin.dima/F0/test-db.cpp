af849ee29d8dbd5d7e295dc8cb6e0b0d1c23b508
Dmitry Levkin <mit.levkin@vk.com>
2026-06-22 21:01:42 +0300

    Add layout test for margins and paddings

--- levkin.dima/F0/test-db.cpp
+++ levkin.dima/F0/test-db.cpp
@@ -33,7 +33,6 @@ BOOST_AUTO_TEST_CASE(test_basic_row_layout)
 
   BOOST_CHECK_EQUAL(child1->box.width, 200.0f);
   BOOST_CHECK_EQUAL(child2->box.width, 400.0f);
-  rl::saveVisualTree(db.getActive(), "layout_debug.html");
   
 }
 
@@ -168,4 +167,49 @@ BOOST_AUTO_TEST_CASE(test_auto_size_calculation)
 
   BOOST_CHECK_EQUAL(ch->box.width, 350.0f);
   BOOST_CHECK_EQUAL(ch->box.height, 250.0f);
+}
+
+BOOST_AUTO_TEST_CASE(test_layout_with_margins_and_paddings)
+{
+    RootDB db;
+    db.createNewLayout("test_offsets", "dummy_path", 1000.0f, 500.0f);
+    RLRootNode* rootNode = db.getActive();
+    
+    rootNode->root.flexDirection = FlexDirection::Row;
+    rootNode->root.justify = JustifyContent::FlexStart;
+    rootNode->root.align = AlignItems::Center;
+
+    rootNode->root.padding.left = 50.0f;
+    rootNode->root.padding.right = 0.0f;
+    rootNode->root.padding.top = 30.0f;
+    rootNode->root.padding.bottom = 0.0f;
+
+    auto ch1 = std::make_unique<RLNode>("child1", 200.0f, 100.0f, FlexDirection::Row);
+    ch1->margin.left = 20.0f;
+    ch1->margin.right = 10.0f;
+    ch1->margin.top = 15.0f;
+    ch1->margin.bottom = 15.0f;
+
+    auto ch2 = std::make_unique<RLNode>("child2", 300.0f, 100.0f, FlexDirection::Row);
+    ch2->margin.left = 25.0f;
+    ch2->margin.right = 0.0f;
+    ch2->margin.top = 0.0f;
+    ch2->margin.bottom = 0.0f;
+
+    RLNode* child1 = ch1.get();
+    RLNode* child2 = ch2.get();
+
+    rootNode->root.addChild(std::move(ch1));
+    rootNode->root.addChild(std::move(ch2));
+
+    db.calculateLayout();
+
+    BOOST_CHECK_EQUAL(child1->box.x, 70.0f);
+    BOOST_CHECK_EQUAL(child1->box.y, 215.0f);
+    BOOST_CHECK_EQUAL(child2->box.x, 305.0f);
+    BOOST_CHECK_EQUAL(child2->box.y, 215.0f);
+    BOOST_CHECK_EQUAL(child1->box.width, 200.0f);
+    BOOST_CHECK_EQUAL(child2->box.width, 300.0f);
+    
+    rl::saveVisualTree(db.getActive(), "layout_offsets_debug.html");
 }
