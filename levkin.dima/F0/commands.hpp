d3e27a9ec7a6c9ade20d76ac3c5ba76e289dd526
Dmitry Levkin <mit.levkin@vk.com>
2026-06-22 15:17:01 +0300

    Refactor layout and node headers

--- levkin.dima/F0/commands.hpp
+++ levkin.dima/F0/commands.hpp
@@ -1,5 +1,5 @@
-#ifndef COMMANDS
-#define COMMANDS
+#ifndef COMMANDS_RL
+#define COMMANDS_RL
 
 #include "hashtable.hpp"
 #include "node.hpp"
@@ -9,45 +9,45 @@
 
 namespace rl {
 
-  struct RootDB;
-  struct RLRoot;
+struct RootDB;
+struct RLRoot;
 
-  template < typename Key, typename Value >
-  using Map = levkin::HashTable< Key, Value >;
+template <typename Key, typename Value>
+using Map = levkin::HashTable<Key, Value>;
 
-  using cmd_t = void (*)(std::istream&, std::ostream&, RootDB&, RLRoot*);
-  using Cmds = Map< std::string, cmd_t >;
+using cmd_t = void (*)(std::istream &, std::ostream &, RootDB &, RLRoot *);
+using Cmds = Map<std::string, cmd_t>;
 
-  struct RLRoot {
-    RLRoot() = default;
-    RLRoot(float width, float height);
+struct RLRoot {
+  RLRoot() = default;
+  RLRoot(float width, float height);
 
-    RLNode root;
-    Map< std::string, RLNode* > mapOfNodes;
-    float baseWidth = 1920.0f;
-    float baseHeight = 1080.0f;
-  };
+  RLNode root;
+  Map<std::string, RLNode *> mapOfNodes;
+  float baseWidth = 1920.0f;
+  float baseHeight = 1080.0f;
+};
 
-  struct RootDB {
-    stf::Vector< RLRoot > data;
-    RLRoot* selected = nullptr;
+struct RootDB {
+  stf::Vector<RLRoot> data;
+  RLRoot *selected = nullptr;
 
-    void select(RLRoot& item);
-    void selectByIndex(size_t index);
-  };
-
-  void healthcheck(std::istream&, std::ostream&, RootDB&, RLRoot*);
-  void initLayout(std::istream&, std::ostream&, RootDB&, RLRoot*);
-  void createChild(std::istream&, std::ostream&, RootDB&, RLRoot*);
-  void createNode(std::istream&, std::ostream&, RootDB&, RLRoot*);
-  void emptyNode(std::istream&, std::ostream&, RootDB&, RLRoot*);
-  void deleteNode(std::istream&, std::ostream&, RootDB&, RLRoot*);
-  void moveNode(std::istream&, std::ostream&, RootDB&, RLRoot*);
-  void setWidth(std::istream&, std::ostream&, RootDB&, RLRoot*);
-  void setHeight(std::istream&, std::ostream&, RootDB&, RLRoot*);
-  void setSize(std::istream&, std::ostream&, RootDB&, RLRoot*);
-  Cmds getCmds();
-
-}
+  void select(RLRoot &item);
+  void selectByIndex(size_t index);
+};
+void bootstrapSession(RootDB &, std::ostream &);
+void healthcheck(std::istream &, std::ostream &, RootDB &, RLRoot *);
+void initLayout(std::istream &, std::ostream &, RootDB &, RLRoot *);
+void createChild(std::istream &, std::ostream &, RootDB &, RLRoot *);
+void createNode(std::istream &, std::ostream &, RootDB &, RLRoot *);
+void emptyNode(std::istream &, std::ostream &, RootDB &, RLRoot *);
+void deleteNode(std::istream &, std::ostream &, RootDB &, RLRoot *);
+void moveNode(std::istream &, std::ostream &, RootDB &, RLRoot *);
+void setWidth(std::istream &, std::ostream &, RootDB &, RLRoot *);
+void setHeight(std::istream &, std::ostream &, RootDB &, RLRoot *);
+void setSize(std::istream &, std::ostream &, RootDB &, RLRoot *);
+void printHelp(std::istream &, std::ostream &, RootDB &, RLRoot *);
+Cmds getCmds();
+} // namespace rl
 
 #endif
--- levkin.dima/F0/node.hpp
+++ levkin.dima/F0/node.hpp
@@ -1,67 +1,49 @@
+#ifndef NODE_RL
+#define NODE_RL
+
+#include "node-properties.hpp"
 #include "vector.hpp"
 #include <memory>
 #include <string>
-#ifndef DOM
-#define DOM
 
 namespace rl {
 
-  enum class FlexDirection { Row, Column, RowReverse, ColumnReverse };
+struct RLNode {
+  std::string id;
+  stf::Vector<std::unique_ptr<RLNode>> children;
+  RLNode *parent = nullptr;
 
-  enum class JustifyContent {
-    FlexStart,
-    FlexEnd,
-    Center,
-    SpaceBetween,
-    SpaceAround
-  };
+  void addChild(std::unique_ptr<RLNode> child);
+  void removeChild(const std::string &nodeId);
+  void clearChildren() noexcept;
 
-  enum class AlignItems { FlexStart, FlexEnd, Center, Stretch };
+  Sides margin;
+  Sides padding;
 
-  struct Sides {
-    float left = 0.0f;
-    float right = 0.0f;
-    float top = 0.0f;
-    float bottom = 0.0f;
-  };
+  float width = -1.0f;
+  float height = -1.0f;
 
-  struct Rect {
-    float x = 0.0f;
-    float y = 0.0f;
-    float width = 0.0f;
-    float height = 0.0f;
-  };
+  FlexDirection flexDirection = FlexDirection::Row;
+  JustifyContent justify = JustifyContent::FlexStart;
+  AlignItems align = AlignItems::FlexStart;
 
-  struct RLNode {
-    std::string id;
-    stf::Vector< std::unique_ptr< RLNode > > children;
-    RLNode* parent = nullptr;
+  Rect box;
 
-    void addChild(std::unique_ptr< RLNode >);
-    void removeChild(const std::string& id);
-    void clearChildren();
+  RLNode() = default;
+  RLNode(std::string nodeId, float w, float h, FlexDirection dir);
+  ~RLNode() = default;
 
-    Sides margin;
-    Sides padding;
-    Rect box;
-    float width = -1.0;
-    float height = -1.0;
+  RLNode(const RLNode &) = delete;
+  RLNode &operator=(const RLNode &) = delete;
 
-    FlexDirection flexDirection = FlexDirection::Row;
-    JustifyContent justify = JustifyContent::FlexStart;
-    AlignItems align = AlignItems::FlexStart;
+  RLNode(RLNode &&rhs) noexcept;
+  RLNode &operator=(RLNode &&rhs) noexcept;
 
-    RLNode() = default;
-    RLNode(std::string nodeId, float w, float h, FlexDirection dir);
-    ~RLNode() = default;
-    RLNode(const RLNode&) = delete;
-    RLNode& operator=(const stf::Vector< RLNode >&) = delete;
-    RLNode(RLNode&& rhs) noexcept;
-    RLNode& operator=(RLNode&& rhs) noexcept;
+private:
+  void updateChildrenParent() noexcept;
+  void reset() noexcept;
+};
 
-  private:
-    void updateChildrenParent() noexcept;
-    void reset() noexcept;
-  };
-}
+} // namespace rl
+
 #endif
