eb66ed2aa8a81f30c4e7bafa8bdab6908a321e2f
grisha-git <donkeevgrisa@gmail.com>
2026-06-22 16:53:54 +0300

    Union func

--- donkeev.grigoriy/S4/trees-commands.cpp
+++ donkeev.grigoriy/S4/trees-commands.cpp
@@ -108,3 +108,55 @@ void donkeev::intersectDicts(std::istream& input, std::ostream&, donkeev::Datase
 
   dicts.push(newName, std::move(result));
 }
+
+void donkeev::uniteDicts(std::istream& input, std::ostream&, donkeev::Datasets& dicts)
+{
+  std::string newName, name1, name2;
+  if (!(input >> newName >> name1 >> name2))
+  {
+    throw std::runtime_error("Bad input");
+  }
+
+  Datasets::iterator existing = dicts.find(newName);
+  Datasets::iterator it1 = dicts.find(name1);
+  Datasets::iterator it2 = dicts.find(name2);
+  if (existing != dicts.end() || it1 == dicts.end() || it2 == dicts.end())
+  {
+    throw std::runtime_error("Bad input");
+  }
+
+  const Dataset& dict1 = it1->second;
+  const Dataset& dict2 = it2->second;
+
+  Dataset result;
+
+  Dataset::constIterator begin1 = dict1.begin();
+  Dataset::constIterator end1 = dict1.end();
+  
+  while (begin1 != end1)
+  {
+    int key = begin1->first;
+    const std::string& value = begin1->second;
+    result.push(key, value);
+    ++begin1;
+  }
+
+  Dataset::constIterator begin2 = dict2.begin();
+  Dataset::constIterator end2 = dict2.end();
+  
+  while (begin2 != end2)
+  {
+    int key = begin2->first;
+    const std::string& value = begin2->second;
+    
+    Dataset::iterator found = result.find(key);
+    if (found == result.end())
+    {
+      result.push(key, value);
+    }
+    
+    ++begin2;
+  }
+
+  dicts.push(newName, std::move(result));
+}
--- donkeev.grigoriy/S4/trees-commands.hpp
+++ donkeev.grigoriy/S4/trees-commands.hpp
@@ -12,6 +12,6 @@ namespace donkeev
   void printDicts(std::istream&, std::ostream&, donkeev::Datasets&);
   void complementDicts(std::istream&, std::ostream&, donkeev::Datasets&);
   void intersectDicts(std::istream&, std::ostream&, donkeev::Datasets&);
-  /*void uniteDicts(std::istream&, std::ostream&, donkeev::Datasets&);*/
+  void uniteDicts(std::istream&, std::ostream&, donkeev::Datasets&);
 }
 #endif
