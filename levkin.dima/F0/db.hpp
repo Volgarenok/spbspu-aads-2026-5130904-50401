53f526e486d77dd31b68bc28554b82873c08ecde
Dmitry Levkin <mit.levkin@vk.com>
2026-06-22 16:01:39 +0300

    Add database and root node structures

---
+++ levkin.dima/F0/db.hpp
@@ -0,0 +1,49 @@
+#ifndef DB_RL
+#define DB_RL
+
+#include "node.hpp"
+#include "utils.hpp"
+#include <memory>
+#include <string>
+
+namespace rl {
+
+struct RLRootNode {
+  std::string name;
+  std::string filePath;
+  RLNode root;
+  Map<std::string, RLNode *> mapOfNodes;
+
+  RLRootNode() {
+    root.id = "root";
+    mapOfNodes.add("root", &root);
+  }
+  RLRootNode(std::string layoutName, std::string path, float w, float h)
+      : name(std::move(layoutName)), filePath(std::move(path)) {
+    root.id = "root";
+    root.width = w;
+    root.height = h;
+    mapOfNodes.add("root", &root);
+  }
+};
+
+class RootDB {
+private:
+  Map<std::string, std::unique_ptr<RLRootNode>> storage;
+  RLRootNode *selected = nullptr;
+
+public:
+  bool loadDatabase(const std::string &dbPath);
+  bool saveDatabase(const std::string &dbPath);
+  bool loadLayout(const std::string &name);
+  bool saveActiveLayout();
+  void createNewLayout(const std::string &name, const std::string &path,
+                       float w, float h);
+  void selectLayout(const std::string &name);
+  RLRootNode *getActive() const;
+  bool hasLayout(const std::string &name) const;
+};
+
+} // namespace rl
+
+#endif
