aa2d7ade43531ebc3207ec0449d3516b36ae72f4
aaaa24 <chernov.arseniy@yandex.ru>
2026-06-23 08:47:47 +0300

    move conditionals to detail; edit by CG

--- chernov.arseniy/common/htiter.hpp
+++ chernov.arseniy/common/htiter.hpp
@@ -5,25 +5,27 @@
 #include <utility>
 
 namespace chernov {
-  template< bool B, class T, class F >
-  struct conditional {
-    using type = T;
-  };
+  namespace detail {
+    template < bool B, class T, class F >
+    struct conditional {
+      using type = T;
+    };
 
-  template< class T, class F >
-  struct conditional< false, T, F > {
-    using type = F;
-  };
+    template < class T, class F >
+    struct conditional< false, T, F > {
+      using type = F;
+    };
+  }
 
-  template< class Key, class Value, class Hash, class Equal >
+  template < class Key, class Value, class Hash, class Equal >
   class HashTable;
 
-  template< class Key, class Value, class Hash, class Equal, bool IsConst >
+  template < class Key, class Value, class Hash, class Equal, bool IsConst >
   class HTIter {
   public:
     using pair_type = std::pair< const Key, Value >;
-    using reference = typename conditional< IsConst, const pair_type &, pair_type & >::type;
-    using point = typename conditional< IsConst, const pair_type *, pair_type * >::type;
+    using reference = typename detail::conditional< IsConst, const pair_type &, pair_type & >::type;
+    using point = typename detail::conditional< IsConst, const pair_type *, pair_type * >::type;
 
     HTIter();
     HTIter(const HashTable< Key, Value, Hash, Equal > * ht, size_t index);
@@ -34,47 +36,47 @@ namespace chernov {
     HTIter & operator++();
     HTIter operator++(int);
 
-    template< bool OtherConst >
+    template < bool OtherConst >
     bool operator==(const HTIter< Key, Value, Hash, Equal, OtherConst > & other) const noexcept;
 
-    template< bool OtherConst >
+    template < bool OtherConst >
     bool operator!=(const HTIter< Key, Value, Hash, Equal, OtherConst > & other) const noexcept;
+
   private:
     const HashTable< Key, Value, Hash, Equal > * ht_;
     size_t index_;
   };
 }
 
-template< class Key, class Value, class Hash, class Equal, bool IsConst >
+template < class Key, class Value, class Hash, class Equal, bool IsConst >
 chernov::HTIter< Key, Value, Hash, Equal, IsConst >::HTIter():
   ht_(nullptr),
   index_(0)
 {}
 
-template< class Key, class Value, class Hash, class Equal, bool IsConst >
-chernov::HTIter< Key, Value, Hash, Equal, IsConst >::
-HTIter(const HashTable< Key, Value, Hash, Equal > * ht, size_t index):
+template < class Key, class Value, class Hash, class Equal, bool IsConst >
+chernov::HTIter< Key, Value, Hash, Equal, IsConst >::HTIter(const HashTable< Key, Value, Hash, Equal > * ht,
+  size_t index):
   ht_(ht),
   index_(index)
 {}
 
-template< class Key, class Value, class Hash, class Equal, bool IsConst >
-typename chernov::HTIter< Key, Value, Hash, Equal, IsConst>::reference
-chernov::HTIter< Key, Value, Hash, Equal, IsConst >::operator*() const
+template < class Key, class Value, class Hash, class Equal, bool IsConst >
+typename chernov::HTIter< Key, Value, Hash, Equal, IsConst >::reference
+  chernov::HTIter< Key, Value, Hash, Equal, IsConst >::operator*() const
 {
   return ht_->data_[index_];
 }
 
-template< class Key, class Value, class Hash, class Equal, bool IsConst >
-typename chernov::HTIter< Key, Value, Hash, Equal, IsConst>::point
-chernov::HTIter< Key, Value, Hash, Equal, IsConst >::operator->() const
+template < class Key, class Value, class Hash, class Equal, bool IsConst >
+typename chernov::HTIter< Key, Value, Hash, Equal, IsConst >::point
+  chernov::HTIter< Key, Value, Hash, Equal, IsConst >::operator->() const
 {
   return ht_->data_ + index_;
 }
 
-template< class Key, class Value, class Hash, class Equal, bool IsConst >
-chernov::HTIter< Key, Value, Hash, Equal, IsConst > &
-chernov::HTIter< Key, Value, Hash, Equal, IsConst >::operator++()
+template < class Key, class Value, class Hash, class Equal, bool IsConst >
+chernov::HTIter< Key, Value, Hash, Equal, IsConst > & chernov::HTIter< Key, Value, Hash, Equal, IsConst >::operator++()
 {
   if (index_ < ht_->num_buckets_ * ht_->bucket_cap_) {
     size_t home_bucket = index_ / ht_->bucket_cap_;
@@ -104,28 +106,26 @@ chernov::HTIter< Key, Value, Hash, Equal, IsConst >::operator++()
   return *this;
 }
 
-template< class Key, class Value, class Hash, class Equal, bool IsConst >
-chernov::HTIter< Key, Value, Hash, Equal, IsConst >
-chernov::HTIter< Key, Value, Hash, Equal, IsConst >::operator++(int)
+template < class Key, class Value, class Hash, class Equal, bool IsConst >
+chernov::HTIter< Key, Value, Hash, Equal, IsConst > chernov::HTIter< Key, Value, Hash, Equal, IsConst >::operator++(int)
 {
   HTIter old = *this;
   ++(*this);
   return old;
 }
 
-template< class Key, class Value, class Hash, class Equal, bool IsConst >
-template< bool OtherConst >
-bool chernov::HTIter< Key, Value, Hash, Equal, IsConst >::
-operator==(const HTIter< Key, Value, Hash, Equal, OtherConst > & other) const noexcept
+template < class Key, class Value, class Hash, class Equal, bool IsConst >
+template < bool OtherConst >
+bool chernov::HTIter< Key, Value, Hash, Equal, IsConst >::operator==(
+  const HTIter< Key, Value, Hash, Equal, OtherConst > & other) const noexcept
 {
   return ht_ == other.ht_ && index_ == other.index_;
 }
 
-
-template< class Key, class Value, class Hash, class Equal, bool IsConst >
-template< bool OtherConst >
-bool chernov::HTIter< Key, Value, Hash, Equal, IsConst >::
-operator!=(const HTIter< Key, Value, Hash, Equal, OtherConst > & other) const noexcept
+template < class Key, class Value, class Hash, class Equal, bool IsConst >
+template < bool OtherConst >
+bool chernov::HTIter< Key, Value, Hash, Equal, IsConst >::operator!=(
+  const HTIter< Key, Value, Hash, Equal, OtherConst > & other) const noexcept
 {
   return !(*this == other);
 }
