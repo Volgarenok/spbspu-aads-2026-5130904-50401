0d5cc319111389fe9938a85290e865f3980f0637
kstudilova <kstudilova@gmail.com>
2026-06-23 16:34:48 +0300

    add budget manager

---
+++ studilova.ksenia/F0/budget_manager.hpp
@@ -0,0 +1,69 @@
+#ifndef BUDGET_MANAGER_HPP
+#define BUDGET_MANAGER_HPP
+
+#include <string>
+
+#include <hash_table.hpp>
+#include <hmac_hash.hpp>
+
+#include "budget.hpp"
+
+namespace studilova
+{
+	class BudgetManager
+	{
+		public:
+  		using BudgetTable = HashTable< std::string, Budget, HMACHash, std::equal_to< std::string > >;
+
+  		BudgetManager() = default;
+  		BudgetManager(BudgetManager&&) = delete;
+			BudgetManager(const BudgetManager&) = delete;
+			BudgetManager& operator=(const BudgetManager&) = delete;
+  		BudgetManager& operator=(BudgetManager&&) = delete;
+			~BudgetManager() = default;
+
+			void createBudget(const std::string& name);
+			Budget& getBudget(const std::string& name);
+			const Budget& getBudget(const std::string& name) const;
+			bool hasBudget(const std::string& name) const;
+			void removeBudget(const std::string& name);
+
+		private:
+  		BudgetTable budgets_;
+	};
+}
+
+void studilova::BudgetManager::createBudget(const std::string& name)
+{
+  budgets_.add(name, Budget(name));
+}
+
+studilova::Budget& studilova::BudgetManager::getBudget(const std::string& name)
+{
+  return budgets_.get(name);
+}
+
+const studilova::Budget& studilova::BudgetManager::getBudget(const std::string& name) const
+{
+  return budgets_.get(name);
+}
+
+bool studilova::BudgetManager::hasBudget(const std::string& name) const
+{
+  try
+  {
+    budgets_.get(name);
+    return true;
+  }
+  catch ( const std::out_of_range&)
+  {
+    return false;
+  }
+}
+
+void studilova::BudgetManager::removeBudget(const std::string& name)
+{
+  budgets_.erase(name);
+}
+
+#endif
