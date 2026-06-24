7d3e4e5558d24a2d6ac785bd148e3d4a71857ff1
kstudilova <kstudilova@gmail.com>
2026-06-25 02:54:43 +0300

    fix CG

--- studilova.ksenia/F0/budget_manager.hpp
+++ studilova.ksenia/F0/budget_manager.hpp
@@ -10,29 +10,29 @@
 
 namespace studilova
 {
-	class BudgetManager
-	{
+  class BudgetManager
+  {
     public:
-  		using BudgetTable = HashTable< std::string, Budget, HMACHash, std::equal_to< std::string > >;
+      using BudgetTable = HashTable< std::string, Budget, HMACHash, std::equal_to< std::string > >;
 
-  		BudgetManager() = default;
-  		BudgetManager(BudgetManager&&) = delete;
-			BudgetManager(const BudgetManager&) = delete;
-			BudgetManager& operator=(const BudgetManager&) = delete;
-  		BudgetManager& operator=(BudgetManager&&) = delete;
-			~BudgetManager() = default;
+      BudgetManager() = default;
+      BudgetManager(BudgetManager&&) = delete;
+      BudgetManager(const BudgetManager&) = delete;
+      BudgetManager& operator=(const BudgetManager&) = delete;
+      BudgetManager& operator=(BudgetManager&&) = delete;
+      ~BudgetManager() = default;
 
-			void createBudget(const std::string& name);
-			Budget& getBudget(const std::string& name);
-			const Budget& getBudget(const std::string& name) const;
-			bool hasBudget(const std::string& name) const;
-			void removeBudget(const std::string& name);
+      void createBudget(const std::string& name);
+      Budget& getBudget(const std::string& name);
+      const Budget& getBudget(const std::string& name) const;
+      bool hasBudget(const std::string& name) const;
+      void removeBudget(const std::string& name);
 
-			Vector<std::string> getBudgetNames() const;
+      Vector<std::string> getBudgetNames() const;
 
     private:
-  		BudgetTable budgets_;
-	};
+      BudgetTable budgets_;
+  };
 }
 
 #endif
--- studilova.ksenia/F0/category.hpp
+++ studilova.ksenia/F0/category.hpp
@@ -8,23 +8,23 @@
 namespace studilova
 {
   class Category
-	{
-		public:
-			Category() noexcept;
-			Category(const std::string& name, Category* parent) noexcept;
+  {
+    public:
+      Category() noexcept;
+      Category(const std::string& name, Category* parent) noexcept;
 
-			const std::string& getName() const noexcept;
-			Category* getParent() const noexcept;
+      const std::string& getName() const noexcept;
+      Category* getParent() const noexcept;
 
-			void addChild(Category* child);
-			const Vector< Category* >& getChildren() const noexcept;
-			Vector< Category* >& getChildren() noexcept;
+      void addChild(Category* child);
+      const Vector< Category* >& getChildren() const noexcept;
+      Vector< Category* >& getChildren() noexcept;
 
-		private:
-			std::string name_;
-			Category* parent_;
-			Vector< Category* > children_;
-	};
+    private:
+      std::string name_;
+      Category* parent_;
+      Vector< Category* > children_;
+  };
 }
 
 #endif
