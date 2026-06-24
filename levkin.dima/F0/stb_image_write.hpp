335215302709474a0f6ed4b5e6dfa502ae119da3
Dmitry Levkin <mit.levkin@vk.com>
2026-06-24 12:41:19 +0300

    forgot to add files for work with png stuff

---
+++ levkin.dima/F0/stb_image_write.hpp
@@ -0,0 +1,14 @@
+#ifndef STB_IMAGE_WRITE_HPP
+#define STB_IMAGE_WRITE_HPP
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#pragma GCC diagnostic pop
+
+#endif
