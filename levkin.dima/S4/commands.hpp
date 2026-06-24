593b7bb04fcc51b5d0a4d915087f9c3b087026c6
Dmitry Levkin <mit.levkin@vk.com>
2026-06-24 18:02:29 +0300

    refactoring

--- levkin.dima/S4/commands.hpp
+++ levkin.dima/S4/commands.hpp
@@ -1,20 +1,15 @@
-
 #ifndef COMMANDS_HPP
 #define COMMANDS_HPP
-
 #include <iostream>
 #include <string>
 #include <map>
 #include "bstree.hpp"
 #include <iosfwd>
 #include <string>
-
 namespace levkin {
   using DictTree = BSTree< int, std::string >;
   using DatasetStore = BSTree< std::string, DictTree >;
-
   using cmd_t = void (*)(std::istream&, std::ostream&, DatasetStore&);
-
   void
   cmdPrint(std::istream& input, std::ostream& output, DatasetStore& datasets);
   void cmdComplement(std::istream& input,
@@ -25,8 +20,6 @@ namespace levkin {
                     DatasetStore& datasets);
   void
   cmdUnion(std::istream& input, std::ostream& output, DatasetStore& datasets);
-
   bool loadDictionaries(const std::string& filename, DatasetStore& datasets);
 }
-
 #endif
