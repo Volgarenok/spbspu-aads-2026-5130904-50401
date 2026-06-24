9a78cbf2e7239dcd9675fc61a4f732717d0c19cb
kstudilova <kstudilova@gmail.com>
2026-06-25 01:26:09 +0300

    move budget_manager realisation to cpp

--- studilova.ksenia/F0/budget_manager.hpp
+++ studilova.ksenia/F0/budget_manager.hpp
@@ -2,7 +2,6 @@
 #define BUDGET_MANAGER_HPP
 
 #include <string>
-#include <stdexcept>
 
 #include <hash_table.hpp>
 #include <hmac_hash.hpp>
@@ -34,49 +33,6 @@ namespace studilova
 		private:
   		BudgetTable budgets_;
 	};
-}
-
-void studilova::BudgetManager::createBudget(const std::string& name)
-{
-  budgets_.add(name, Budget(name));
-}
-
-studilova::Budget& studilova::BudgetManager::getBudget(const std::string& name)
-{
-  return budgets_.get(name);
-}
-
-const studilova::Budget& studilova::BudgetManager::getBudget(const std::string& name) const
-{
-  return budgets_.get(name);
-}
-
-bool studilova::BudgetManager::hasBudget(const std::string& name) const
-{
-  try
-  {
-    budgets_.get(name);
-    return true;
-  }
-  catch (const std::out_of_range&)
-  {
-    return false;
-  }
-}
-
-void studilova::BudgetManager::removeBudget(const std::string& name)
-{
-  budgets_.erase(name);
-}
-
-studilova::Vector<std::string> studilova::BudgetManager::getBudgetNames() const
-{
-  Vector< std::string > result;
-  for (auto it = budgets_.begin(); it != budgets_.end(); ++it)
-  {
-    result.pushBack((*it).first);
-  }
-  return result;
 }
 
 #endif
