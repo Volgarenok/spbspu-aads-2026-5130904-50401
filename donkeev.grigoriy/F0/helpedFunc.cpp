1de7867da3104cdbf38a2d360cb506e98cec8673
grisha-git <donkeevgrisa@gmail.com>
2026-06-26 11:00:43 +0300

    Save new car to data base

--- donkeev.grigoriy/F0/helpedFunc.cpp
+++ donkeev.grigoriy/F0/helpedFunc.cpp
@@ -20,3 +20,35 @@ std::string donkeev::nextWord(const std::string& str, size_t& pos)
   
   return str.substr(start, pos - start);
 }
+
+int inputInt(const std::string& prompt)
+{
+    int value;
+    std::cout << prompt;
+    std::cin >> value;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    return value;
+}
+
+std::string donkeev::inputString(const std::string& prompt)
+{
+  std::string value;
+  std::cout << prompt;
+  std::getline(std::cin, value);
+  return value;
+}
+
+void donkeev::saveCarToDataBase(const Car& car, const std::string& filename)
+{
+  std::ofstream file(filename, std::ios::app);
+  if (!file.is_open())
+  {
+    throw std::runtime_error("Bad file");
+    return;
+  }
+  
+  std::string line = car.vin + " " + car.brand + " " + car.model;
+  
+  file << line << "\n";
+  file.close();
+}
--- donkeev.grigoriy/F0/helpedFunc.hpp
+++ donkeev.grigoriy/F0/helpedFunc.hpp
@@ -1,6 +1,15 @@
 #include <string>
+#include <iostream>
+#include <limits>
+#include <fstream>
+
+#include "Car.hpp"
 
 namespace donkeev
 {
   std::string nextWord(const std::string&, size_t&);
+  int inputInt(const std::string&);
+  std::string inputString(const std::string&);
+
+  void saveCarToDataBase(const Car&, const std::string&);
 }
