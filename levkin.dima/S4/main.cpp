86a59cd47c506cf11560b7083b80929903b6c6fa
Dmitry Levkin <mit.levkin@vk.com>
2026-06-27 20:20:30 +0300

    BST in main instead of hash table

--- levkin.dima/S4/main.cpp
+++ levkin.dima/S4/main.cpp
@@ -1,35 +1,23 @@
 #include "commands.hpp"
 #include "bstree.hpp"
-#include "hashtable.hpp"
 #include <iostream>
-#include <fstream>
 #include <sstream>
-#include <boost/functional/hash.hpp>
-
 int main(int argc, char* argv[])
 {
   if (argc < 2) {
     std::cerr << "Error: filename parameter is required.\n";
     return 1;
   }
-
   levkin::DatasetStore datasets;
   if (!levkin::loadDictionaries(argv[1], datasets)) {
     std::cerr << "Error: could not open file " << argv[1] << "\n";
     return 1;
   }
-
-
-  using BoostStringHash = boost::hash< std::string >;
-  using StandardStringEqual = std::equal_to< std::string >;
-
-  levkin::HashTable< std::string, levkin::cmd_t, BoostStringHash, StandardStringEqual > cmds(4, 2);
-
-  cmds.add("print", levkin::cmdPrint);
-  cmds.add("complement", levkin::cmdComplement);
-  cmds.add("intersect", levkin::cmdIntersect);
-  cmds.add("union", levkin::cmdUnion);
-
+  levkin::BSTree< std::string, levkin::cmd_t > cmds;
+  cmds.push("print", levkin::cmdPrint);
+  cmds.push("complement", levkin::cmdComplement);
+  cmds.push("intersect", levkin::cmdIntersect);
+  cmds.push("union", levkin::cmdUnion);
   std::string line;
   while (std::getline(std::cin, line)) {
     if (line.empty()) {
@@ -38,22 +26,20 @@ int main(int argc, char* argv[])
     std::stringstream ss(line);
     std::string cmd_name;
     ss >> cmd_name;
-
     if (cmds.has(cmd_name)) {
       try {
         auto func = cmds.get(cmd_name);
         func(ss, std::cout, datasets);
         if (ss.fail()) {
-          std::cout << "<error>\n";
+          std::cout << "<INVALID COMAND>\n";
           std::cin.clear();
         }
       } catch (...) {
-        std::cout << "<error>\n";
+        std::cout << "<INVALID COMAND>\n";
       }
     } else {
-      std::cout << "<error>\n";
+      std::cout << "<INVALID COMAND>\n";
     }
   }
-
   return 0;
 }
