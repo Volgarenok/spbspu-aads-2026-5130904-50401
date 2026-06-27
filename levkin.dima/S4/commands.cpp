49fda59e1231df39938ef58080fd329fc497ecf6
Dmitry Levkin <mit.levkin@vk.com>
2026-06-27 20:25:48 +0300

    Implement dictionary loading from file

--- levkin.dima/S4/commands.cpp
+++ levkin.dima/S4/commands.cpp
@@ -1,3 +1,31 @@
 #include "commands.hpp"
 #include <fstream>
 #include <sstream>
+
+bool levkin::loadDictionaries(const std::string& filename,
+                              DatasetStore& datasets)
+{
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    return false;
+  }
+
+  std::string line;
+  while (std::getline(file, line)) {
+    if (line.empty()) {
+      continue;
+    }
+    std::stringstream ss(line);
+    std::string dictName;
+    ss >> dictName;
+
+    SubTree newDict;
+    int key;
+    std::string value;
+    while (ss >> key >> value) {
+      newDict.push(key, value);
+    }
+    datasets.push(dictName, std::move(newDict));
+  }
+  return true;
+}
