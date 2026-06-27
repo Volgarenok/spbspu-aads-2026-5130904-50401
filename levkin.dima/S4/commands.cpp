3cecebe7df08a227dfd77f023ae2da7600db9e41
Dmitry Levkin <mit.levkin@vk.com>
2026-06-27 20:30:27 +0300

    Implement union command for datasets

--- levkin.dima/S4/commands.cpp
+++ levkin.dima/S4/commands.cpp
@@ -115,3 +115,32 @@ void levkin::cmdIntersect(std::istream& in, std::ostream& out, DatasetStore& dat
   }
   datasets.push(res_name, std::move(result));
 }
+
+void levkin::cmdUnion(std::istream& in, std::ostream& out, DatasetStore& datasets)
+{
+  std::string res_name, left_name, right_name;
+  if (!(in >> res_name >> left_name >> right_name)) {
+    throw std::invalid_argument("Invalid arguments");
+  }
+
+  if (!datasets.has(left_name) || !datasets.has(right_name)) {
+    out << "<error>\n";
+    return;
+  }
+
+  const SubTree& left = datasets.get(left_name);
+  const SubTree& right = datasets.get(right_name);
+  SubTree result;
+
+  for (auto it = left.cbegin(); it != left.cend(); ++it) {
+    result.push(it->key, it->value);
+  }
+  for (auto it = right.cbegin(); it != right.cend(); ++it) {
+    result.push(it->key, it->value);
+  }
+
+  if (datasets.has(res_name)) {
+    datasets.drop(res_name);
+  }
+  datasets.push(res_name, std::move(result));
+}
