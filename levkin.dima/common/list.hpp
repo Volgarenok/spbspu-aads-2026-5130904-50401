a46738c1a778e6798b6b61711fe378b42dc4c877
Dmitry Levkin <mit.levkin@vk.com>
2026-06-28 13:33:30 +0300

    renaming

--- levkin.dima/common/list.hpp
+++ levkin.dima/common/list.hpp
@@ -4,6 +4,8 @@
 #include <cstddef>
 #include <stdexcept>
 #include <utility>
+#include <memory>
+
 namespace levkin {
   template < class T >
   class List;
@@ -128,48 +130,57 @@ namespace levkin {
   template < class T >
   LIter< T > List< T >::begin() noexcept
   {
-    return LIter< T >(pseudo->next);
+    return LIter< T >(pseudo_->next);
   }
+
   template < class T >
   LCIter< T > List< T >::begin() const noexcept
   {
     return cbegin();
   }
+
   template < class T >
   LCIter< T > List< T >::cbegin() const noexcept
   {
-    return LCIter< T >(pseudo->next);
+    return LCIter< T >(pseudo_->next);
   }
+
   template < class T >
   LIter< T > List< T >::end() noexcept
   {
-    return LIter< T >(pseudo);
+    return LIter< T >(pseudo_);
   }
+
   template < class T >
   LCIter< T > List< T >::end() const noexcept
   {
     return cend();
   }
+
   template < class T >
   LCIter< T > List< T >::cend() const noexcept
   {
-    return LCIter< T >(pseudo);
+    return LCIter< T >(pseudo_);
   }
+
   template < class T >
-  void List< T >::pushFront(T val)
+  void List< T >::pushFront(const T& val)
   {
-    insertAfter(LIter< T >(pseudo), val);
+    insertAfter(LIter< T >(pseudo_), val);
   }
+
   template < class T >
-  void List< T >::pushBack(T val)
+  void List< T >::pushBack(const T& val)
   {
-    insertAfter(LIter< T >(pseudo->prev), val);
+    insertAfter(LIter< T >(pseudo_->prev), val);
   }
+
   template < class T >
-  List< T >::List() : pseudo(new detail::NodeBase())
+  void List< T >::popFront() noexcept
   {
-    pseudo->next = pseudo;
-    pseudo->prev = pseudo;
+    if (begin() != end()) {
+      erase(begin());
+    }
   }
 
   template < class T >
@@ -243,121 +254,24 @@ namespace levkin {
     }
     return count;
   }
+
   template < class T >
-  void List< T >::clearAndInit(size_t size, T val)
-  {
-    clear();
-    for (size_t i = 0; i < size; ++i) {
-      this->pushBack(val);
-    }
-  }
+  List< T >::List(NodeBase* pseudoNode) noexcept:
+    pseudo_(pseudoNode)
+  {}
+
   template < class T >
-  void List< T >::erase(LIter< T > from, LIter< T > to)
+  LIter< T > List< T >::eraseFast(LIter< T > pos) noexcept
   {
-    while (from != to) {
-      erase(from++);
-    }
-  }
-  template < class T >
-  LIter< T > List< T >::erase(LIter< T > pos)
-  {
-    if (pos == end()) {
-      return pos;
-    }
-    return __eraseFast(pos);
-  }
-  template < class T >
-  void List< T >::popFront()
-  {
-    if (begin() != end()) {
-      erase(begin());
-    }
-  }
-  template < class T >
-  void List< T >::popBack()
-  {
-    if (begin() != end()) {
-      erase(LIter< T >(pseudo->prev));
-    }
-  }
-  template < class T >
-  LIter< T > List< T >::insertAfter(LIter< T > it, const T& val)
-  {
-    if (it.curr == nullptr) {
-      throw std::out_of_range("out of bounds or null. wierd");
-    }
-    detail::Node< T >* newNode = new detail::Node< T >();
-    newNode->val = val;
-    newNode->prev = it.curr;
-    newNode->next = it.curr->next;
-    newNode->next->prev = newNode;
-    it.curr->next = newNode;
-    return LIter< T >(newNode);
-  }
-  template < class T >
-  void List< T >::clear() noexcept
-  {
-    while (pseudo->next != pseudo) {
-      __eraseFast(LIter< T >(pseudo->next));
-    }
-  }
-  template < class T >
-  List< T >::List(T val) : List()
-  {
-    pushBack(val);
-  }
-  template < class T >
-  List< T >::~List()
-  {
-    if (pseudo == nullptr) {
-      return;
-    }
-    clear();
-    delete pseudo;
-  }
-  template < class T >
-  List< T >::List(const List< T >& a) : List()
-  {
-    try {
-      for (LCIter< T > i = a.cbegin(); i != a.cend(); i++) {
-        pushBack(*i);
-      }
-    } catch (...) {
-      clear();
-      delete pseudo;
-      throw;
-    }
-  }
-  template < class T >
-  List< T >::List(List< T >&& a) noexcept : List(a.pseudo)
-  {
-    a.pseudo = nullptr;
-  }
-  template < class T >
-  List< T >& List< T >::operator=(List< T > a) noexcept
-  {
-    a.swap(*this);
-    return *this;
-  }
-  template < class T >
-  void List< T >::swap(List< T >& a) noexcept
-  {
-    std::swap(a.pseudo, this->pseudo);
-  }
-  template < class T >
-  List< T >::List(detail::NodeBase* pseudo_node) : pseudo(pseudo_node)
-  {
-  }
-  template < class T >
-  LIter< T > List< T >::__eraseFast(LIter< T > pos) noexcept
-  {
-    detail::NodeBase* toDelete = pos.curr;
+    NodeBase* toDelete = pos.curr_;
     toDelete->prev->next = toDelete->next;
     toDelete->next->prev = toDelete->prev;
-    detail::NodeBase* nxt = toDelete->next;
-    delete static_cast< detail::Node< T >* >(toDelete);
+
+    NodeBase* nxt = toDelete->next;
+    delete castNode(toDelete);
     return LIter< T >(nxt);
   }
+
   template < class T >
   class LCIter
   {
@@ -428,7 +342,7 @@ namespace levkin {
   };
 
   template < class T >
-  LCIter< T >::LCIter(LIter< T > it) noexcept : curr(it.curr)
+  class LIter
   {
     friend class List< T >;
     friend class LCIter< T >;
@@ -498,4 +412,5 @@ namespace levkin {
     NodeBase* curr_ = nullptr;
   };
 }
+
 #endif
