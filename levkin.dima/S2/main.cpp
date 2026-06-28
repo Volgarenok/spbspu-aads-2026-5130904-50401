a1c632fc20afbd19a75118b684da408126ff95de
Dmitry Levkin <mit.levkin@vk.com>
2026-06-28 13:09:42 +0300

    CG: include order and spacing

--- levkin.dima/S2/main.cpp
+++ levkin.dima/S2/main.cpp
@@ -1,24 +1,30 @@
-#include "utils.hpp"
 #include <exception>
 #include <fstream>
 #include <iostream>
+#include "utils.hpp"
+
 int main(int argc, char* argv[])
 {
-  try {
-
-    std::ifstream file;
-    if (argc > 1) {
-      file.open(argv[1]);
-      if (!file) {
-        std::cerr << "bad file\n";
-        return 1;
-      }
+  std::ifstream file;
+  if (argc > 1) {
+    file.open(argv[1]);
+    if (!file) {
+      std::cerr << "bad file\n";
+      return 1;
     }
-    std::istream& in = (argc > 1) ? file : std::cin;
+  }
+  std::istream& in = (argc > 1) ? file : std::cin;
+
+  try {
     levkin::Stack< long long > results = levkin::parse(in);
 
+    if (!results.empty()) {
+      std::cout << results.top();
+      results.pop();
+    }
     while (!results.empty()) {
-      std::cout << results.drop() << (results.empty() ? "" : " ");
+      std::cout << " " << results.top();
+      results.pop();
     }
     std::cout << "\n";
   } catch (const std::exception& e) {
